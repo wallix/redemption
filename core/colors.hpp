97c12decb5b3eb81c0b056303589e6939de77d33
Christophe Grosjean <pykoder@gmail.com>
2011-05-17 00:25:47 +0200

    Removed obsolete useless color management code

--- core/colors.hpp
+++ core/colors.hpp
@@ -50,75 +50,7 @@ typedef RGBcolor RGBPalette[256];
 // |    24 bpp   |    3 bytes |     RGB color triplet (1 byte per component).  |
 // +-------------+------------+------------------------------------------------+
 
-#warning we should ensure input domain for r, g and b is what we want it to be
-// rrrgggbb
-inline uint8_t color8(uint8_t r, uint8_t g, uint8_t b)
-{
-  return (r & 0xE0) | ((g >> 3) & 0x1C) | ((b >> 6) & 0x03);
-}
-
-// 0 r1 r2 r3 r4 r5 g1 g2 g3 g4 g5 b1 b2 b3 b4 b5
-inline uint16_t color15(const uint8_t r, const uint8_t g, const uint8_t b)
-{
-   // r1 r2 r3 r4 r5 r6 r7 r8
-  return ((r << 7) & 0x7C00) // 0 r1 r2 r3 r4 r5 0 0 0 0 0 0 0 0 0 0
-    // g1 g2 g3 g4 g5 g6 g7 g8
-    |((g << 2) & 0x03E0) // 0 0 0 0 0 0 g1 g2 g3 g4 g5 0 0 0 0 0
-    // b1 b2 b3 b4 b5 b6 b7 b8
-    | (b >> 3); // 0 0 0 0 0 0 0 0 0 0 0 b1 b2 b3 b4 b5
-}
-
-inline void splitcolor15(uint8_t & r, uint8_t & g, uint8_t & b, uint32_t c)
-{
-  // r1 r2 r3 r4 r5 g1 g2 g3 g4 g5 b1 b2 b3 b4 b5
-  r = ((c >> 7) & 0xf8) | ((c >> 12) & 0x7); // r1 r2 r3 r4 r5 r1 r2 r3
-  g = ((c >> 2) & 0xf8) | ((c >>  7) & 0x7); // g1 g2 g3 g4 g5 g1 g2 g3
-  b = ((c << 3) & 0xf8) | ((c >>  2) & 0x7); // b1 b2 b3 b4 b5 b1 b2 b3
-}
-
-inline uint32_t color16(uint8_t r, uint8_t g, uint8_t b)
-{
-   // r1 r2 r3 r4 r5 r6 r7 r8
-   return ((r << 8) & 0xF800) // r1 r2 r3 r4 r5 0 0 0 0 0 0 0 0 0 0 0
-       // g1 g2 g3 g4 g5 g6 g7 g8
-       |((g << 3) & 0x07E0)       // 0 0 0 0 0 g1 g2 g3 g4 g5 g6 0 0 0 0 0
-       // b1 b2 b3 b4 b5 b6 b7 b8
-       | (b >> 3);                // 0 0 0 0 0 0 0 0 0 0 0 b1 b2 b3 b4 b5
-}
-
-inline void splitcolor16(uint8_t & r, uint8_t & g, uint8_t & b, uint32_t c)
-{
-  // r1 r2 r3 r4 r5 g1 g2 g3 g4 g5 g6 b1 b2 b3 b4 b5
-  r = ((c >> 8) & 0xf8) | ((c >> 13) & 0x7); // r1 r2 r3 r4 r5 r6 r7 r8
-  g = ((c >> 3) & 0xfc) | ((c >>  9) & 0x3); // g1 g2 g3 g4 g5 g6 g1 g2
-  b = ((c << 3) & 0xf8) | ((c >>  2) & 0x7); // b1 b2 b3 b4 b5 b1 b2 b3
-}
-
-inline uint32_t color24RGB(const uint8_t r, const uint8_t g, const uint8_t b)
-{
-  return (r << 16) | (g << 8) | b;
-}
-
-inline uint32_t color24BGR(const uint8_t r, const uint8_t g, const uint8_t b)
-{
-  return color24RGB(b, g, r);
-}
-
-inline void splitcolor32RGB(uint8_t & r, uint8_t & g, uint8_t & b, uint32_t c)
-{
-  r = c >> 16;
-  g = c >> 8;
-  b = c;
-}
-
-inline void splitcolor32BGR(uint8_t & r, uint8_t & g, uint8_t & b, uint32_t c)
-{
-  b = c >> 16;
-  g = c >> 8;
-  r = c;
-}
-
-inline uint32_t color_decode(const uint32_t c, const uint8_t in_bpp, const uint32_t (& palette)[256]){
+static inline RGBcolor color_decode(const RGBcolor c, const uint8_t in_bpp, const uint32_t (& palette)[256]){
     switch (in_bpp){
     case 8:
       return palette[c] & 0xFFFFFF;
@@ -150,7 +82,7 @@ inline uint32_t color_decode(const uint32_t c, const uint8_t in_bpp, const uint3
     return 0;
 }
 
-inline uint32_t color_encode(const uint32_t c, const uint8_t out_bpp, const uint32_t (& palette)[256]){
+static inline RGBcolor color_encode(const RGBcolor c, const uint8_t out_bpp, const uint32_t (& palette)[256]){
     switch (out_bpp){
     case 8:
     // rrrgggbb
@@ -189,7 +121,7 @@ inline uint32_t color_encode(const uint32_t c, const uint8_t out_bpp, const uint
 }
 
 // reference color is BGR 24, not usual RGB
-inline uint32_t color_encodeBGR24(const uint32_t pix, const uint8_t out_bpp, const uint32_t (& palette)[256])
+static inline RGBcolor color_encodeBGR24(const RGBcolor pix, const uint8_t out_bpp, const uint32_t (& palette)[256])
 {
     return color_encode(((pix & 0xFF) << 16) | (pix & 0x00FF00) | ((pix >> 16) & 0xFF), out_bpp, palette);
 }
