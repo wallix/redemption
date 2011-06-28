4bb5ccb1bbf40e58315c4a7f14e534d865a4e267
Christophe Grosjean <pykoder@gmail.com>
2011-06-28 17:04:23 +0800

    Fixing RDP Layer

--- front/server_rdp.hpp
+++ front/server_rdp.hpp
@@ -132,24 +132,176 @@ struct server_rdp {
             stream.out_uint8(g);
             stream.out_uint8(r);
         }
-        stream.mark_end();
+        this->send_rdp_packet(stream, RDP_DATA_PDU_UPDATE, stream.rdp_hdr - stream.data);
+    }
 
+
+    /*****************************************************************************/
+    void server_rdp_send(Stream & stream, int pdu_type) throw (Error)
+    {
         stream.p = stream.rdp_hdr;
         int len = stream.end - stream.p;
         stream.out_uint16_le(len);
-        stream.out_uint16_le(0x10 | RDP_PDU_DATA);
+        stream.out_uint16_le(0x10 | pdu_type);
         stream.out_uint16_le(this->mcs_channel);
-        stream.out_uint32_le(this->share_id);
-        stream.out_uint8(0);
-        stream.out_uint8(1);
-        stream.out_uint16_le(len - 14);
-        stream.out_uint8(RDP_DATA_PDU_UPDATE);
-        stream.out_uint8(0);
-        stream.out_uint16_le(0);
+        this->sec_layer.server_sec_send(stream, MCS_GLOBAL_CHANNEL);
+    }
+
+// [MS-RDPBCGR] 2.2.8.1.1.1.2 Share Data Header (TS_SHAREDATAHEADER)
+// =================================================================
+// The TS_SHAREDATAHEADER header is a T.128 legacy mode header (see [T128]
+// section 8.3) present in slow-path I/O packets.
+
+// shareControlHeader (6 bytes): Share Control Header (section 2.2.8.1.1.1.1)
+//   containing information about the packet.
+
+// shareId (4 bytes): A 32-bit, unsigned integer. Share identifier for the
+//   packet (see [T128] section 8.4.2 for more information about share IDs).
+
+// pad1 (1 byte): An 8-bit, unsigned integer. Padding. Values in this field are
+//   ignored.
+
+// streamId (1 byte): An 8-bit, unsigned integer. The stream identifier for the
+//   packet.
+// +------------------------+--------------------------------------------------+
+// | 0x00 STREAM_UNDEFINED  | Undefined stream priority. This value might be   |
+// |                        | used in the Server Synchronize PDU (see section  |
+// |                        | 2.2.1.19) due to a server-side RDP bug. It MUST  |
+// |                        | not be used in conjunction with any other PDUs.  |
+// |                        |                                                  |
+// +------------------------+--------------------------------------------------+
+// | 0x01 STREAM_LOW        | Low-priority stream.                             |
+// +------------------------+--------------------------------------------------+
+// | 0x02 STREAM_MED        | Medium-priority stream.                          |
+// +------------------------+--------------------------------------------------+
+// | 0x04 STREAM_HI         | High-priority stream.                            |
+// +------------------------+--------------------------------------------------+
+
+// uncompressedLength (2 bytes): A 16-bit, unsigned integer. The uncompressed
+//   length of the packet in bytes.
+
+// pduType2 (1 byte): An 8-bit, unsigned integer. The type of data PDU.
+
+// +------------------------------+--------------------------------------------+
+// | 2 PDUTYPE2_UPDATE            | Update PDU (section 2.2.9.1.1.3)           |
+// +------------------------------+--------------------------------------------+
+// | 20 PDUTYPE2_CONTROL          | Control PDU (section 2.2.1.15.1)           |
+// +------------------------------+--------------------------------------------+
+// | 27 PDUTYPE2_POINTER          | Pointer Update PDU (section 2.2.9.1.1.4)   |
+// +------------------------------+--------------------------------------------+
+// | 28 PDUTYPE2_INPUT            | Input PDU (section 2.2.8.1.1.3)            |
+// +------------------------------+--------------------------------------------+
+// | 31 PDUTYPE2_SYNCHRONIZE      | Synchronize PDU (section 2.2.1.14.1)       |
+// +------------------------------+--------------------------------------------+
+// | 33 PDUTYPE2_REFRESH_RECT     | Refresh Rect PDU (section 2.2.11.2.1)      |
+// +------------------------------+--------------------------------------------+
+// | 34 PDUTYPE2_PLAY_SOUND       | Play Sound PDU (section 2.2.9.1.1.5.1)     |
+// +------------------------------+--------------------------------------------+
+// | 35 PDUTYPE2_SUPPRESS_OUTPUT  | Suppress Output PDU (section 2.2.11.3.1)   |
+// +------------------------------+--------------------------------------------+
+// | 36 PDUTYPE2_SHUTDOWN_REQUEST | Shutdown Request PDU (section 2.2.2.2.1)   |
+// +------------------------------+------+-------------------------------------+
+// | 37 PDUTYPE2_SHUTDOWN_DENIED         | Shutdown Request Denied PDU         |
+// |                                     |         (section 2.2.2.3.1)         |
+// +-------------------------------------+-------------------------------------+
+// | 38 PDUTYPE2_SAVE_SESSION_INFO       | Save Session Info PDU               |
+// |                                     |         (section 2.2.10.1.1)        |
+// +-------------------------------------+-------------------------------------+
+// | 39 PDUTYPE2_FONTLIST                | Font List PDU (section 2.2.1.18.1)  |
+// +-------------------------------------+-------------------------------------+
+// | 40 PDUTYPE2_FONTMAP                 | Font Map PDU (section 2.2.1.22.1)   |
+// +-------------------------------------+-------------------------------------+
+// | 41 PDUTYPE2_SET_KEYBOARD_INDICATORS | Set Keyboard Indicators PDU         |
+// |                                     |       (section 2.2.8.2.1.1)         |
+// +-------------------------------------+---+---------------------------------+
+// | 43 PDUTYPE2_BITMAPCACHE_PERSISTENT_LIST | Persistent Key List PDU         |
+// |                                         | (section 2.2.1.17.1)            |
+// +-------------------------------------+---+---------------------------------+
+// | 44 PDUTYPE2_BITMAPCACHE_ERROR_PDU   | Bitmap Cache Error PDU (see         |
+// |                                     |     [MS-RDPEGDI] section 2.2.2.3.1) |
+// +-------------------------------------+-------------------------------------+
+// | 45 PDUTYPE2_SET_KEYBOARD_IME_STATUS | Set Keyboard IME Status PDU         |
+// |                                     |       (section 2.2.8.2.2.1)         |
+// +-----------------------------------+-+-------------------------------------+
+// | 46 PDUTYPE2_OFFSCRCACHE_ERROR_PDU | Offscreen Bitmap Cache Error PDU      |
+// |                                   | (see [MS-RDPEGDI] section 2.2.2.3.2)  |
+// +-----------------------------------+---------------------------------------+
+// | 47 PDUTYPE2_SET_ERROR_INFO_PDU    | Set Error Info PDU (section 2.2.5.1.1)|
+// +-----------------------------------+---------------------------------------+
+// | 48 PDUTYPE2_DRAWNINEGRID_ERROR_PDU| DrawNineGrid Cache Error PDU          |
+// |                                   | (see [MS-RDPEGDI] section 2.2.2.3.3)  |
+// +-----------------------------------+---------------------------------------+
+// | 49 PDUTYPE2_DRAWGDIPLUS_ERROR_PDU | GDI+ Error PDU (see [MS-RDPEGDI]      |
+// |                                   |  section 2.2.2.3.4)                   |
+// +-----------------------------------+---------------------------------------+
+// | 50 PDUTYPE2_ARC_STATUS_PDU        | Auto-Reconnect Status PDU             |
+// |                                   |    (section 2.2.4.1.1)                |
+// +-----------------------------------+---------------------------------------+
+
+// compressedType (1 byte): An 8-bit, unsigned integer. The compression type
+//   and flags specifying the data following the Share Data Header (section
+//   2.2.8.1.1.1.2).
+
+// +--------------------------+------------------------------------------------+
+// | 0x0F CompressionTypeMask | Indicates the package which was used for       |
+// |                          | compression. See the table which follows for a |
+// |                          | list of compression packages.                  |
+// +--------------------------+------------------------------------------------+
+// | 0x20 PACKET_COMPRESSED   | The payload data is compressed. This value     |
+// |                          | corresponds to MPPC bit C (see [RFC2118]       |
+// |                          | section 3.1).                                  |
+// +--------------------------+------------------------------------------------+
+// | 0x40 PACKET_AT_FRONT     | The decompressed packet MUST be placed at the  |
+// |                          | beginning of the history buffer. This value    |
+// |                          | corresponds to MPPC bit B (see [RFC2118]       |
+// |                          | section 3.1).                                  |
+// +--------------------------+------------------------------------------------+
+// | 0x80 PACKET_FLUSHED      | The history buffer MUST be reinitialized. This |
+// |                          | value corresponds to MPPC bit A (see [RFC2118] |
+// |                          | section 3.1).                                  |
+// +--------------------------+------------------------------------------------+
+
+// Possible compression package values:
+
+// +---------------------------+-----------------------------------------------+
+// | 0 PACKET_COMPR_TYPE_8K    | MPPC-8K compression (see section 3.1.8.4.1)   |
+// +---------------------------+-----------------------------------------------+
+// | 1 PACKET_COMPR_TYPE_64K   | MPPC-64K compression (see section 3.1.8.4.2)  |
+// +---------------------------+-----------------------------------------------+
+// | 2 PACKET_COMPR_TYPE_RDP6  | RDP 6.0 bulk compression (see [MS-RDPEGDI]    |
+// |                           | section 3.1.8.1).                             |
+// +---------------------------+-----------------------------------------------+
+// | 2 PACKET_COMPR_TYPE_RDP61 | RDP 6.1 bulk compression (see [MS-RDPEGDI]    |
+// |                           | section 3.1.8.2).                             |
+// +---------------------------+-----------------------------------------------+
+
+// Instructions specifying how to compress a data stream are listed in section
+//   3.1.8.2, while decompression of a data stream is described in section
+//   3.1.8.3.
+
+// compressedLength (2 bytes): A 16-bit, unsigned integer. The compressed length
+//   of the packet in bytes.
+
+
+    void send_rdp_packet(Stream & stream, uint8_t pdu_type2, size_t offset_hdr)
+    {
+        stream.mark_end();
+
+        int len = stream.end - &(stream.data[offset_hdr]);
+        // ShareControlHeader
+        stream.set_out_uint16_le(len,                 offset_hdr);
+        stream.set_out_uint16_le(0x10 | RDP_PDU_DATA, offset_hdr+2);
+        stream.set_out_uint16_le(this->mcs_channel,   offset_hdr+4);
+
+        stream.set_out_uint32_le(this->share_id,      offset_hdr+6);
+        stream.set_out_uint8(0,                       offset_hdr+10);
+        stream.set_out_uint8(1,                       offset_hdr+11);
+        stream.set_out_uint16_le(len - 14,            offset_hdr+12);
+        stream.set_out_uint8(pdu_type2,               offset_hdr+14);
+        stream.set_out_uint8(0,                       offset_hdr+15);
+        stream.set_out_uint16_le(0,                   offset_hdr+16);
 
         this->sec_layer.server_sec_send(stream, MCS_GLOBAL_CHANNEL);
-
-
     }
 
 //    2.2.9.1.1.4     Server Pointer Update PDU (TS_POINTER_PDU)
@@ -345,23 +497,7 @@ struct server_rdp {
 //    colorPointerData (1 byte): Single byte representing unused padding.
 //      The contents of this byte should be ignored.
 
-        stream.mark_end();
-
-        stream.p = stream.rdp_hdr;
-        int len = stream.end - stream.p;
-        stream.out_uint16_le(len);
-        stream.out_uint16_le(0x10 | RDP_PDU_DATA);
-        stream.out_uint16_le(this->mcs_channel);
-        stream.out_uint32_le(this->share_id);
-        stream.out_uint8(0);
-        stream.out_uint8(1);
-        stream.out_uint16_le(len - 14);
-        stream.out_uint8(RDP_DATA_PDU_POINTER);
-        stream.out_uint8(0);
-        stream.out_uint16_le(0);
-
-        this->sec_layer.server_sec_send(stream, MCS_GLOBAL_CHANNEL);
-
+        this->send_rdp_packet(stream, RDP_DATA_PDU_POINTER, stream.rdp_hdr - stream.data);
     }
 
 
@@ -408,21 +544,7 @@ struct server_rdp {
         stream.out_uint16_le(cache_idx);
         stream.mark_end();
 
-        stream.p = stream.rdp_hdr;
-        int len = stream.end - stream.p;
-        stream.out_uint16_le(len);
-        stream.out_uint16_le(0x10 | RDP_PDU_DATA);
-        stream.out_uint16_le(this->mcs_channel);
-        stream.out_uint32_le(this->share_id);
-        stream.out_uint8(0);
-        stream.out_uint8(1);
-        stream.out_uint16_le(len - 14);
-        stream.out_uint8(RDP_DATA_PDU_POINTER);
-        stream.out_uint8(0);
-        stream.out_uint16_le(0);
-
-        this->sec_layer.server_sec_send(stream, MCS_GLOBAL_CHANNEL);
-
+        this->send_rdp_packet(stream, RDP_DATA_PDU_POINTER, stream.rdp_hdr - stream.data);
     }
 
     void activate_and_process_data(Callback & cb)
@@ -552,17 +674,6 @@ struct server_rdp {
     }
 
     /*****************************************************************************/
-    void server_rdp_send(Stream & stream, int pdu_type) throw (Error)
-    {
-        stream.p = stream.rdp_hdr;
-        int len = stream.end - stream.p;
-        stream.out_uint16_le(len);
-        stream.out_uint16_le(0x10 | pdu_type);
-        stream.out_uint16_le(this->mcs_channel);
-        this->sec_layer.server_sec_send(stream, MCS_GLOBAL_CHANNEL);
-    }
-
-    /*****************************************************************************/
     void server_rdp_send_data_update_sync() throw (Error)
     {
         Stream stream(8192);
@@ -571,22 +682,8 @@ struct server_rdp {
         stream.p += 18;
         stream.out_uint16_le(RDP_UPDATE_SYNCHRONIZE);
         stream.out_clear_bytes(2);
-        stream.mark_end();
 
-        stream.p = stream.rdp_hdr;
-        int len = stream.end - stream.p;
-        stream.out_uint16_le(len);
-        stream.out_uint16_le(0x10 | RDP_PDU_DATA);
-        stream.out_uint16_le(this->mcs_channel);
-        stream.out_uint32_le(this->share_id);
-        stream.out_uint8(0);
-        stream.out_uint8(1);
-        stream.out_uint16_le(len - 14);
-        stream.out_uint8(RDP_DATA_PDU_UPDATE);
-        stream.out_uint8(0);
-        stream.out_uint16_le(0);
-
-        this->sec_layer.server_sec_send(stream, MCS_GLOBAL_CHANNEL);
+        this->send_rdp_packet(stream, RDP_DATA_PDU_UPDATE, stream.rdp_hdr - stream.data);
     }
 
     void server_rdp_incoming() throw (Error)
