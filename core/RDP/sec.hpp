56da99c04f2969da7402b9da09655df0042b0d2e
Christophe Grosjean <pykoder@gmail.com>
2011-09-04 07:21:24 +0800

    Use SecIn object for sec decryption

--- core/RDP/sec.hpp
+++ core/RDP/sec.hpp
@@ -237,6 +237,175 @@ struct CryptContext
 
 };
 
+
+
+
+// 2.2.1.1.1   RDP Negotiation Request (RDP_NEG_REQ)
+// =================================================
+//  The RDP Negotiation Request structure is used by a client to advertise the
+//  security protocols which it supports.
+
+// type (1 byte): An 8-bit, unsigned integer. Negotiation packet type. This
+//   field MUST be set to 0x01 (TYPE_RDP_NEG_REQ) to indicate that the packet
+//   is a Negotiation Request.
+
+// flags (1 byte): An 8-bit, unsigned integer. Negotiation packet flags. There
+//   are currently no defined flags so the field MUST be set to 0x00.
+
+// length (2 bytes): A 16-bit, unsigned integer. Indicates the packet size.
+//   This field MUST be set to 0x0008 (8 bytes).
+
+// requestedProtocols (4 bytes): A 32-bit, unsigned integer. Flags indicating
+//   the supported security protocols.
+
+// +---------------------------------+-----------------------------------------+
+// | 0x00000000 PROTOCOL_RDP_FLAG    |  Legacy RDP encryption.                 |
+// +---------------------------------+-----------------------------------------+
+// | 0x00000001 PROTOCOL_SSL_FLAG    | TLS 1.0 (section 5.4.5.1).              |
+// +---------------------------------+-----------------------------------------+
+// | 0x00000002 PROTOCOL_HYBRID_FLAG | Credential Security Support Provider    |
+// |                                 | protocol (CredSSP) (section 5.4.5.2).   |
+// |                                 | If this flag is set, then the           |
+// |                                 | PROTOCOL_SSL_FLAG (0x00000001) SHOULD   |
+// |                                 | also be set because Transport Layer     |
+// |                                 | Security (TLS) is a subset of CredSSP.  |
+// +---------------------------------+-----------------------------------------+
+
+// 2.2.1.2.1   RDP Negotiation Response (RDP_NEG_RSP)
+// ==================================================
+
+//  The RDP Negotiation Response structure is used by a server to inform the
+//  client of the security protocol which it has selected to use for the
+//  connection.
+
+// type (1 byte): An 8-bit, unsigned integer. Negotiation packet type. This field MUST be set to
+//   0x02 (TYPE_RDP_NEG_RSP) to indicate that the packet is a Negotiation Response.
+
+// flags (1 byte): An 8-bit, unsigned integer. Negotiation packet flags.
+
+// +--------------------------------+------------------------------------------+
+// | EXTENDED_CLIENT_DATA_SUPPORTED | The server supports extended client data |
+// | 0x00000001                     | blocks in the GCC Conference Create      |
+// |                                | Request user data (section 2.2.1.3).     |
+// +--------------------------------+------------------------------------------+
+
+// length (2 bytes): A 16-bit, unsigned integer. Indicates the packet size. This
+//   field MUST be set to 0x0008 (8 bytes)
+
+// selectedProtocol (4 bytes): A 32-bit, unsigned integer. Field indicating the
+//   selected security protocol.
+
+// +---------------------------------------------------------------------------+
+// | 0x00000000 PROTOCOL_RDP    | Legacy RDP encryption                        |
+// +---------------------------------------------------------------------------+
+// | 0x00000001 PROTOCOL_SSL    | TLS 1.0 (section 5.4.5.1)                    |
+// +---------------------------------------------------------------------------+
+// | 0x00000002 PROTOCOL_HYBRID | CredSSP (section 5.4.5.2)                    |
+// +---------------------------------------------------------------------------+
+
+// 2.2.1.2.2   RDP Negotiation Failure (RDP_NEG_FAILURE)
+// =====================================================
+
+//  The RDP Negotiation Failure structure is used by a server to inform the
+//  client of a failure that has occurred while preparing security for the
+//  connection.
+
+// type (1 byte): An 8-bit, unsigned integer. Negotiation packet type. This
+//   field MUST be set to 0x03 (TYPE_RDP_NEG_FAILURE) to indicate that the
+//   packet is a Negotiation Failure.
+
+// flags (1 byte): An 8-bit, unsigned integer. Negotiation packet flags. There
+//   are currently no defined flags so the field MUST be set to 0x00.
+
+// length (2 bytes): A 16-bit, unsigned integer. Indicates the packet size. This
+//   field MUST be set to 0x0008 (8 bytes).
+
+// failureCode (4 bytes): A 32-bit, unsigned integer. Field containing the
+//   failure code.
+
+// +---------------------------+-----------------------------------------------+
+// | SSL_REQUIRED_BY_SERVER    | The server requires that the client support   |
+// | 0x00000001                | Enhanced RDP Security (section 5.4) with      |
+// |                           | either TLS 1.0 (section 5.4.5.1) or CredSSP   |
+// |                           | (section 5.4.5.2). If only CredSSP was        |
+// |                           | requested then the server only supports TLS.  |
+// +---------------------------+-----------------------------------------------+
+// | SSL_NOT_ALLOWED_BY_SERVER | The server is configured to only use Standard |
+// | 0x00000002                | RDP Security mechanisms (section 5.3) and     |
+// |                           | does not support any External                 |
+// |                           | Security Protocols (section 5.4.5).           |
+// +---------------------------+-----------------------------------------------+
+// | SSL_CERT_NOT_ON_SERVER    | The server does not possess a valid server    |
+// | 0x00000003                | authentication certificate and cannot         |
+// |                           | initialize the External Security Protocol     |
+// |                           | Provider (section 5.4.5).                     |
+// +---------------------------+-----------------------------------------------+
+// | INCONSISTENT_FLAGS        | The list of requested security protocols is   |
+// | 0x00000004                | not consistent with the current security      |
+// |                           | protocol in effect. This error is only        |
+// |                           | possible when the Direct Approach (see        |
+// |                           | sections 5.4.2.2 and 1.3.1.2) is used and an  |
+// |                           | External Security Protocol (section 5.4.5) is |
+// |                           | already being used.                           |
+// +---------------------------+-----------------------------------------------+
+// | HYBRID_REQUIRED_BY_SERVER | The server requires that the client support   |
+// | 0x00000005                | Enhanced RDP Security (section 5.4) with      |
+// |                           | CredSSP (section 5.4.5.2).                    |
+// +---------------------------+-----------------------------------------------+
+
+
+class SecOut
+{
+    Stream & stream;
+    uint16_t offhdr;
+    uint8_t crypt_level;
+    CryptContext & crypt;
+    public:
+    SecOut(Stream & stream, uint8_t crypt_level, uint32_t flags, CryptContext & crypt)
+        : stream(stream), offhdr(stream.p - stream.data), crypt_level(crypt_level), crypt(crypt)
+    {
+        if (crypt_level > 1){
+            this->stream.out_uint32_le(flags);
+            this->stream.skip_uint8(8);
+        }
+        else {
+            this->stream.out_uint32_le(0);
+        }
+    }
+
+    void end(){
+        if (crypt_level > 1){
+            uint8_t * data = this->stream.data + this->offhdr + 12;
+            int datalen = this->stream.p - data;
+            this->crypt.sign(this->stream.data + this->offhdr + 4, 8, data, datalen);
+            this->crypt.encrypt(data, datalen);
+        }
+    }
+};
+
+
+class SecIn
+{
+    public:
+    uint32_t flags;
+    SecIn(Stream & stream, CryptContext & crypt)
+    {
+        this->flags = stream.in_uint32_le();
+        if ((this->flags & SEC_ENCRYPT)  || (this->flags & 0x0400)){
+            #warning shouldn't we check signature ?
+            stream.skip_uint8(8); /* signature */
+            // decrypting to the end of tpdu
+            crypt.decrypt(stream.p, stream.end - stream.p);
+        }
+    }
+
+    void end(){
+        #warning put some assertion here to ensure all data has been consumed
+    }
+
+};
+
+
 struct Sec
 {
 
@@ -1832,14 +2001,9 @@ struct Sec
             throw Error(ERR_MCS_RECV_ID_NOT_MCS_SDIN);
         }
         int len = mcs_in.len;
-        int sec_flags = stream.in_uint32_le();
-        if ((sec_flags & SEC_ENCRYPT) || (sec_flags & 0x0400)) { /* SEC_REDIRECT_ENCRYPT */
-            stream.skip_uint8(8); /* signature */
-            this->decrypt.decrypt(stream.p, stream.end - stream.p);
-        }
+        SecIn sec(stream, this->decrypt);
 
-        if (sec_flags & SEC_LICENCE_NEG) { /* 0x80 */
-
+        if (sec.flags & SEC_LICENCE_NEG) { /* 0x80 */
             uint8_t tag = stream.in_uint8();
             stream.skip_uint8(3); /* version, length */
             switch (tag) {
@@ -2288,170 +2452,5 @@ struct Sec
 
 };
 
-
-// 2.2.1.1.1   RDP Negotiation Request (RDP_NEG_REQ)
-// =================================================
-//  The RDP Negotiation Request structure is used by a client to advertise the
-//  security protocols which it supports.
-
-// type (1 byte): An 8-bit, unsigned integer. Negotiation packet type. This
-//   field MUST be set to 0x01 (TYPE_RDP_NEG_REQ) to indicate that the packet
-//   is a Negotiation Request.
-
-// flags (1 byte): An 8-bit, unsigned integer. Negotiation packet flags. There
-//   are currently no defined flags so the field MUST be set to 0x00.
-
-// length (2 bytes): A 16-bit, unsigned integer. Indicates the packet size.
-//   This field MUST be set to 0x0008 (8 bytes).
-
-// requestedProtocols (4 bytes): A 32-bit, unsigned integer. Flags indicating
-//   the supported security protocols.
-
-// +---------------------------------+-----------------------------------------+
-// | 0x00000000 PROTOCOL_RDP_FLAG    |  Legacy RDP encryption.                 |
-// +---------------------------------+-----------------------------------------+
-// | 0x00000001 PROTOCOL_SSL_FLAG    | TLS 1.0 (section 5.4.5.1).              |
-// +---------------------------------+-----------------------------------------+
-// | 0x00000002 PROTOCOL_HYBRID_FLAG | Credential Security Support Provider    |
-// |                                 | protocol (CredSSP) (section 5.4.5.2).   |
-// |                                 | If this flag is set, then the           |
-// |                                 | PROTOCOL_SSL_FLAG (0x00000001) SHOULD   |
-// |                                 | also be set because Transport Layer     |
-// |                                 | Security (TLS) is a subset of CredSSP.  |
-// +---------------------------------+-----------------------------------------+
-
-// 2.2.1.2.1   RDP Negotiation Response (RDP_NEG_RSP)
-// ==================================================
-
-//  The RDP Negotiation Response structure is used by a server to inform the
-//  client of the security protocol which it has selected to use for the
-//  connection.
-
-// type (1 byte): An 8-bit, unsigned integer. Negotiation packet type. This field MUST be set to
-//   0x02 (TYPE_RDP_NEG_RSP) to indicate that the packet is a Negotiation Response.
-
-// flags (1 byte): An 8-bit, unsigned integer. Negotiation packet flags.
-
-// +--------------------------------+------------------------------------------+
-// | EXTENDED_CLIENT_DATA_SUPPORTED | The server supports extended client data |
-// | 0x00000001                     | blocks in the GCC Conference Create      |
-// |                                | Request user data (section 2.2.1.3).     |
-// +--------------------------------+------------------------------------------+
-
-// length (2 bytes): A 16-bit, unsigned integer. Indicates the packet size. This
-//   field MUST be set to 0x0008 (8 bytes)
-
-// selectedProtocol (4 bytes): A 32-bit, unsigned integer. Field indicating the
-//   selected security protocol.
-
-// +---------------------------------------------------------------------------+
-// | 0x00000000 PROTOCOL_RDP    | Legacy RDP encryption                        |
-// +---------------------------------------------------------------------------+
-// | 0x00000001 PROTOCOL_SSL    | TLS 1.0 (section 5.4.5.1)                    |
-// +---------------------------------------------------------------------------+
-// | 0x00000002 PROTOCOL_HYBRID | CredSSP (section 5.4.5.2)                    |
-// +---------------------------------------------------------------------------+
-
-// 2.2.1.2.2   RDP Negotiation Failure (RDP_NEG_FAILURE)
-// =====================================================
-
-//  The RDP Negotiation Failure structure is used by a server to inform the
-//  client of a failure that has occurred while preparing security for the
-//  connection.
-
-// type (1 byte): An 8-bit, unsigned integer. Negotiation packet type. This
-//   field MUST be set to 0x03 (TYPE_RDP_NEG_FAILURE) to indicate that the
-//   packet is a Negotiation Failure.
-
-// flags (1 byte): An 8-bit, unsigned integer. Negotiation packet flags. There
-//   are currently no defined flags so the field MUST be set to 0x00.
-
-// length (2 bytes): A 16-bit, unsigned integer. Indicates the packet size. This
-//   field MUST be set to 0x0008 (8 bytes).
-
-// failureCode (4 bytes): A 32-bit, unsigned integer. Field containing the
-//   failure code.
-
-// +---------------------------+-----------------------------------------------+
-// | SSL_REQUIRED_BY_SERVER    | The server requires that the client support   |
-// | 0x00000001                | Enhanced RDP Security (section 5.4) with      |
-// |                           | either TLS 1.0 (section 5.4.5.1) or CredSSP   |
-// |                           | (section 5.4.5.2). If only CredSSP was        |
-// |                           | requested then the server only supports TLS.  |
-// +---------------------------+-----------------------------------------------+
-// | SSL_NOT_ALLOWED_BY_SERVER | The server is configured to only use Standard |
-// | 0x00000002                | RDP Security mechanisms (section 5.3) and     |
-// |                           | does not support any External                 |
-// |                           | Security Protocols (section 5.4.5).           |
-// +---------------------------+-----------------------------------------------+
-// | SSL_CERT_NOT_ON_SERVER    | The server does not possess a valid server    |
-// | 0x00000003                | authentication certificate and cannot         |
-// |                           | initialize the External Security Protocol     |
-// |                           | Provider (section 5.4.5).                     |
-// +---------------------------+-----------------------------------------------+
-// | INCONSISTENT_FLAGS        | The list of requested security protocols is   |
-// | 0x00000004                | not consistent with the current security      |
-// |                           | protocol in effect. This error is only        |
-// |                           | possible when the Direct Approach (see        |
-// |                           | sections 5.4.2.2 and 1.3.1.2) is used and an  |
-// |                           | External Security Protocol (section 5.4.5) is |
-// |                           | already being used.                           |
-// +---------------------------+-----------------------------------------------+
-// | HYBRID_REQUIRED_BY_SERVER | The server requires that the client support   |
-// | 0x00000005                | Enhanced RDP Security (section 5.4) with      |
-// |                           | CredSSP (section 5.4.5.2).                    |
-// +---------------------------+-----------------------------------------------+
-
-
-class SecOut
-{
-    Stream & stream;
-    uint16_t offhdr;
-    uint8_t crypt_level;
-    CryptContext & crypt;
-    public:
-    SecOut(Stream & stream, uint8_t crypt_level, uint32_t flags, CryptContext & crypt)
-        : stream(stream), offhdr(stream.p - stream.data), crypt_level(crypt_level), crypt(crypt)
-    {
-        if (crypt_level > 1){
-            this->stream.out_uint32_le(flags);
-            this->stream.skip_uint8(8);
-        }
-        else {
-            this->stream.out_uint32_le(0);
-        }
-    }
-
-    void end(){
-        if (crypt_level > 1){
-            uint8_t * data = this->stream.data + this->offhdr + 12;
-            int datalen = this->stream.p - data;
-            this->crypt.sign(this->stream.data + this->offhdr + 4, 8, data, datalen);
-            this->crypt.encrypt(data, datalen);
-        }
-    }
-};
-
-
-class SecIn
-{
-    public:
-    uint32_t flags;
-    SecIn(Stream & stream, CryptContext & crypt)
-    {
-        this->flags = stream.in_uint32_le();
-        if (this->flags & SEC_ENCRYPT){
-            #warning shouldn't we check signature ?
-            stream.skip_uint8(8); /* signature */
-            // decrypting to the end of tpdu
-            crypt.decrypt(stream.p, stream.end - stream.p);
-        }
-    }
-
-    void end(){
-        #warning put some assertion here to ensure all data has been consumed
-    }
-
-};
 
 #endif
