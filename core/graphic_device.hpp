6e167e090c4cd053bf93d43dab103378a69b8d26
Christophe Grosjean <pykoder@gmail.com>
2011-10-03 12:07:11 +0200

    Added missing virtual class definition file

---
+++ core/graphic_device.hpp
@@ -0,0 +1,94 @@
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   Product name: redemption, a FLOSS RDP proxy
+   Copyright (C) Wallix 2010
+   Author(s): Christophe Grosjean, Javier Caverni
+   Based on xrdp Copyright (C) Jay Sorg 2004-2010
+
+   module object. Some kind of interface between core and modules
+
+*/
+#if !defined(__GRAPHIC_DEVICE_HPP__)
+#define __GRAPHIC_DEVICE_HPP__
+
+#include <stdint.h>
+#include "client_info.hpp"
+#include "rect.hpp"
+#include "colors.hpp"
+#include "font.hpp"
+#include "RDP/orders/RDPOrdersCommon.hpp"
+#include "RDP/orders/RDPOrdersPrimaryHeader.hpp"
+#include "RDP/orders/RDPOrdersPrimaryOpaqueRect.hpp"
+#include "RDP/orders/RDPOrdersPrimaryScrBlt.hpp"
+#include "RDP/orders/RDPOrdersPrimaryDestBlt.hpp"
+#include "RDP/orders/RDPOrdersPrimaryMemBlt.hpp"
+#include "RDP/orders/RDPOrdersPrimaryPatBlt.hpp"
+#include "RDP/orders/RDPOrdersPrimaryLineTo.hpp"
+#include "RDP/orders/RDPOrdersPrimaryGlyphIndex.hpp"
+#include "RDP/orders/RDPOrdersSecondaryHeader.hpp"
+#include "RDP/orders/RDPOrdersSecondaryColorCache.hpp"
+#include "RDP/orders/RDPOrdersSecondaryBmpCache.hpp"
+#include "RDP/orders/RDPOrdersSecondaryBrushCache.hpp"
+#include "RDP/orders/RDPOrdersSecondaryGlyphCache.hpp"
+
+
+struct GraphicDevice
+{
+    virtual int text_width(const char * text) = 0;
+    virtual int text_height(const char * text) = 0;
+    virtual int server_begin_update() = 0;
+    virtual int server_end_update() = 0;
+    virtual const ClientInfo & get_client_info() const = 0;
+    virtual int get_front_bpp() const = 0;
+    virtual int get_front_width() const = 0;
+    virtual int get_front_height() const = 0;
+    virtual const Rect get_front_rect() = 0;
+    virtual void draw_window(const Rect & r, uint32_t bgcolor, const char * caption, bool has_focus) = 0;
+    virtual void draw_combo(const Rect & r, const char * caption, int state, bool has_focus) = 0;
+    virtual void draw_button(const Rect & r, const char * caption, int state, bool has_focus) = 0;
+    virtual void draw_edit(const Rect & r, char password_char, char * buffer, size_t edit_pos, bool has_focus) = 0;
+    virtual void server_draw_text(uint16_t x, uint16_t y, const char * text, uint32_t fgcolor, uint32_t bgcolor) = 0;
+    virtual void opaque_rect(const RDPOpaqueRect & cmd) = 0;
+    virtual void scr_blt(const RDPScrBlt & cmd) = 0;
+    virtual void dest_blt(const RDPDestBlt & cmd) = 0;
+    virtual void pat_blt(const RDPPatBlt & cmd) = 0;
+    virtual void mem_blt(const RDPMemBlt & memblt, Bitmap & bitmap, const BGRPalette & palette) = 0;
+    virtual void server_set_pen(int style, int width) = 0;
+
+    virtual void line_to(const RDPLineTo & cmd) = 0;
+    virtual void glyph_index(const RDPGlyphIndex & cmd) = 0;
+    virtual void brush_cache(const int index) = 0;
+    virtual void color_cache(const BGRPalette & palette, uint8_t cacheIndex) = 0;
+    virtual void bitmap_cache(const uint8_t cache_id, const uint16_t cache_idx) = 0;
+    virtual void glyph_cache(const FontChar & font_char, int font_index, int char_index) = 0;
+    virtual void bitmap_update(Bitmap & bitmap, const Rect & dst, int srcx, int srcy) = 0;
+    virtual void set_pointer(int cache_idx) = 0;
+    virtual void send_global_palette() = 0;
+    virtual void server_set_pointer(int x, int y, uint8_t* data, uint8_t* mask) = 0;
+
+    virtual void set_mod_palette(const BGRPalette & palette) = 0;
+    virtual const BGRColor convert(const BGRColor color) const = 0;
+    virtual const BGRColor convert_opaque(const BGRColor color) const = 0;
+    virtual const BGRColor convert24_opaque(const BGRColor color) const = 0;
+    virtual const BGRColor convert24(const BGRColor color) const = 0;
+    virtual uint32_t convert_to_black(uint32_t color) = 0;
+
+
+
+};
+
+
+#endif
