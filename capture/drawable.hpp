10c9f59ece9b6e5644c9a90a72a4712b62b1b359
christophe grosjean <pykoder@gmail.com>
2011-11-29 11:45:10 +0100

    Extracted lowlevel opaquerect in drawable

--- capture/drawable.hpp
+++ capture/drawable.hpp
@@ -48,27 +48,12 @@ public:
 
     void draw(const RDPOpaqueRect & cmd, const Rect & clip)
     {
-        const Rect trect = screen.intersect(clip).intersect(cmd.rect);
-        const uint32_t color = color_decode(cmd.color, this->bpp, this->palette);
-
-        uint8_t * const base = this->data + trect.y * this->rowsize + trect.x * ::nbbytes(this->bpp);
-        uint8_t * p = base;
-
-        for (size_t x = 0; x < (size_t)trect.cx ; x++){
-            if (this->bgr){
-                p[0] = color >> 16; p[1] = color >> 8; p[2] = color;
-            }
-            else {
-                p[0] = color; p[1] = color >> 8; p[2] = color >> 16;
-            }
-            p += 3;
+        const Rect & trect = screen.intersect(clip).intersect(cmd.rect);
+        uint32_t color = color_decode(cmd.color, this->bpp, this->palette);
+        if (this->bgr){
+            color = ((color << 16) & 0xFF0000) | (color & 0xFF00) |((color >> 16) & 0xFF);
         }
-        uint8_t * target = base;
-        size_t line_size = trect.cx * ::nbbytes(this->bpp);
-        for (size_t y = 1; y < (size_t)trect.cy ; y++){
-            target += this->rowsize;
-            memcpy(target, base, line_size);
-        }
+        this->opaquerect(trect, color);
     }
 
     void draw(const RDPDestBlt & cmd, const Rect & clip)
@@ -80,7 +65,7 @@ public:
     void draw(const RDPScrBlt & cmd, const Rect & clip)
     {
         // Destination rectangle : drect
-        const Rect & drect = cmd.rect.intersect(clip).intersect(screen);;
+        const Rect drect = screen.intersect(clip).intersect(cmd.rect);
         if (drect.isempty()){ return; }
         // adding delta move dest to source
         const signed int deltax = cmd.srcx - cmd.rect.x;
@@ -97,9 +82,25 @@ public:
         return this->data + ((rect.y + rect.cy - 1) * this->screen.cx + rect.x) * ::nbbytes(this->bpp);
     }
 
+    // low level opaquerect,
+    // mostly avoid clipping because we already took care of it
+    // also we already swapped color if we are using BGR instead of RGB
+    void opaquerect(const Rect & rect, const uint32_t color)
+    {
+        uint8_t * const base = first_pixel(rect);
+        uint8_t * p = base;
 
-
-
+        for (size_t x = 0; x < (size_t)rect.cx ; x++){
+            p[0] = color; p[1] = color >> 8; p[2] = color >> 16;
+            p += 3;
+        }
+        uint8_t * target = base;
+        size_t line_size = rect.cx * ::nbbytes(this->bpp);
+        for (size_t y = 1; y < (size_t)rect.cy ; y++){
+            target += this->rowsize;
+            memcpy(target, base, line_size);
+        }
+    }
 
     // low level destblt,
     // mostly avoid clipping because we already took care of it
@@ -110,15 +111,15 @@ public:
 
         switch (rop){
         case 0x00: // blackness
-            for (size_t y = 0; y < rect.cy ; y++){
+            for (size_t y = 0; y < (size_t)rect.cy ; y++){
                 memset(p, 0, rect.cx * ::nbbytes(this->bpp));
                 p += this->rowsize;
             }
         break;
         case 0x55: // inversion
-            for (size_t y = 0; y < rect.cy ; y++){
+            for (size_t y = 0; y < (size_t)rect.cy ; y++){
                 p = base + this->rowsize * y;
-                for (size_t x = 0; x < rect.cx ; x++){
+                for (size_t x = 0; x < (size_t)rect.cx ; x++){
                     p[0] ^= 0xFF; p[1] ^= 0xFF; p[2] ^= 0xFF;
                     p += 3;
                 }
@@ -127,7 +128,7 @@ public:
         case 0xAA: // change nothing
         break;
         case 0xFF: // whiteness
-            for (size_t y = 0; y < rect.cy ; y++){
+            for (size_t y = 0; y < (size_t)rect.cy ; y++){
                 memset(p, 0, rect.cx * ::nbbytes(this->bpp));
                 p += this->rowsize;
             }
@@ -152,7 +153,7 @@ public:
                 uint8_t * source = this->first_pixel(srect);
                 int offset = 0;
                 size_t width_in_bytes = this->screen.cx * ::nbbytes(this->bpp);
-                for (size_t j = 0; j < drect.cy ; j++) {
+                for (size_t j = 0; j < (size_t)drect.cy ; j++) {
                     memcpy(target + offset,
                            source + offset,
                            drect.cx * ::nbbytes(this->bpp));
@@ -164,7 +165,7 @@ public:
                 uint8_t * source = this->beginning_of_last_line(srect);
                 int offset = 0;
                 size_t width_in_bytes = this->screen.cx * ::nbbytes(this->bpp);
-                for (size_t j = 0; j < drect.cy ; j++) {
+                for (size_t j = 0; j < (size_t)drect.cy ; j++) {
                     memcpy(target + offset,
                            source + offset,
                            drect.cx * ::nbbytes(this->bpp));
@@ -176,7 +177,7 @@ public:
                 uint8_t * source = this->first_pixel(srect);
                 int offset = 0;
                 size_t width_in_bytes = this->screen.cx * ::nbbytes(this->bpp);
-                for (size_t j = 0; j < drect.cy ; j++) {
+                for (size_t j = 0; j < (size_t)drect.cy ; j++) {
                     memmove(target + offset,
                            source + offset,
                            drect.cx * ::nbbytes(this->bpp));
