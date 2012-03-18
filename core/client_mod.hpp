3737e75fc4e2613677af49a7d0d4cf6f516c7d40
Christophe Grosjean <pykoder@gmail.com>
2012-03-18 03:41:58 +0100

    Moved composite drawing primitives from client_mod to internal_mod

--- core/client_mod.hpp
+++ core/client_mod.hpp
@@ -205,231 +205,16 @@ struct client_mod : public Callback {
         }
     }
 
-
-    int get_front_bpp() const {
-        return this->front.get_front_bpp();
-    }
-
-    int get_front_width() const {
-        return this->front.get_front_width();
-    }
-
-    int get_front_height() const {
-        return this->front.get_front_height();
-    }
-
-     const Rect get_front_rect(){
-        return Rect(0, 0, this->get_front_width(), get_front_height());
-    }
-
-    void draw_window(const Rect & r, uint32_t bgcolor, const char * caption, bool has_focus, const Rect & clip){
-
-        // Window surface and border
-        this->front.draw(RDPOpaqueRect(r, bgcolor), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x + 1, r.y + 1, r.cx - 2, 1), WHITE), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x + 1, r.y + 1, 1, r.cy - 2), WHITE), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x + 1, r.y + r.cy - 2, r.cx - 2, 1), DARK_GREY), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x+r.cx-2, r.y + 1, 1, r.cy), DARK_GREY), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x, r.y + r.cy - 1, r.cx, 1), BLACK), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x + r.cx - 1, r.y, 1, r.cy), BLACK), clip);
-
-        // Title bar
-        this->front.draw(RDPOpaqueRect(Rect(r.x + 3, r.y + 3, r.cx - 5, 18),
-                          has_focus?WABGREEN:DARK_GREY), clip);
-
-        this->server_draw_text(r.x + 4, r.y + 4, caption,
-                has_focus?WABGREEN:DARK_GREY,
-                has_focus?WHITE:BLACK, clip);
-    }
-
-    void draw_combo(const Rect & r, const char * caption, int state, bool has_focus, const Rect & clip)
+    void glyph_cache(const FontChar & font_char, int font_index, int char_index)
     {
-        this->front.draw(RDPOpaqueRect(Rect(r.x, r.y, r.cx, r.cy), GREY), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x + 1, r.y + 1, r.cx - 3, r.cy - 3), WHITE), clip);
-        if (has_focus) {
-            this->front.draw(RDPOpaqueRect(Rect(r.x + 3, r.y + 3, (r.cx - 6) - 18, r.cy - 5), DARK_WABGREEN), clip);
-        }
-        this->front.draw(RDPOpaqueRect(Rect(r.x, r.y, r.cx, 1), DARK_GREY), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x, r.y, 1, r.cy), DARK_GREY), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x, r.y + r.cy- 1, r.cx, 1), WHITE), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x + r.cx - 1, r.y, 1, r.cy), WHITE), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x + 1, r.y + 1, 1, r.cy - 2), BLACK), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x + 1, r.y + 1, r.cx - 2, 1), BLACK), clip);
-        this->server_draw_text(r.x + 4, r.y + 3, caption, has_focus?DARK_WABGREEN:WHITE, has_focus?WHITE:BLACK, clip);
-        this->draw_button(Rect(r.x + r.cx - 20, r.y + 2, 18, r.cy - 4), "", state, false, clip);
+        RDPGlyphCache cmd(font_index, 1, char_index, font_char.offset, font_char.baseline, font_char.width, font_char.height, font_char.data);
+        this->front.orders->draw(cmd);
     }
-
-    void draw_button(const Rect & r, const char * caption, int state, bool has_focus, const Rect & clip){
-
-        int bevel = (state == BUTTON_STATE_DOWN)?1:0;
-
-        this->front.draw(RDPOpaqueRect(r, GREY), clip);
-        if (state == BUTTON_STATE_DOWN) {
-            this->front.draw(RDPOpaqueRect(Rect(r.x, r.y, r.cx, 1), BLACK), clip);
-            this->front.draw(RDPOpaqueRect(Rect(r.x, r.y, 1, r.cy), BLACK), clip);
-            this->front.draw(RDPOpaqueRect(Rect(r.x + 1, r.y + 1, r.cx - 2, 1), DARK_GREY), clip);
-            this->front.draw(RDPOpaqueRect(Rect(r.x + 1, r.y + 1, 1, r.cy - 2), DARK_GREY), clip);
-            this->front.draw(RDPOpaqueRect(Rect(r.x + 1, r.y + (r.cy - 2), r.cx - 1, 1), DARK_GREY), clip);
-            this->front.draw(RDPOpaqueRect(Rect(r.x + (r.cx - 2), r.y + 1, 1, r.cy - 1), DARK_GREY), clip);
-            this->front.draw(RDPOpaqueRect(Rect(r.x, r.y + (r.cy - 1), r.cx, 1), BLACK), clip);
-            this->front.draw(RDPOpaqueRect(Rect(r.x + (r.cx - 1), r.y, 1, r.cy), BLACK), clip);
-        } else {
-            this->front.draw(RDPOpaqueRect(Rect(r.x, r.y, r.cx, 1), WHITE), clip);
-            this->front.draw(RDPOpaqueRect(Rect(r.x, r.y, 1, r.cy), WHITE), clip);
-            this->front.draw(RDPOpaqueRect(Rect(r.x + 1, r.y + (r.cy - 2), r.cx - 1, 1), DARK_GREY), clip);
-            this->front.draw(RDPOpaqueRect(Rect((r.x + r.cx) - 2, r.y + 1, 1, r.cy - 1), DARK_GREY), clip);
-            this->front.draw(RDPOpaqueRect(Rect(r.x, r.y + (r.cy - 1), r.cx, 1), BLACK), clip);
-            this->front.draw(RDPOpaqueRect(Rect(r.x + (r.cx - 1), r.y, 1, r.cy), BLACK), clip);
-        }
-        int w = 0;
-        int h = 0;
-        this->text_metrics(caption, w, h);
-        this->server_draw_text(
-            r.x + r.cx / 2 - w / 2 + bevel,
-            r.y + r.cy / 2 - h / 2 + bevel,
-            caption, GREY, BLACK, clip);
-        // focus rect
-        if (has_focus) {
-            this->front.draw(
-                RDPPatBlt(Rect(r.x + 3 + bevel, r.y + 3 + bevel, r.cx - 8, 2),
-                    0xF0, GREY, BLACK,
-                    RDPBrush(r.x, r.y, 3, 0xaa, (const uint8_t *)"\xaa\x55\xaa\x55\xaa\x55\xaa\x55")), clip);
-            this->front.draw(
-                RDPPatBlt(Rect(r.x + 3 + bevel, r.y + 3 + bevel, 2, r.cy - 8),
-                    0xF0, GREY, BLACK,
-                    RDPBrush(r.x, r.y, 3, 0xaa, (const uint8_t *)"\xaa\x55\xaa\x55\xaa\x55\xaa\x55")), clip);
-            this->front.draw(
-                RDPPatBlt(Rect(r.x + r.cx - 6 + bevel, r.y + 3 + bevel, 2, r.cy - 8),
-                    0xF0, GREY, BLACK,
-                    RDPBrush(r.x, r.y, 3, 0xaa, (const uint8_t *)"\xaa\x55\xaa\x55\xaa\x55\xaa\x55")), clip);
-            this->front.draw(
-                RDPPatBlt(Rect(r.x + 3 + bevel, r.y + r.cy - 6 + bevel, r.cx - 8, 2),
-                    0xF0, GREY, BLACK,
-                    RDPBrush(r.x, r.y, 3, 0xaa, (const uint8_t *)"\xaa\x55\xaa\x55\xaa\x55\xaa\x55")), clip);
-        }
-    }
-
-    void draw_edit(const Rect & r, char password_char, char * buffer, size_t edit_pos, bool has_focus, const Rect & clip){
-        this->front.draw(RDPOpaqueRect(Rect(r.x+1, r.y+1, r.cx - 3, r.cy - 3), DARK_GREEN), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x, r.y, r.cx, 1), BLACK), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x, r.y, 1, r.cy), BLACK), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x, r.y + r.cy - 1, r.cx, 1), WHITE), clip);
-        this->front.draw(RDPOpaqueRect(Rect(r.x + r.cx - 1, r.y, 1, r.cy), WHITE), clip);
-
-        /* draw text */
-        char text[255];
-        wchar_t wtext[255];
-
-        if (password_char != 0) {
-            int i = mbstowcs(0, buffer, 0);
-            memset(text, password_char, i);
-            text[i] = 0;
-            this->server_draw_text(r.x + 4, r.y + 2, text, DARK_GREEN, LIGHT_GREEN, clip);
-        }
-        else {
-            this->server_draw_text(r.x + 4, r.y + 2, buffer, DARK_GREEN, LIGHT_GREEN, clip);
-        }
-        /* draw xor box(cursor) */
-        if (has_focus) {
-            if (password_char != 0) {
-                for (size_t index = 0; index < edit_pos; index++) {
-                    if (index >= 255) {
-                        break;
-                    }
-                    wtext[index] = password_char;
-                }
-                wtext[edit_pos] = 0;
-                wcstombs(text, wtext, 255);
-            } else {
-                mbstowcs(wtext, buffer, 255);
-                wtext[edit_pos] = 0;
-                wcstombs(text, wtext, 255);
-            }
-
-            int w = 0;
-            int h = 0;
-            this->text_metrics(text, w, h);
-            this->front.draw(
-                RDPOpaqueRect(Rect(r.x + 4 + w, r.y + 3, 2, r.cy - 6), PALE_GREEN),
-                clip);
-        }
-    }
-
-    TODO(" implementation of the server_draw_text function below is quite broken (a small subset of possibilities is implemented  especially for data). See MS-RDPEGDI 2.2.2.2.1.1.2.13 GlyphIndex (GLYPHINDEX_ORDER)")
-    void server_draw_text(uint16_t x, uint16_t y, const char * text, uint32_t fgcolor, uint32_t bgcolor, const Rect & clip)
-    {
-        setlocale(LC_CTYPE, "fr_FR.UTF-8");
-        this->front.send_global_palette();
-
-        // add text to glyph cache
-        TODO(" use mbsrtowcs instead")
-        int len = mbstowcs(0, text, 0);
-        wchar_t* wstr = new wchar_t[len + 2];
-        mbstowcs(wstr, text, len + 1);
-        int total_width = 0;
-        int total_height = 0;
-        uint8_t *data = new uint8_t[len * 4];
-        memset(data, 0, len * 4);
-        int f = 0;
-        int c = 0;
-        int distance_from_previous_fragment = 0;
-        for (int index = 0; index < len; index++) {
-            FontChar* font_item = this->front.font.font_items[wstr[index]];
-            TODO(" avoid passing parameters by reference to get results")
-            switch (this->front.cache.add_glyph(font_item, f, c))
-            {
-                case Cache::GLYPH_ADDED_TO_CACHE:
-                    this->glyph_cache(*font_item, f, c);
-                break;
-                default:
-                break;
-            }
-            data[index * 2] = c;
-            data[index * 2 + 1] = distance_from_previous_fragment;
-            distance_from_previous_fragment = font_item->incby;
-            total_width += font_item->incby;
-            total_height = std::max(total_height, font_item->height);
-        }
-
-        const Rect bk(x, y, total_width + 1, total_height);
-
-         RDPGlyphIndex glyphindex(
-            f, // cache_id
-            0x03, // fl_accel
-            0x0, // ui_charinc
-            1, // f_op_redundant,
-            bgcolor, // bgcolor
-            fgcolor, // fgcolor
-            bk, // bk
-            Rect(), // op
-            // brush
-            RDPBrush(0, 0, 3, 0xaa,
-                (const uint8_t *)"\xaa\x55\xaa\x55\xaa\x55\xaa\x55"),
-//            this->brush,
-            x,  // glyph_x
-            y + total_height, // glyph_y
-            len * 2, // data_len in bytes
-            data // data
-        );
-
-        this->front.draw(glyphindex, clip);
-
-        delete [] wstr;
-        delete [] data;
-    }
-
 
     void server_set_pen(int style, int width)
     {
         this->pen.style = style;
         this->pen.width = width;
-    }
-
-
-    void glyph_cache(const FontChar & font_char, int font_index, int char_index)
-    {
-        RDPGlyphCache cmd(font_index, 1, char_index, font_char.offset, font_char.baseline, font_char.width, font_char.height, font_char.data);
-        this->front.orders->draw(cmd);
     }
 
 
@@ -453,9 +238,20 @@ struct client_mod : public Callback {
         }
     }
 
-    uint32_t convert_to_black(uint32_t color)
-    {
-        return 0; // convert(color);
+    int get_front_bpp() const {
+        return this->front.get_front_bpp();
+    }
+
+    int get_front_width() const {
+        return this->front.get_front_width();
+    }
+
+    int get_front_height() const {
+        return this->front.get_front_height();
+    }
+
+     const Rect get_front_rect(){
+        return Rect(0, 0, this->get_front_width(), get_front_height());
     }
 
     virtual void rdp_input_invalidate(const Rect & r) = 0;
