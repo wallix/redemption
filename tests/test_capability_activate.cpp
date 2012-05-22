68d0915ecb35a84eefbc3b5ffed35117b2615d53
Dominique Lafages <dlafages@wallix.com>
2012-05-22 14:47:00 +0200

    refs #3387 : Added TU for capability : ACTIVATE

---
+++ tests/test_capability_activate.cpp
@@ -0,0 +1,64 @@
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   Product name: redemption, a FLOSS RDP proxy
+   Copyright (C) Wallix 2010
+   Author(s): Christophe Grosjean
+
+   Unit test to RDP Activate object
+   Using lib boost functions for testing
+*/
+
+#define BOOST_AUTO_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE TestCapabilityActivation
+#include <boost/test/auto_unit_test.hpp>
+
+#define LOGPRINT
+#include "log.hpp"
+#include "RDP/capabilities.hpp"
+
+
+BOOST_AUTO_TEST_CASE(TestCapabilityActivationEmit)
+{
+    ActivationCaps activation_caps;
+    activation_caps.helpKeyFlag = 0;
+    activation_caps.helpKeyIndexFlag = 0;
+    activation_caps.helpExtendedKeyFlag = 0;
+    activation_caps.windowManagerKeyFlag = 0;
+
+    BOOST_CHECK_EQUAL(activation_caps.capabilityType, (uint16_t)CAPSTYPE_ACTIVATION);
+    BOOST_CHECK_EQUAL(activation_caps.len, (uint16_t)RDP_CAPLEN_ACTIVATION);
+    BOOST_CHECK_EQUAL(activation_caps.helpKeyFlag, (uint16_t) 0);
+    BOOST_CHECK_EQUAL(activation_caps.helpKeyIndexFlag, (uint16_t) 0);
+    BOOST_CHECK_EQUAL(activation_caps.helpExtendedKeyFlag, (uint16_t) 0);
+    BOOST_CHECK_EQUAL(activation_caps.windowManagerKeyFlag, (uint16_t) 0);
+
+    Stream stream(1024);
+    activation_caps.emit(stream);
+    stream.end = stream.p;
+    stream.p = stream.data;
+
+    ActivationCaps activation_caps2;
+
+    BOOST_CHECK_EQUAL(activation_caps2.capabilityType, (uint16_t)CAPSTYPE_ACTIVATION);
+    BOOST_CHECK_EQUAL(activation_caps2.len, (uint16_t)RDP_CAPLEN_ACTIVATION);
+    activation_caps2.recv(stream);
+
+    BOOST_CHECK_EQUAL(activation_caps2.helpKeyFlag, (uint16_t) 0);
+    BOOST_CHECK_EQUAL(activation_caps2.helpKeyIndexFlag, (uint16_t) 0);
+    BOOST_CHECK_EQUAL(activation_caps2.helpExtendedKeyFlag, (uint16_t) 0);
+    BOOST_CHECK_EQUAL(activation_caps2.windowManagerKeyFlag, (uint16_t) 0);
+}
