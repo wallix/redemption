6bcdfc8b44ae15aa195a4de5de41d95b2e0628cf
Christophe Grosjean <pykoder@gmail.com>
2013-03-14 04:29:55 +0100

    Added unit tests for sq_inmeta

--- tests/rio/test_sq_inmeta.cpp
+++ tests/rio/test_sq_inmeta.cpp
@@ -21,7 +21,7 @@
 
 #define BOOST_AUTO_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE TestSQIntracker
+#define BOOST_TEST_MODULE TestSQInmeta
 #include <boost/test/auto_unit_test.hpp>
 
 #define LOGPRINT
@@ -49,96 +49,70 @@
 // tv_begin and tv_end are start and stop of chunk
 
 
-BOOST_AUTO_TEST_CASE(TestSeqIntracker)
+BOOST_AUTO_TEST_CASE(TestSeqInmeta)
 {
     // SQIntracker is a Sequence for a chain of files used for reading, defined by an input tracker
     // The tracker starts with three header lines, then lines containing filenames and chunk time limits
 
     // sq_next() change filename to the next one in the list by reading tracker
     // the next call to sq_get_trans() will open a new trans for reading using this file
-    ::unlink("TESTOFS-000000.wrm");
-    ::unlink("TESTOFS-000001.wrm");
-    ::unlink("TESTOFS-000002.wrm");
-    ::unlink("TESTOFS-000003.wrm");
 
-    int fd1 = ::open("TESTOFS-000000.wrm", O_WRONLY|O_CREAT, S_IRUSR|S_IRUSR);
+    int fd1 = ::open("tmp.tmp", O_WRONLY|O_CREAT, S_IRUSR|S_IRUSR);
     ::close(fd1);
-    int fd2 = ::open("TESTOFS-000001.wrm", O_WRONLY|O_CREAT, S_IRUSR|S_IRUSR);
-    BOOST_CHECK_EQUAL(fd1, fd2);
-    ::close(fd2);
-    int fd3 = ::open("TESTOFS-000002.wrm", O_WRONLY|O_CREAT, S_IRUSR|S_IRUSR);
-    BOOST_CHECK_EQUAL(fd1, fd3);
-    ::close(fd3);
-
-    char trackerdata[] = "800 600\n"
-                      "0\n"
-                      "\n"
-                      "TESTOFS-000000.wrm 1352304810 1352304841\n"
-                      "TESTOFS-000001.wrm 1352304840 1352304861\n"
-                      "TESTOFS-000002.wrm 1352304860 1352304881\n"
-                    ;
-    RIO tracker;
-    RIO_ERROR status = rio_init_generator(&tracker, trackerdata, sizeof(trackerdata) - 1);
-    BOOST_CHECK_EQUAL(RIO_ERROR_OK, status);
+    ::unlink("tmp.tmp");
 
     timeval tv_begin;
     timeval tv_end;
     unsigned int chunk_num;
     char path[1024];    
 
-    SQ * sq = sq_new_intracker(&status, &tracker);
+    RIO_ERROR status;
+    SQ * sq = sq_new_inmeta(&status, "./tests/fixtures/sample", ".mwrm");
     BOOST_CHECK(sq != NULL);
     BOOST_CHECK_EQUAL(RIO_ERROR_OK, status);
 
     status = sq_get_chunk_info(sq, &chunk_num, path, sizeof(path), &tv_begin, &tv_end);
     BOOST_CHECK_EQUAL(RIO_ERROR_OK, status);
     BOOST_CHECK_EQUAL(1, chunk_num);
-    BOOST_CHECK(0 == strcmp(path, "TESTOFS-000000.wrm"));
+    BOOST_CHECK(0 == strcmp(path, "./tests/fixtures/sample0.wrm"));
     BOOST_CHECK_EQUAL(1352304810, tv_begin.tv_sec);
-    BOOST_CHECK_EQUAL(1352304841, tv_end.tv_sec);  
+    BOOST_CHECK_EQUAL(1352304870, tv_end.tv_sec);  
 
     RIO * rt = NULL;
     rt = sq_get_trans(sq, &status);
     BOOST_CHECK(rt != NULL);
-    BOOST_CHECK_EQUAL(fd1, rt->u.outfile.fd);
+    BOOST_CHECK_EQUAL(fd1+1, rt->u.outfile.fd);
     
     rt = sq_get_trans(sq, &status);
     BOOST_CHECK(rt != NULL);
-    BOOST_CHECK_EQUAL(fd1, rt->u.outfile.fd);
+    BOOST_CHECK_EQUAL(fd1+1, rt->u.outfile.fd);
     
     BOOST_CHECK_EQUAL(RIO_ERROR_OK, sq_next(sq));
     status = sq_get_chunk_info(sq, &chunk_num, path, sizeof(path), &tv_begin, &tv_end);
     BOOST_CHECK_EQUAL(RIO_ERROR_OK, status);
     BOOST_CHECK_EQUAL(2, chunk_num);
-    BOOST_CHECK(0 == strcmp(path, "TESTOFS-000001.wrm"));
-    BOOST_CHECK_EQUAL(1352304840, tv_begin.tv_sec);
-    BOOST_CHECK_EQUAL(1352304861, tv_end.tv_sec);  
+    BOOST_CHECK(0 == strcmp(path, "./tests/fixtures/sample1.wrm"));
+    BOOST_CHECK_EQUAL(1352304870, tv_begin.tv_sec);
+    BOOST_CHECK_EQUAL(1352304930, tv_end.tv_sec);  
     
     rt = sq_get_trans(sq, &status);
     BOOST_CHECK(rt != NULL);
-    BOOST_CHECK_EQUAL(fd1, rt->u.outfile.fd);
+    BOOST_CHECK_EQUAL(fd1+1, rt->u.outfile.fd);
     
     BOOST_CHECK_EQUAL(RIO_ERROR_OK, sq_next(sq));
     status = sq_get_chunk_info(sq, &chunk_num, path, sizeof(path), &tv_begin, &tv_end);
     BOOST_CHECK_EQUAL(RIO_ERROR_OK, status);
     BOOST_CHECK_EQUAL(3, chunk_num);
-    BOOST_CHECK(0 == strcmp(path, "TESTOFS-000002.wrm"));
-    BOOST_CHECK_EQUAL(1352304860, tv_begin.tv_sec);
-    BOOST_CHECK_EQUAL(1352304881, tv_end.tv_sec);  
+    BOOST_CHECK(0 == strcmp(path, "./tests/fixtures/sample2.wrm"));
+    BOOST_CHECK_EQUAL(1352304930, tv_begin.tv_sec);
+    BOOST_CHECK_EQUAL(1352304990, tv_end.tv_sec);  
 
     rt = sq_get_trans(sq, &status);
     BOOST_CHECK(rt != NULL);
-    BOOST_CHECK_EQUAL(fd1, rt->u.outfile.fd);
+    BOOST_CHECK_EQUAL(fd1+1, rt->u.outfile.fd);
 
     BOOST_CHECK_EQUAL(RIO_ERROR_EOF, sq_next(sq));
 
     sq_delete(sq);
-    rio_clear(&tracker);
-    
-    // tearDown: cleanup files after test
-    BOOST_CHECK_EQUAL(0, ::unlink("TESTOFS-000000.wrm"));
-    BOOST_CHECK_EQUAL(0, ::unlink("TESTOFS-000001.wrm"));
-    BOOST_CHECK_EQUAL(0, ::unlink("TESTOFS-000002.wrm"));
-    BOOST_CHECK_EQUAL(-1, ::unlink("TESTOFS-000003.wrm"));
 }
 
