15b4986704770ed998fbc88bb0715b550b58d2b9
Xiaopeng ZHOU <xiaopengzhou@hotmail.com>
2013-04-10 18:19:48 +0200

    CryptoInByMetaSequenceTransport. Fix reading of file encryption parameter.

--- transport/inbymetasequencetransport.hpp
+++ transport/inbymetasequencetransport.hpp
@@ -96,4 +96,80 @@ public:
     }
 };
 
+
+
+/**********************************
+* CryptoInByMetaSequenceTransport *
+**********************************/
+
+class CryptoInByMetaSequenceTransport : public Transport {
+public:
+    char path[1024];
+    unsigned begin_chunk_time;
+    unsigned end_chunk_time;
+    unsigned chunk_num;
+
+    RIO * rio;
+    SQ * seq;
+
+    CryptoInByMetaSequenceTransport(const char * filename, const char * extension)
+    : Transport()
+    {
+        memset(this->path, 0, sizeof(path));
+        this->begin_chunk_time = 0;
+        this->end_chunk_time = 0;
+        this->chunk_num = 0;
+
+        RIO_ERROR status = RIO_ERROR_OK;
+        SQ * seq = NULL;
+        this->rio = rio_new_cryptoinmeta(&status, &seq, filename, extension);
+        if (status != RIO_ERROR_OK){
+            throw Error(ERR_TRANSPORT);
+        }
+        this->seq = seq;
+    }
+
+    ~CryptoInByMetaSequenceTransport()
+    {
+        if (this->rio){
+            rio_delete(this->rio);
+        }
+    }
+
+    void next_chunk_info()
+    {
+        {
+            timeval tv_begin = {};
+            timeval tv_end = {};
+            RIO_ERROR status = sq_get_chunk_info(this->seq, &this->chunk_num, this->path, sizeof(this->path), &tv_begin, &tv_end);
+            if (status != RIO_ERROR_OK){
+                throw Error(ERR_TRANSPORT_READ_FAILED);
+            }
+            this->begin_chunk_time = tv_begin.tv_sec;
+            this->end_chunk_time = tv_end.tv_sec;
+        }
+        // if some error occurs calling sq_next 
+        // it will be took care of when opening next chunk, not now
+        sq_next(this->seq);
+    }
+
+    using Transport::recv;
+    virtual void recv(char ** pbuffer, size_t len) throw (Error)
+    {
+        ssize_t res = rio_recv(this->rio, *pbuffer, len);
+        if (res <= 0){
+            throw Error(ERR_TRANSPORT_READ_FAILED, errno);
+        }
+        *pbuffer += res;
+        if (res != (ssize_t)len){
+            throw Error(ERR_TRANSPORT_NO_MORE_DATA, errno);
+        }
+    }
+
+    using Transport::send;
+    virtual void send(const char * const buffer, size_t len) throw (Error) {
+        throw Error(ERR_TRANSPORT_INPUT_ONLY_USED_FOR_RECV, 0);
+    }
+};
+
 #endif
