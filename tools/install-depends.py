ed670059a2c68647d3ce93a168c760415ddd88bc
Christophe Grosjean <pykoder@gmail.com>
2013-05-16 18:20:48 +0200

    Added tool parsing control files to find dependencies (and install needed packages) (WIP)

---
+++ tools/install-depends.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import shutil
+import subprocess
+import os
+import re
+
+if __name__ == '__main__':
+
+try:
+    sections = []
+    section = {}
+    current = None
+    count = 0
+    for line in open("packaging/debian/squeeze/debian/control"):
+        res = re.match(r'^(?:([A-Za-z0-9-]+[:])\s*([^\r\n]+)\r*|(\s*)\r*$|([^\r\n]+)\r*)$', line)
+        if res.group(1):
+            current = res.group(1)
+            section[current] = res.group(2)
+        if current and res.group(4):
+            section[current] += res.group(4)
+        if res.group(3):
+            sections.append(section)
+            section = {}
+            current = None
+    sections.append(section)
+
+    for d in sections:
+        if d.get('Source:') == 'redemption':
+            os.system("apt-get install %s" % d.get('Build-depends:').split(r'\s*,\s*').join(' '))
+            exit(0)
+
+except Exception, e:
+    print "Dependency install failed %s" % e
+    exit(-1)
