d582aec5efe6265d133dbccdb36621aec4082d51
Christophe Grosjean <pykoder@gmail.com>
2013-06-04 12:40:11 +0200

    Fixed test_widget2_composite include of widget2_rect

--- tests/mod/internal/widget2/test_widget2_composite.cpp
+++ tests/mod/internal/widget2/test_widget2_composite.cpp
@@ -26,7 +26,7 @@
 #define LOGNULL
 #include "log.hpp"
 
-#include "internal/widget2/widget_rect.hpp"
+#include "internal/widget2/widget2_rect.hpp"
 #include "internal/widget2/widget2_composite.hpp"
 #include "png.hpp"
 #include "ssl_calls.hpp"
