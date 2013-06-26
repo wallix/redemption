5df22b9c5bd68b42122764ec2f8248d3e816703b
Christophe Grosjean <pykoder@gmail.com>
2013-06-26 06:15:14 +0200

    Fixed sockettransport test (was not testing sockettransport)

--- tests/transport/test_inbymetasequencetransport.cpp
+++ tests/transport/test_inbymetasequencetransport.cpp
@@ -37,110 +37,6 @@
 #include "error.hpp"
 
 
-//BOOST_AUTO_TEST_CASE(TestChainedMWRM)
-//{
-//    const char * input_filename = "./tests/fixtures/sample.mwrm";
-//    char buffer[60];
-//    char * begin = buffer;
-//    char * end = buffer;
-//    int fd = ::open(input_filename, O_RDONLY);
-//    if (fd == -1){
-//        LOG(LOG_ERR, "Open failed with error %s", strerror(errno));
-//        throw Error(ERR_TRANSPORT);
-//    }
-//    const char * expected[] = {
-//        "800 600\n",
-//        "0\n",
-//        "\n",
-//        "./tests/fixtures/sample0.wrm 1352304810 1352304870\n",
-//        "./tests/fixtures/sample1.wrm 1352304870 1352304930\n",
-//        "./tests/fixtures/sample2.wrm 1352304930 1352304990\n",
-//    };
-//    const size_t expected_len[] = {
-//        8,
-//        2,
-//        1,
-//        51,
-//        51,
-//        51,
-//    };
-//    const bool has_eol[] = {
-//        true,
-//        true,
-//        true,
-//        true,
-//        true,
-//        true,
-//    };
-
-
-//    char * eol = NULL;
-//    for (size_t i=0; i < 6 ; i++){
-//        bool res = readline(fd, &begin, &end, &eol, buffer, sizeof(buffer));
-//        BOOST_CHECK_EQUAL(expected_len[i], eol-begin);
-//        BOOST_CHECK_EQUAL(has_eol[i], res);
-//        BOOST_CHECK_EQUAL(0, memcmp(expected[i], begin, eol-begin));
-//        begin = eol;
-//    }
-//    ::close(fd);
-//}
-
-
-//BOOST_AUTO_TEST_CASE(TestChainedMWRMShortBuffer)
-//{
-//    const char * input_filename = "./tests/fixtures/sample.mwrm";
-//    char buffer[30];
-//    char * begin = buffer;
-//    char * end = buffer;
-//    int fd = ::open(input_filename, O_RDONLY);
-//    if (fd == -1){
-//        LOG(LOG_ERR, "Open failed with error %s", strerror(errno));
-//        throw Error(ERR_TRANSPORT);
-//    }
-//    const char * expected[] = {
-//        "800 600\n",
-//        "0\n",
-//        "\n",
-//        "./tests/fixtures/sample0.wrm 1",
-//        "352304810 1352304870\n",
-//        "./tests/fixtures/sample1.wrm 1",
-//        "352304870 1352304930\n",
-//        "./tests/fixtures/sample2.wrm 1",
-//        "352304930 1352304990\n",
-//    };
-//    const size_t expected_len[] = {
-//        8,
-//        2,
-//        1,
-//        30,
-//        21,
-//        30,
-//        21,
-//        30,
-//        21
-//    };
-//    const bool has_eol[] = {
-//        true,
-//        true,
-//        true,
-//        false,
-//        true,
-//        false,
-//        true,
-//        false,
-//        true,
-//    };
-
-//    char * eol = NULL;
-//    for (size_t i=0; i < 9 ; i++){
-//        bool res = readline(fd, &begin, &end, &eol, buffer, sizeof(buffer));
-//        BOOST_CHECK_EQUAL(expected_len[i], eol-begin);
-//        BOOST_CHECK_EQUAL(has_eol[i], res);
-//        BOOST_CHECK_EQUAL(0, memcmp(expected[i], begin, eol-begin));
-//        begin = eol;
-//    }
-//    ::close(fd);
-//}
 
 
 BOOST_AUTO_TEST_CASE(TestSequenceFollowedTransportWRM1)
@@ -157,7 +53,6 @@ BOOST_AUTO_TEST_CASE(TestSequenceFollowedTransportWRM1)
             total += pbuffer - buffer;
         }
     } catch (const Error & e) {
-        TODO("Is it the right exception ? This one occurs because at some point we do not have another file to provide in the sequence from which to get more data");
         BOOST_CHECK_EQUAL((unsigned)ERR_TRANSPORT_NO_MORE_DATA, (unsigned)e.id);
         total += pbuffer - buffer;
     };
