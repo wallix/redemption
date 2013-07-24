51ef006568c3d6656bd51aacd4f49cfe4be1e674
Meng Tan <mtan@wallix.com>
2013-07-24 17:07:13 +0200

    add description for break_interval key in video section of config

--- core/config.hpp
+++ core/config.hpp
@@ -807,11 +807,11 @@ public:
         this->video.capture_flv   = false;
         this->video.capture_ocr   = false;
 
-        this->video.ocr_interval    = 100; // 1 every second
+        this->video.ocr_interval    = 100;        // 1 every second
         this->video.png_interval    = 3000;
         this->video.capture_groupid = 33;
-        this->video.frame_interval  = 40;
-        this->video.break_interval  = 600;
+        this->video.frame_interval  = 40;         // 2,5 frame per second
+        this->video.break_interval  = 600;        // 10 minutes interval
         this->video.png_limit       = 3;
         strcpy(this->video.replay_path, "/tmp/");
 
