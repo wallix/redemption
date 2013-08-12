74bb8750978d2d95a03d4761b4aec7fe1d70416b
XiaopengZHOU <rzhou@wallix.com>
2013-08-12 13:11:31 +0200

    Fix some warnings.

--- core/RDP/share.hpp
+++ core/RDP/share.hpp
@@ -456,8 +456,8 @@ struct ShareData
                 LOG(LOG_INFO, "ShareData::recv_begin: got unexpected compressed share data");
             }
 
-            uint32_t  roff;
-            uint32_t  rlen;
+            uint32_t  roff = 0;
+            uint32_t  rlen = 0;
             decompress_rdp( dec, this->payload.get_data(), this->payload.size()
                           , this->compressedType, &roff, &rlen);
 
