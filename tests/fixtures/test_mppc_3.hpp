51a3c2fee6fe8f9c1013cf622603c0dd73a431e0
XiaopengZHOU <rzhou@wallix.com>
2013-08-12 15:40:48 +0200

    Merge all unit tests of RDP 5.0 compression in one file.

--- tests/fixtures/test_mppc_3.hpp
+++ tests/fixtures/test_mppc_3.hpp
@@ -1,35 +1,3 @@
-/*
-    This program is free software; you can redistribute it and/or modify it
-     under the terms of the GNU General Public License as published by the
-     Free Software Foundation; either version 2 of the License, or (at your
-     option) any later version.
-
-    This program is distributed in the hope that it will be useful, but
-     WITHOUT ANY WARRANTY; without even the implied warranty of
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-     Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-     with this program; if not, write to the Free Software Foundation, Inc.,
-     675 Mass Ave, Cambridge, MA 02139, USA.
-
-    Product name: redemption, a FLOSS RDP proxy
-    Copyright (C) Wallix 2013
-    Author(s): Christophe Grosjean, Raphael Zhou
-*/
-
-#define BOOST_AUTO_TEST_MAIN
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE TestMppc3
-#include <boost/test/auto_unit_test.hpp>
-
-#include "log.hpp"
-#define LOGNULL
-
-#include "RDP/mppc.hpp"
-
-BOOST_AUTO_TEST_CASE(TestRDP50BlukCompression3)
-{
 uint8_t historyBuffer[] = {
 /* 0000 */ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // ................
 /* 0010 */ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,  // ................
@@ -17792,38 +17760,3 @@ uint8_t compressed_data[] = {
 /* 22a0 */ 0x77, 0x00, 0x43, 0x88, 0x40, 0x6a, 0xa0, 0x13, 0xef, 0x19, 0x8a, 0x80, 0xea, 0xde, 0x2a, 0x00,  // w.C.@j........*.
 /* 22b0 */ 0x60, 0xf2, 0x30, 0x00, 0x24, 0x93, 0xec, 0x20, 0x60, 0x40, 0x1d, 0xa8, 0x00,           // `.0.$.. `@...
 };
-
-    rdp_mppc_enc * mppc_enc = new rdp_mppc_enc(PROTO_RDP_50);
-
-
-    BOOST_CHECK_EQUAL(sizeof(historyBuffer),     mppc_enc->buf_len);
-    BOOST_CHECK_EQUAL(sizeof(outputBufferPlus),  mppc_enc->buf_len + 64);
-    BOOST_CHECK_EQUAL(sizeof(hash_table),        mppc_enc->buf_len * 2);
-    BOOST_CHECK_EQUAL(sizeof(uncompressed_data), 12851);
-    BOOST_CHECK_EQUAL(sizeof(compressed_data),   8893);
-
-
-    mppc_enc->protocol_type = 2;
-    memcpy(mppc_enc->historyBuffer,    historyBuffer,    mppc_enc->buf_len);
-    memcpy(mppc_enc->outputBufferPlus, outputBufferPlus, mppc_enc->buf_len + 64);
-    mppc_enc->historyOffset = 0;
-    mppc_enc->buf_len       = 65536;
-    mppc_enc->bytes_in_opb  = 0;
-    mppc_enc->flags         = 0;
-    mppc_enc->flagsHold     = 0;
-    mppc_enc->first_pkt     = 1;
-    memcpy(mppc_enc->hash_table,       hash_table,       mppc_enc->buf_len * 2);
-
-
-    compress_rdp(mppc_enc, uncompressed_data, sizeof(uncompressed_data));
-
-    int flags = PACKET_COMPRESSED;
-
-    BOOST_CHECK_EQUAL(flags, (mppc_enc->flags & PACKET_COMPRESSED));
-
-    BOOST_CHECK_EQUAL(8893,  mppc_enc->bytes_in_opb);
-    BOOST_CHECK_EQUAL(0,     memcmp( compressed_data, mppc_enc->outputBuffer
-                                   , mppc_enc->bytes_in_opb));
-
-    delete(mppc_enc);
-}
