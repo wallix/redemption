bb836519178644ef8c43b6a9b0b8a6fa9d439304
Meng Tan <mtan@wallix.com>
2013-09-10 13:43:32 +0200

    Fix WidgetPassword cursor positionning on scrolling with mouse

--- mod/internal/widget2/password.hpp
+++ mod/internal/widget2/password.hpp
@@ -145,7 +145,7 @@ public:
                 }
             }
             else {
-                this->edit_pos = std::min<size_t>((x - this->dx() + this->masked_text.x_text - this->w_char/2) / this->w_char, this->num_chars-1);
+                this->edit_pos = std::min<size_t>((x - this->dx() - this->masked_text.x_text - this->w_char/2) / this->w_char, this->num_chars-1);
                 this->edit_buffer_pos = UTF8GetPos(reinterpret_cast<uint8_t*>(&this->label.buffer[0]), this->edit_pos);
             }
             if (e != this->edit_pos) {
