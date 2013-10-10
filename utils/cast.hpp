273fc4788014e5c5d017f686d29eb01daf3c1383
Meng Tan <mtan@wallix.com>
2013-10-10 18:24:21 +0200

    add missing cast.hpp file (again)

---
+++ utils/cast.hpp
@@ -0,0 +1,34 @@
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   Product name: redemption, a FLOSS RDP proxy
+   Copyright (C) Wallix 2012
+   Author(s): Christophe Grosjean, Raphael Zhou, Meng Tan
+
+*/
+
+#ifndef _REDEMPTION_UTILS_CAST_HPP_
+#define _REDEMPTION_UTILS_CAST_HPP_
+#include <stdint.h>
+
+static inline uint8_t * byte_ptr_cast(char * data) {
+    return reinterpret_cast<uint8_t *>(data);
+}
+
+static inline const uint8_t * byte_ptr_cast(const char * data) {
+    return reinterpret_cast<const uint8_t *>(data);
+}
+
+#endif
