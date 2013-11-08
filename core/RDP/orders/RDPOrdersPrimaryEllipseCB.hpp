8df296047a493e1de9c9e8af299aa0cf162f9201
Meng Tan <mtan@wallix.com>
2013-11-08 10:12:37 +0100

    Manage DELTA orders in EllipseCB

--- core/RDP/orders/RDPOrdersPrimaryEllipseCB.hpp
+++ core/RDP/orders/RDPOrdersPrimaryEllipseCB.hpp
@@ -180,11 +180,25 @@ public:
         if (!common.clip.contains(this->el.get_rect())){
             header.control |= BOUNDS;
         }
+
+        int16_t left      = this->el.left();
+        int16_t top       = this->el.top();
+        int16_t right     = this->el.right();
+        int16_t bottom    = this->el.bottom();
+        int16_t oldleft   = oldcmd.el.left();
+        int16_t oldtop    = oldcmd.el.top();
+        int16_t oldright  = oldcmd.el.right();
+        int16_t oldbottom = oldcmd.el.bottom();
+
+        header.control |= (is_1_byte(left - oldleft) &&
+                           is_1_byte(top - oldtop) &&
+                           is_1_byte(right - oldright) &&
+                           is_1_byte(bottom - oldbottom)) * RDP::DELTA;
         header.fields =
-            ( this->el.left()   != oldcmd.el.left()  ) * 0x0001
-            |(this->el.top()    != oldcmd.el.top()   ) * 0x0002
-            |(this->el.right()  != oldcmd.el.right() ) * 0x0004
-            |(this->el.bottom() != oldcmd.el.bottom()) * 0x0008
+            ( left              != oldleft           ) * 0x0001
+            |(top               != oldtop            ) * 0x0002
+            |(right             != oldright          ) * 0x0004
+            |(bottom            != oldbottom         ) * 0x0008
             |(this->brop2       != oldcmd.brop2      ) * 0x0010
             |(this->fill_mode   != oldcmd.fill_mode  ) * 0x0020
             |(this->back_color  != oldcmd.back_color ) * 0x0040
@@ -196,10 +210,10 @@ public:
             |(memcmp(this->brush.extra, oldcmd.brush.extra, 7) != 0) * 0x1000;
 
         common.emit(stream, header, oldcommon);
-        header.emit_coord(stream, 0x0001, this->el.left(),   oldcmd.el.left());
-        header.emit_coord(stream, 0x0002, this->el.top(),    oldcmd.el.top());
-        header.emit_coord(stream, 0x0004, this->el.right(),  oldcmd.el.right());
-        header.emit_coord(stream, 0x0008, this->el.bottom(), oldcmd.el.bottom());
+        header.emit_coord(stream, 0x0001, left,   oldleft);
+        header.emit_coord(stream, 0x0002, top,    oldtop);
+        header.emit_coord(stream, 0x0004, right,  oldright);
+        header.emit_coord(stream, 0x0008, bottom, oldbottom);
 
         if (header.fields & 0x0010) { stream.out_uint8(this->brop2); }
 
