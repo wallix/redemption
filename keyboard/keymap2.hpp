f30a05b3862ba9d3949d0cdbfa90c9267d10bb53
Raphael Zhou <rzhou@wallix.com>
2013-11-26 13:53:31 +0100

    Fixed disabling Ctrl+Shift-Esc keyboard sequence in front.

--- keyboard/keymap2.hpp
+++ keyboard/keymap2.hpp
@@ -236,9 +236,13 @@ struct Keymap2
         this->keys_down[extendedKeyCode] = !(keyboardFlags & KBDFLAGS_RELEASE);
 
         ctrl_alt_del =
-            (this->keys_down[LEFT_CTRL] || this->keys_down[RIGHT_CTRL]) &&  // Ctrl
-            (this->keys_down[LEFT_ALT] || this->keys_down[RIGHT_ALT]) &&    // Alt
-            (this->keys_down[0xD3] || this->keys_down[0x53] || this->keys_down[0x01]);  // Del, Numpad del or Escape
+            (this->keys_down[LEFT_CTRL] || this->keys_down[RIGHT_CTRL]) &&      // Ctrl
+            (this->keys_down[LEFT_ALT] || this->keys_down[RIGHT_ALT]) &&        // Alt
+            (this->keys_down[0xD3] || this->keys_down[0x53]);                   // Del (or Numpad del)
+        ctrl_alt_del |=
+            (this->keys_down[LEFT_CTRL] || this->keys_down[RIGHT_CTRL]) &&      // Ctrl
+            (this->keys_down[LEFT_SHIFT] || this->keys_down[RIGHT_SHIFT]) &&    // Shift
+            this->keys_down[0x01];                                              // Escape
 
         if (is_ctrl_pressed() && is_alt_pressed()
         && ((extendedKeyCode == 207)||(extendedKeyCode == 83))){
