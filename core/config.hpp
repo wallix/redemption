d0ace644764781b2d188e6c9f18a06c76e0152a8
Raphael Zhou <rzhou@wallix.com>
2014-01-07 16:20:35 +0100

    Changed defaut crypto key.

--- core/config.hpp
+++ core/config.hpp
@@ -846,10 +846,10 @@ public:
         this->globals.enable_bitmap_update = false;
 
         memcpy(this->globals.crypto_key,
-            "\0\0\0\0\0\0\0\0"
-            "\0\0\0\0\0\0\0\0"
-            "\0\0\0\0\0\0\0\0"
-            "\0\0\0\0\0\0\0\0",
+            "\x00\x01\x02\x03\x04\x05\x06\x07"
+            "\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"
+            "\x10\x11\x12\x13\x14\x15\x16\x17"
+            "\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F",
             sizeof(this->globals.crypto_key));
         // End Init globals
 
