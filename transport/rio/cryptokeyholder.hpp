d364a5ddeee08e73f98f2a9cea35b665112a43b8
Raphael Zhou <rzhou@wallix.com>
2014-01-30 17:30:13 +0100

    Added missing file 'cryptokeyholder.hpp'.

---
+++ transport/rio/cryptokeyholder.hpp
@@ -0,0 +1,52 @@
+/*
+    This program is free software; you can redistribute it and/or modify it
+     under the terms of the GNU General Public License as published by the
+     Free Software Foundation; either version 2 of the License, or (at your
+     option) any later version.
+
+    This program is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+     Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+     with this program; if not, write to the Free Software Foundation, Inc.,
+     675 Mass Ave, Cambridge, MA 02139, USA.
+
+    Product name: redemption, a FLOSS RDP proxy
+    Copyright (C) Wallix 2013
+    Author(s): Christophe Grosjean, Raphael Zhou
+*/
+
+#ifndef _REDEMPTION_TRANSPORT_RIO_CRYPTO_KEY_HOLDER_H_
+#define _REDEMPTION_TRANSPORT_RIO_CRYPTO_KEY_HOLDER_H_
+
+struct crypto_key_holder {
+    crypto_key_holder() {}
+
+    virtual ~crypto_key_holder() {}
+
+    virtual const char * get_key_0() const {
+        static const char key[] = {
+            "\x00\x01\x02\x03\x04\x05\x06\x07"
+            "\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"
+            "\x10\x11\x12\x13\x14\x15\x16\x17"
+            "\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F"
+        };
+
+        return key;
+    }
+
+    virtual const char * get_key_1() const {
+        static const char key[] = {
+            "\x00\x01\x02\x03\x04\x05\x06\x07"
+            "\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"
+            "\x10\x11\x12\x13\x14\x15\x16\x17"
+            "\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F"
+        };
+
+        return key;
+    }
+};
+
+#endif  // #ifndef _REDEMPTION_TRANSPORT_RIO_CRYPTO_KEY_HOLDER_H_
