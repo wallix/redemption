57e0fdf14e42e41b2788fdc53524f0c6955aad94
Meng Tan <mtan@wallix.com>
2014-05-16 14:44:42 +0200

    Packager: replace any variable surrounded by % in template files

--- tools/packager.py
+++ tools/packager.py
@@ -14,17 +14,13 @@ import platform
 def usage():
   print("Usage: %s [-h|--help] --update-version version [--no-entry-changelog] [--no-git-commit] [--git-tag] [--git-push-tag] | --build-package [--package-distribution name] [--force-target target] [--debug]" % sys.argv[0])
 
-# [--prefix path] [--etc-prefix path] [--cert-prefix path]
-# [--force-distro name] [--force-distro-codename name]
 try:
   options, args = getopt.getopt(sys.argv[1:], "h",
-                             ["help", "update-version=", "build-package",
-                              "no-entry-changelog",
-                              "no-git-commit", "git-tag", "git-push-tag",
-                              # "prefix=", "etc-prefix=", "cert-prefix=",
-                              "package-distribution=",
-                              # "force-distro=", "force-distro-codename=",
-                              "debug", "force-target="])
+                                ["help", "update-version=", "build-package",
+                                 "no-entry-changelog",
+                                 "no-git-commit", "git-tag", "git-push-tag",
+                                 "package-distribution=",
+                                 "debug", "force-target="])
 except getopt.GetoptError as err:
   print(str(err))
   usage()
@@ -33,24 +29,27 @@ except getopt.GetoptError as err:
 class opts(object):
   tag = None
   target_param_path = "packaging/targets"
-  force_target = None
   packagetemp = "packaging/template/debian"
-  prefix = 'usr/local'
-  etc_prefix = 'etc/rdpproxy'
-  cert_prefix = 'etc/rdpproxy/cert'
-  package_distribution = 'unstable'
-  archi = 'any'
+  force_target = None
+
   debug = False
   entry_changelog = True
-  force_distro = None
-  force_distro_release = None
-  force_distro_codename = None
   git_commit = True
   git_tag = False
   git_push_tag = False
 
   update_version = False
   build_package = False
+
+  config = {}
+  config["%PREFIX%"] = 'usr/local'
+  config["%ETC_PREFIX%"] = 'etc/rdpproxy'
+  config["%CERT_PREFIX%"] = 'etc/rdpproxy/cert'
+  config["%RECORD_PATH%"] = '/var/rdpproxy/recorded'
+  config["%RECORD_TMP_PATH%"] = '/var/rdpproxy/tmp'
+  config["%PKG_DISTRIBUTION%"] = 'unstable'
+  config["%ARCHI%"] = 'any'
+  config["%REDEMPTION_USER%"] = ''
 
 for o,a in options:
   if o in ("-h", "--help"):
@@ -70,25 +69,12 @@ for o,a in options:
   elif o == "--git-push-tag":
     opts.git_push_tag = True
   elif o == "--package-distribution":
-    opts.package_distribution = a
-  # elif o == "--prefix":
-  #   opts.prefix = a
-  # elif o == "--etc-prefix":
-  #   opts.etc_prefix = a
-  # elif o == "--cert-prefix":
-  #   opts.cert_prefix = a
-  # elif o == "--force-distro":
-  #   opts.force_distro = a
-  # elif o == "--force-distro-codename":
-  #   opts.force_distro_codename = a
+    opts.config["%PKG_DISTRIBUTION%"] = a
   elif o == "--debug":
     opts.debug = True
   elif o == "--force-target":
     opts.force_target = a
 
-# if ((not opts.build_package) and
-#     ((not opts.update_version) or
-#      (not opts.tag))):
 if not (opts.build_package or (opts.update_version and opts.tag)):
   usage()
   sys.exit(1)
@@ -114,158 +100,140 @@ def copy_and_replace(src, dst, old, new):
   out = out.replace(old, new)
   writeall(dst, out)
 
-rgx_split = re.compile("\s+")
-rgx_if = re.compile("^\s*@if\s+")
-rgx_else = re.compile("^\s*@else\s*")
-rgx_endif = re.compile("^\s*@endif\s*$")
-rgx_elif = re.compile("^\s*@elif\s+")
+# rgx_split = re.compile("\s+")
+# rgx_if = re.compile("^\s*@if\s+")
+# rgx_else = re.compile("^\s*@else\s*")
+# rgx_endif = re.compile("^\s*@endif\s*$")
+# rgx_elif = re.compile("^\s*@elif\s+")
 
-def _test_condition(filename, num, l, vars):
-  a = re.split(rgx_split, l)
-  if len(a) >= 2:
-    idx = 0;
-    if not a[0]:
-      idx = 1
-    if a[idx] == 'not':
-      test = False
-      idx += 1
-    else:
-      test = True
-    if idx:
-      a = a[idx:]
+# def _test_condition(filename, num, l, vars):
+#   a = re.split(rgx_split, l)
+#   if len(a) >= 2:
+#     idx = 0;
+#     if not a[0]:
+#       idx = 1
+#     if a[idx] == 'not':
+#       test = False
+#       idx += 1
+#     else:
+#       test = True
+#     if idx:
+#       a = a[idx:]
 
-  if len(a) < 2 or not a[1]:
-    raise Exception("conditional error in '%s' at line %s" % (filename, num))
+#   if len(a) < 2 or not a[1]:
+#     raise Exception("conditional error in '%s' at line %s" % (filename, num))
 
-  if a[0] in ['version-less', 'version-or-less']:
-    if not (len(a) == 2 or (len(a) == 3 and a[2] == '')):
-      raise Exception("%s too many argument in '%s' at line %s" % (a[0], filename, num))
-    if a[0] == 'version-less':
-      return (vars['version'] < float(a[1])) == test
-    return (vars['version'] <= float(a[1])) == test
+#   if a[0] in ['version-less', 'version-or-less']:
+#     if not (len(a) == 2 or (len(a) == 3 and a[2] == '')):
+#       raise Exception("%s too many argument in '%s' at line %s" % (a[0], filename, num))
+#     if a[0] == 'version-less':
+#       return (vars['version'] < float(a[1])) == test
+#     return (vars['version'] <= float(a[1])) == test
 
-  if not a[0] in vars:
-    raise Exception("test '%s' unknow in '%s' at line %s" % (a[0], filename, num))
-  x = vars[a[0]]
-  return (x and x in a[1:]) == test
-
-
-def _parse_template(filename, num, f, vars, rec = 0, stateif = False, addtext = True):
-  out = ''
-  stateelse = False
-  for l in f:
-    num += 1
-    m = re.search(rgx_if, l)
-    if m:
-      cond = _test_condition(filename, num, l[m.end()-1:], vars)
-      if stateif or stateelse:
-        num,ret = _parse_template(filename, num, f, vars, rec+1, True, cond)
-        out += ret
-      else:
-        stateif = True
-        addtext = cond
-    else:
-      m = re.search(rgx_else, l)
-      if m:
-        if not stateif or stateelse:
-          raise Exception("else without if in '%s' at line %s" % (filename, num))
-        addtext = not addtext
-        stateelse = True
-        stateif = False
-      else:
-        m = re.search(rgx_elif, l)
-        if m:
-          if not stateif or stateelse:
-            raise Exception("elif without if in '%s' at line %s" % (filename, num))
-          addtext = _test_condition(filename, num, l[m.end()-1:], vars)
-        else:
-          m = re.search(rgx_endif, l)
-          if m:
-            if not stateif and not stateelse:
-              raise Exception("endif without if in '%s' at line %s" % (filename, num))
-            if rec:
-              return num,out
-            addtext = True
-            stateelse = False
-            stateif = False
-          elif addtext:
-            out += l
-  if stateif or stateelse:
-    raise Exception("if without endif in '%s' at line %s" % (filename, num))
-  return num,out
+#   if not a[0] in vars:
+#     raise Exception("test '%s' unknow in '%s' at line %s" % (a[0], filename, num))
+#   x = vars[a[0]]
+#   return (x and x in a[1:]) == test
 
 
-def parse_template(filename, distro, codename, version, arch):
-  vars = {'dist': distro, 'codename':codename, 'version': float(version), 'arch': arch}
-  with open(filename) as f:
-    num,out = _parse_template(filename, 0, f, vars)
-    return out
-  return ''
-rgx_prefix = re.compile("^\s*PREFIX=\s*(.*)$")
-rgx_etc_prefix = re.compile("^\s*ETC_PREFIX=\s*(.*)$")
-rgx_cert_prefix = re.compile("^\s*CERT_PREFIX=\s*(.*)$")
-rgx_archi = re.compile("^\s*ARCHI=\s*(.*)$")
-rgx_build_depends = re.compile("^\s*BUILD_DEPENDS=\s*(.*)$")
-rgx_depends = re.compile("^\s*DEPENDS=\s*(.*)$")
-rgx_distro = re.compile("^\s*DISTRO=\s*(.*)$")
-rgx_distro_release = re.compile("^\s*DISTRO_RELEASE=\s*(.*)$")
-rgx_distro_codename = re.compile("^\s*DISTRO_CODENAME=\s*(.*)$")
-
-def _parse_target_param(fl):
-  for l in fl:
-    m = re.search(rgx_prefix, l)
-    if m:
-      opts.prefix = m.group(1)
-      continue
-    m = re.search(rgx_etc_prefix, l)
-    if m:
-      opts.etc_prefix = m.group(1)
-      continue
-    m = re.search(rgx_cert_prefix, l)
-    if m:
-      opts.cert_prefix = m.group(1)
-      continue
-    m = re.search(rgx_archi, l)
-    if m:
-      opts.archi = m.group(1)
-      continue
-    m = re.search(rgx_build_depends, l)
-    if m:
-      opts.build_depends = m.group(1)
-      continue
-    m = re.search(rgx_depends, l)
-    if m:
-      opts.depends = m.group(1)
-      continue
-    m = re.search(rgx_distro, l)
-    if m:
-      opts.force_distro = m.group(1)
-      continue
-    m = re.search(rgx_distro_release, l)
-    if m:
-      opts.force_distro_release = m.group(1)
-      continue
-    m = re.search(rgx_distro_codename, l)
-    if m:
-      opts.force_distro_codename = m.group(1)
-      continue
+# def _parse_template(filename, num, f, vars, rec = 0, stateif = False, addtext = True):
+#   out = ''
+#   stateelse = False
+#   for l in f:
+#     num += 1
+#     m = re.search(rgx_if, l)
+#     if m:
+#       cond = _test_condition(filename, num, l[m.end()-1:], vars)
+#       if stateif or stateelse:
+#         num,ret = _parse_template(filename, num, f, vars, rec+1, True, cond)
+#         out += ret
+#       else:
+#         stateif = True
+#         addtext = cond
+#     else:
+#       m = re.search(rgx_else, l)
+#       if m:
+#         if not stateif or stateelse:
+#           raise Exception("else without if in '%s' at line %s" % (filename, num))
+#         addtext = not addtext
+#         stateelse = True
+#         stateif = False
+#       else:
+#         m = re.search(rgx_elif, l)
+#         if m:
+#           if not stateif or stateelse:
+#             raise Exception("elif without if in '%s' at line %s" % (filename, num))
+#           addtext = _test_condition(filename, num, l[m.end()-1:], vars)
+#         else:
+#           m = re.search(rgx_endif, l)
+#           if m:
+#             if not stateif and not stateelse:
+#               raise Exception("endif without if in '%s' at line %s" % (filename, num))
+#             if rec:
+#               return num,out
+#             addtext = True
+#             stateelse = False
+#             stateif = False
+#           elif addtext:
+#             out += l
+#   if stateif or stateelse:
+#     raise Exception("if without endif in '%s' at line %s" % (filename, num))
+#   return num,out
 
 
+# def parse_template(filename, distro, codename, version, arch):
+#   vars = {'dist': distro, 'codename':codename, 'version': float(version), 'arch': arch}
+#   with open(filename) as f:
+#     num,out = _parse_template(filename, 0, f, vars)
+#     return out
+#   return ''
+
+
+# Parse target Config files
+rgx_general = re.compile("^([^#]\S*)\s*=\s*(.*)$")
+def parse_config_line(line):
+  m = re.search(rgx_general, line.strip())
+  if m:
+    opts.config['%%%s%%' % m.group(1).strip()] = m.group(2).strip()
 
 def parse_target_param(filename):
   with open(filename) as f:
-    _parse_target_param(f)
+    for l in f:
+      parse_config_line(l)
 # IO Files functions END
 
-# Check uncommited changes BEGIN
-def check_uncommited_changes():
-  res = subprocess.Popen(["git", "diff", "--shortstat"],
-                         stdout = subprocess.PIPE,
-                         stderr = subprocess.STDOUT
-                        ).communicate()[0]
-  if res:
-    raise Exception('your repository has uncommited changes:\n%sPlease commit before packaging.' % (res))
-# Check uncommited changes END
+# Replace occurences in templates with dictionnaire
+rgx_var = re.compile("(%[A-Z][A-Z0-9_]*%)")
+
+def replace_dict_line(line, dico):
+  res = line
+  miter = rgx_var.finditer(res)
+  for m in reversed(list(miter)):
+    value = dico.get(m.group(1))
+    if not value:
+      value = ''
+    res = res[:m.start(1)] + value + res[m.end(1):]
+  return res
+
+def replace_dict_file(filename, dico):
+  res = ''
+  with open(filename) as f:
+    for l in f:
+      res += replace_dict_line(l, dico)
+  return res
+
+def copy_and_replace_dict_file(filename, dico, target):
+  out = replace_dict_file(filename, dico)
+  writeall(target, out)
+
+# Unused
+# def export_var(key):
+#   res = ""
+#   value = opts.config["%%%s%%" % key.upper()]
+#   if value:
+#     res = "export %s=%s" % (key.upper(), value)
+#   return res
+
 
 # UPDATE VERSION FUNCTIONS
 def update_version_file(newtag):
@@ -291,6 +259,16 @@ def update_changelog_template(newtag):
   changelog += readall("%s/changelog" % opts.packagetemp)
   writeall("%s/changelog" % opts.packagetemp, changelog)
 
+# CHECKERS
+# Check uncommited changes BEGIN
+def check_uncommited_changes():
+  res = subprocess.Popen(["git", "diff", "--shortstat"],
+                         stdout = subprocess.PIPE,
+                         stderr = subprocess.STDOUT
+                         ).communicate()[0]
+  if res:
+    raise Exception('your repository has uncommited changes:\n%sPlease commit before packaging.' % (res))
+# Check uncommited changes END
 # Check tag version BEGIN
 def check_new_tag_version_with_local_and_remote_tags(newtag):
   locale_tags = subprocess.Popen(["git", "tag", "--list"],
@@ -311,7 +289,6 @@ def check_new_tag_version_with_local_and_remote_tags(newtag):
     raise Exception('tag %s already exists (remote).' % newtag)
 # Check tag version END
 
-# BUILD FUNCTIONS
 # Check matching versions BEGIN
 def check_matching_version_changelog():
   found = False
@@ -363,47 +340,30 @@ def check_last_version_commited_match_current_version(version):
     raise Exception('Repository head mismatch current version ("%s" != "%s"), please tag current version before building packet' % (version, tag_describe))
 # Check last version tag commited match current version tag END
 
+# INFO GETTERS
+def get_device_architecture():
+  res = platform.machine()
+  if res:
+    return res
+  raise Exception('Device architecture not found')
+
 def archi_to_control_archi(architecture):
   if architecture == 'x86_64':
     return 'amd64'
   if architecture == 'i386':
     return architecture
+  if architecture == 'i686':
+    return architecture
   return 'any'
 
-def get_device_architecture():
-  res = platform.machine()
-  if res:
-    return res
-  # res = subprocess.Popen(["lscpu"], stdout=subprocess.PIPE, stderr = subprocess.STDOUT).communicate()[0]
-  # for l in res.split('\n'):
-  #   m = re.match('^Architecture.*:.*(x86_64|i386)', l)
-  #   if m:
-  #     return m.group(1)
-  raise Exception('Device architecture not found')
 
 status = 0
 remove_diff = False
 try:
-  # TESTS
-  # if opts.force_target:
-  #   try:
-  #     parse_target_param("%s/%s" % (opts.target_param_path, opts.force_target))
-  #   except IOError:
-  #     raise Exception('Target param file not found (%s/%s)' %
-  #                     (opts.target_param_path, opts.force_target))
-  #   print opts.prefix
-  #   print opts.etc_prefix
-  #   print opts.cert_prefix
-  #   print opts.archi
-  #   print opts.build_depends
-  #   print opts.depends
-  #   device_archi = get_device_architecture()
-  #   if not ((opts.archi == 'any') or (device_archi == opts.archi)):
-  #     print 'Target architecture (%s) does not match current device architecture (%s)' % (opts.archi, device_archi)
-  #   exit(0)
 
   check_uncommited_changes()
   remove_diff = True
+
   if (opts.update_version and opts.tag):
     # check tag does not exist
     check_new_tag_version_with_local_and_remote_tags(opts.tag)
@@ -429,6 +389,7 @@ try:
   if opts.build_package:
     opts.tag = check_matching_version_changelog()
     check_last_version_commited_match_current_version(opts.tag)
+
     # Set debian (packaging data) directory with distro specific packaging files BEGIN
     # Create temporary directory
     os.mkdir("debian", 0766)
@@ -443,37 +404,35 @@ try:
 
     # check Distro
     distro, distro_release, distro_codename = distroinfo.get_distro()
-    if opts.force_distro != None:
-      distro = opts.force_distro
-    if opts.force_distro_release != None:
-      distro_release = opts.force_distro_release
-    if opts.force_distro_codename != None:
-      distro_codename = opts.force_distro_codename
+    if opts.config.get("%DISTRO%"):
+      distro = opts.config.get("%DISTRO%")
+    if opts.config.get("%DISTRO_RELEASE%"):
+      distro_release = opts.config["%DISTRO_RELEASE%"]
+    if opts.config.get("%DISTRO_CODENAME%"):
+      distro_codename = opts.config["%DISTRO_CODENAME%"]
 
     # Check Architecture
     device_archi = get_device_architecture()
-    if not ((opts.archi == 'any') or (device_archi == opts.archi)):
-      raise Exception('Target architecture (%s) does not match current device architecture (%s)' % (opts.archi, device_archi))
-    opts.archi = device_archi
+    if not ((opts.config["%ARCHI%"] == 'any') or (device_archi == opts.config["%ARCHI%"])):
+      raise Exception('Target architecture (%s) does not match current device architecture (%s)' % (opts.config["%ARCHI%"], device_archi))
+    opts.config["%ARCHI%"] = archi_to_control_archi(device_archi)
 
     # write redemption.install file
-    install_file = readall("%s/redemption.install" % opts.packagetemp)
-    install_file = install_file.replace('%prefix', opts.prefix)
-    install_file = install_file.replace('%etc_prefix', opts.etc_prefix)
-    writeall("debian/redemption.install", install_file)
+    copy_and_replace_dict_file("%s/redemption.install" % opts.packagetemp,
+                               opts.config,
+                               "debian/redemption.install")
 
-    env = 'PREFIX="%s"\nETC_PREFIX="%s"\nCERT_PREFIX="%s"' % (opts.prefix,
-                                                              opts.etc_prefix,
-                                                              opts.cert_prefix)
     # write redemption.postinst
-    copy_and_replace("%s/redemption.postinst" % opts.packagetemp,
-                     "debian/redemption.postinst", '%setenv', env)
+    copy_and_replace_dict_file("%s/redemption.postinst" % opts.packagetemp,
+                               opts.config,
+                               "debian/redemption.postinst")
 
     # write rules
     if opts.debug:
-      env += "\nBJAM_EXTRA_INSTALL=debug"
-    copy_and_replace("%s/rules" % opts.packagetemp, "debian/rules", '%setenv', env)
-
+      opts.config["%DEBUG_OPT%"] = "debug"
+    copy_and_replace_dict_file("%s/rules" % opts.packagetemp,
+                               opts.config,
+                               "debian/rules")
     # write changelog
     target = ''
     if distro == 'ubuntu':
@@ -481,17 +440,17 @@ try:
       target += distro_codename
     else:
       # debian codename
-      opts.package_distribution = distro_codename
-    changelog = readall("%s/changelog" % opts.packagetemp)
-    writeall("debian/changelog",
-             changelog.replace('%target_name',
-                               target).replace('%pkg_distribution',
-                                               opts.package_distribution))
+      opts.config["%PKG_DISTRIBUTION%"] = distro_codename
+    opts.config["%TARGET_NAME%"] = target
+    copy_and_replace_dict_file("%s/changelog" % opts.packagetemp,
+                               opts.config,
+                               "debian/changelog")
 
     # write control
-    out = parse_template("%s/control" % opts.packagetemp,
-                         distro, distro_codename, distro_release, distroinfo.get_arch())
-    writeall("debian/control", out.replace('%REDEMPTION_VERSION', opts.tag).replace('%ARCHI', archi_to_control_archi(opts.archi)))
+    opts.config["%REDEMPTION_VERSION%"] = opts.tag
+    copy_and_replace_dict_file("%s/control" % opts.packagetemp,
+                               opts.config,
+                               "debian/control")
 
     # write compat
     shutil.copy("%s/compat" % opts.packagetemp, "debian/compat")
