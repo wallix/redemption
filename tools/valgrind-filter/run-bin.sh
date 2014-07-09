9e437861b3df40dc7fe55a91eb97bcfbac58dc38
jonathanpoelen <jonathan.poelen@gmail.com>
2014-07-09 17:44:05 +0200

    upgrade valgrind-filter

--- tools/valgrind-filter/run-bin.sh
+++ tools/valgrind-filter/run-bin.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-[ -z "$1" ] && echo "Usage: $0 bin" >&2 return 1
+[ -z "$1" ] && echo "Usage: $0 valgrind-arguments" >&2 return 1
 
-valgrind --max-stackframe=3595736 --leak-check=full --show-reachable=yes "$1" \
-|& "$(dirname "$0")"/valgrind-filter-ssl \
-| grep -C1000 -P '\w+\.\w+:\d+|total heap usage:' --color=always
+dir="`dirname "$0"`"
+valgrind \
+    --leak-check=full \
+    --track-fds=yes \
+    --show-reachable=yes \
+    --suppressions="$dir"/valgrind-ignore-ssl-snappy \
+    "$@" \
+|& "$dir"/valgrind-output-color
