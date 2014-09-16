ca92281f7ec1f66aff8f45e55bb919cbf6291689
Raphael Zhou <rzhou@wallix.com>
2014-09-16 17:57:42 +0200

    Refactoring bitmap compression for Bitmap Update.

--- core/RDP/RDPGraphicDevice.hpp
+++ core/RDP/RDPGraphicDevice.hpp
@@ -126,4 +126,21 @@ public:
     virtual ~RDPCaptureDevice() {}
 };
 
+void compress_and_draw_bitmap_update( const RDPBitmapData & bitmap_data, const Bitmap & bmp
+                                    , uint8_t target_bpp, RDPGraphicDevice & gd) {
+    REDASSERT(!(bitmap_data.flags & BITMAP_COMPRESSION));
+
+    BStream bmp_stream(65535);
+    bmp.compress(target_bpp, bmp_stream);
+    bmp_stream.mark_end();
+
+    RDPBitmapData target_bitmap_data = bitmap_data;
+
+    target_bitmap_data.bits_per_pixel = bmp.bpp();
+    target_bitmap_data.flags          = BITMAP_COMPRESSION | NO_BITMAP_COMPRESSION_HDR;
+    target_bitmap_data.bitmap_length  = bmp_stream.size();
+
+    gd.draw(target_bitmap_data, bmp_stream.get_data(), bmp_stream.size(), bmp);
+}
+
 #endif
