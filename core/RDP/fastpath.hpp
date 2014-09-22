0354a0b1c6437b526703dd9c2c4da19cb857eae1
Christophe Grosjean <pykoder@gmail.com>
2014-09-22 15:35:02 +0200

    Fixing Fastpath RecvInputEvent before SubStream FastPath removal

--- core/RDP/fastpath.hpp
+++ core/RDP/fastpath.hpp
@@ -184,41 +184,39 @@ namespace FastPath {
 //  it is present).
 
     struct ClientInputEventPDU_Recv {
+        uint8_t   fpInputHeader;
+        uint8_t   action;
+        uint8_t   numEvents;
         uint8_t   secFlags;
+        uint16_t  length;
         uint32_t  fipsInformation;
         uint8_t   dataSignature[8];
         SubStream payload;
-        uint8_t   numEvents;
 
         ClientInputEventPDU_Recv(Stream & stream, CryptContext & decrypt)
-        : secFlags(0)
-        , fipsInformation(0)
-        , dataSignature{}
-        , payload(stream) 
-        , numEvents(0)
-        {
-            uint8_t byte = stream.in_uint8();
-            int action = byte & 0x03;
+        : fpInputHeader(stream.in_uint8())
+        , action([this](){
+            uint8_t action = this->fpInputHeader & 0x03;
             if (action != 0) {
                 LOG(LOG_ERR, "Fast-path PDU expected: action=0x%X", action);
                 throw Error(ERR_RDP_FASTPATH);
             }
-
-            this->numEvents = (byte & 0x3C) >> 2;// ????
-            this->secFlags  = (byte & 0xC0) >> 6;
-
-
+            return action;
+        }())
+        , numEvents((this->fpInputHeader & 0x3C) >> 2)
+        , secFlags((this->fpInputHeader & 0xC0) >> 6)
+        , length([&stream](){
              uint16_t length = stream.in_uint8();
              if (length & 0x80){
                  length = (length & 0x7F) << 8 | stream.in_uint8();
              }
+             return length;
+        }())
+        , fipsInformation(0)
+        , dataSignature{}
+        , payload(stream) 
+        {
 
-            if (length != stream.size()){
-                LOG( LOG_ERR
-                   , "FastPath::ClientInputEventPDU_Recv: inconsistent length in header (length=%u)"
-                   , length);
-                throw Error(ERR_RDP_FASTPATH);
-            }
             TODO("RZ: Should we treat fipsInformation ?");
 
             if (this->secFlags & FASTPATH_INPUT_ENCRYPTED) {
