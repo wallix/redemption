4549c0dd5f5433d1d5777e335c9454122330b524
Raphael Zhou <rzhou@wallix.com>
2014-12-26 09:40:11 +0100

    Fixed unit test of rdp client of WAB.

--- tests/client_mods/test_rdp_client_wab.cpp
+++ tests/client_mods/test_rdp_client_wab.cpp
@@ -1,24 +1,24 @@
 /*
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
+    This program is free software; you can redistribute it and/or modify it
+     under the terms of the GNU General Public License as published by the
+     Free Software Foundation; either version 2 of the License, or (at your
+     option) any later version.
 
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-   GNU General Public License for more details.
+    This program is distributed in the hope that it will be useful, but
+     WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+     Public License for more details.
 
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+    You should have received a copy of the GNU General Public License along
+     with this program; if not, write to the Free Software Foundation, Inc.,
+     675 Mass Ave, Cambridge, MA 02139, USA.
 
-   Product name: redemption, a FLOSS RDP proxy
-   Copyright (C) Wallix 2010
-   Author(s): Christophe Grosjean, Javier Caverni
-   Based on xrdp Copyright (C) Jay Sorg 2004-2010
+    Product name: redemption, a FLOSS RDP proxy
+    Copyright (C) Wallix 2013
+    Author(s): Christophe Grosjean, Javier Caverni, Raphael Zhou
+    Based on xrdp Copyright (C) Jay Sorg 2004-2010
 
-   Unit test to writing RDP orders to file and rereading them
+    Unit test to writing RDP orders to file and rereading them
 */
 
 #define BOOST_AUTO_TEST_MAIN
@@ -26,15 +26,19 @@
 #define BOOST_TEST_MODULE TestRdpClientWab
 #include <boost/test/auto_unit_test.hpp>
 
+// Comment the code block below to generate testing data.
 #define LOGNULL
+// Uncomment the code block below to generate testing data.
 //#define LOGPRINT
-#include "check_sig.hpp"
 
+#include "check_sig.hpp"
 #include "test_transport.hpp"
 #include "client_info.hpp"
 #include "rdp/rdp.hpp"
-
 #include "../front/fake_front.hpp"
+// Uncomment the code block below to generate testing data.
+//#include "netutils.hpp"
+//#include "socket_transport.hpp"
 
 BOOST_AUTO_TEST_CASE(TestDecodePacket)
 {
@@ -51,18 +55,20 @@ BOOST_AUTO_TEST_CASE(TestDecodePacket)
                                  | PERF_DISABLE_FULLWINDOWDRAG | PERF_DISABLE_MENUANIMATIONS;
     FakeFront front(info, verbose);
 
-    const char * name       = "RDP Wab Target";
+    const char * name = "RDP Wab Target";
 
-    // int                 client_sck = ip_connect("10.10.47.32", 3389, 3, 1000, verbose);
-    // redemption::string  error_message;
-    // SocketTransport     t( name
-    //                      , client_sck
-    //                      , "10.10.47.32"
-    //                      , 3389
-    //                      , verbose
-    //                      , &error_message
-    //                      );
+    // Uncomment the code block below to generate testing data.
+    //int             client_sck = ip_connect("10.10.47.86", 3389, 3, 1000, verbose);
+    //std::string     error_message;
+    //SocketTransport t( name
+    //                , client_sck
+    //                , "10.10.47.86"
+    //                , 3389
+    //                , verbose
+    //                , &error_message
+    //                );
 
+    // Comment the code block below to generate testing data.
     #include "fixtures/dump_wab.hpp"
     TestTransport t(name, indata, sizeof(indata), outdata, sizeof(outdata), verbose);
 
@@ -74,7 +80,7 @@ BOOST_AUTO_TEST_CASE(TestDecodePacket)
 
     ModRDPParams mod_rdp_params( "x"
                                , "x"
-                               , "10.10.47.32"
+                               , "10.10.47.86"
                                , "192.168.1.100"
                                , 7
                                , 511
@@ -96,7 +102,7 @@ BOOST_AUTO_TEST_CASE(TestDecodePacket)
 
     // To always get the same client random, in tests
     LCGRandom gen(0);
-    mod_rdp mod_(t, front, info, gen, mod_rdp_params);
+    mod_rdp   mod_(t, front, info, gen, mod_rdp_params);
     mod_api * mod = &mod_;
 
     if (verbose > 2) {
@@ -107,18 +113,18 @@ BOOST_AUTO_TEST_CASE(TestDecodePacket)
     BOOST_CHECK_EQUAL(mod->get_front_width(),  1024);
     BOOST_CHECK_EQUAL(mod->get_front_height(), 768);
 
-    uint32_t count = 0;
-    BackEvent_t res = BACK_EVENT_NONE;
+    uint32_t    count = 0;
+    BackEvent_t res   = BACK_EVENT_NONE;
     while (res == BACK_EVENT_NONE) {
         LOG(LOG_INFO, "===================> count = %u", count);
-        if (count++ >= 12) break;
+        if (count++ >= 8) break;
         mod->draw_event(time(NULL));
     }
 
     char message[1024];
-    if (!check_sig(front.gd.impl(), message,
-    "\xb0\x16\x5e\xf5\x85\xdc\x1e\x3f\x91\x04\xe8\x0c\x19\x5e\x65\x34\xb5\x1b\xbd\xa6"
-    )){
+    if (!check_sig( front.gd.impl(), message
+                  , "\x7f\x88\xc9\xef\x98\xff\xaa\x53\x42\x53\x76\x1b\x11\x75\x9f\x5d\x86\x10\x61\xe9"
+                  )) {
         BOOST_CHECK_MESSAGE(false, message);
     }
     //front.dump_png("trace_wab_");
