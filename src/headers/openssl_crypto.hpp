859b25a7998ae2ee77b2e7df443430fa5ec2ecd1
Christophe Grosjean <pykoder@gmail.com>
2015-05-19 16:50:57 +0200

    Fixed inclusion conflict with set_key macro from des.h

--- src/headers/openssl_crypto.hpp
+++ src/headers/openssl_crypto.hpp
@@ -41,6 +41,15 @@ extern "C" {
 #include <openssl/evp.h>
 #include <openssl/aes.h>
 
+#include <openssl/ec.h>
+#include <openssl/ecdsa.h>
+#include <openssl/blowfish.h>
+#include <openssl/rand.h>
+#include <openssl/des.h>
+//#include <openssl/des_old.h>
+// We remove this macro from des_old because it conflicts with method set_key of some objects
+#undef set_key
+
 #ifdef __cplusplus
 }
 #endif
