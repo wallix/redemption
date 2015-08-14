8fc060194917fe5f6d2b276f8e7c495ff74e81ce
Raphael Zhou <rzhou@wallix.com>
2015-08-14 14:29:30 +0200

    Unit test for CLIPRDR virtual channel manager. (WIP) XfreeRDP + Up, down & full denied

--- tests/mod/rdp/channels/test_cliprdr_channel.cpp
+++ tests/mod/rdp/channels/test_cliprdr_channel.cpp
@@ -186,3 +186,294 @@ BOOST_AUTO_TEST_CASE(TestCliprdrChannelXfreeRDPFullAuthrisation)
 
     BOOST_CHECK(end_of_file_reached || t.get_status());
 }
+
+BOOST_AUTO_TEST_CASE(TestCliprdrChannelXfreeRDPDownDenied)
+{
+    int verbose = MODRDP_LOGLEVEL_CLIPRDR | MODRDP_LOGLEVEL_CLIPRDR_DUMP;
+
+    ClipboardVirtualChannel::Params clipboard_virtual_channel_params;
+
+    clipboard_virtual_channel_params.authentifier              = nullptr;
+    clipboard_virtual_channel_params.exchanged_data_limit      = 0;
+    clipboard_virtual_channel_params.verbose                   = verbose;
+
+    clipboard_virtual_channel_params.clipboard_down_authorized = false;
+    clipboard_virtual_channel_params.clipboard_up_authorized   = true;
+    clipboard_virtual_channel_params.clipboard_file_authorized = true;
+
+    clipboard_virtual_channel_params.dont_log_data_into_syslog = false;
+
+
+    #include "fixtures/test_cliprdr_channel_xfreerdp_down_denied.hpp"
+    TestTransport t("cliprdr", indata, sizeof(indata), outdata, sizeof(outdata),
+        verbose);
+
+    TestToClientSender to_client_sender(t);
+    TestToServerSender to_server_sender(t);
+
+    ClipboardVirtualChannel clipboard_virtual_channel(
+        &to_client_sender, &to_server_sender, clipboard_virtual_channel_params);
+
+    uint8_t         virtual_channel_data[CHANNELS::CHANNEL_CHUNK_LENGTH];
+    WriteOnlyStream virtual_channel_stream(virtual_channel_data,
+                                           sizeof(virtual_channel_data));
+
+    virtual_channel_stream.reset();
+
+    bool end_of_file_reached = false;
+
+    try
+    {
+        while (true) {
+            t.recv(reinterpret_cast<char**>(&virtual_channel_stream.end),
+                   16    // dest(4) + total_length(4) + flags(4) +
+                         //     chunk_length(4)
+                );
+
+            const uint32_t dest              =
+                virtual_channel_stream.in_uint32_le();
+            const uint32_t total_length      =
+                virtual_channel_stream.in_uint32_le();
+            const uint32_t flags             =
+                virtual_channel_stream.in_uint32_le();
+            const uint32_t chunk_data_length =
+                virtual_channel_stream.in_uint32_le();
+
+            //std::cout << "dest=" << dest <<
+            //    ", total_length=" << total_length <<
+            //    ", flags=" <<  flags <<
+            //    ", chunk_data_length=" << chunk_data_length <<
+            //    std::endl;
+
+            virtual_channel_stream.reset();
+
+            uint8_t * chunk_data = virtual_channel_stream.end;
+
+            t.recv(&virtual_channel_stream.end, chunk_data_length);
+
+            //hexdump_c(chunk_data, virtual_channel_stream.in_remain());
+
+            if (!dest)  // Client
+            {
+                clipboard_virtual_channel.process_client_message(
+                    total_length, flags, chunk_data, chunk_data_length);
+            }
+            else
+            {
+                std::unique_ptr<AsynchronousTask> out_asynchronous_task;
+
+                clipboard_virtual_channel.process_server_message(
+                    total_length, flags, chunk_data, chunk_data_length,
+                    out_asynchronous_task);
+
+                BOOST_CHECK(false == (bool)out_asynchronous_task);
+            }
+
+            virtual_channel_stream.reset();
+        }
+    }
+    catch (Error & e) {
+        if (e.id != ERR_TRANSPORT_NO_MORE_DATA) {
+            LOG(LOG_ERR, "Exception=%d", e.id);
+            throw;
+        }
+
+        end_of_file_reached = true;
+    }
+
+    BOOST_CHECK(end_of_file_reached || t.get_status());
+}
+
+BOOST_AUTO_TEST_CASE(TestCliprdrChannelXfreeRDPUpDenied)
+{
+    int verbose = MODRDP_LOGLEVEL_CLIPRDR | MODRDP_LOGLEVEL_CLIPRDR_DUMP;
+
+    ClipboardVirtualChannel::Params clipboard_virtual_channel_params;
+
+    clipboard_virtual_channel_params.authentifier              = nullptr;
+    clipboard_virtual_channel_params.exchanged_data_limit      = 0;
+    clipboard_virtual_channel_params.verbose                   = verbose;
+
+    clipboard_virtual_channel_params.clipboard_down_authorized = true;
+    clipboard_virtual_channel_params.clipboard_up_authorized   = false;
+    clipboard_virtual_channel_params.clipboard_file_authorized = true;
+
+    clipboard_virtual_channel_params.dont_log_data_into_syslog = false;
+
+
+    #include "fixtures/test_cliprdr_channel_xfreerdp_up_denied.hpp"
+    TestTransport t("cliprdr", indata, sizeof(indata), outdata, sizeof(outdata),
+        verbose);
+
+    TestToClientSender to_client_sender(t);
+    TestToServerSender to_server_sender(t);
+
+    ClipboardVirtualChannel clipboard_virtual_channel(
+        &to_client_sender, &to_server_sender, clipboard_virtual_channel_params);
+
+    uint8_t         virtual_channel_data[CHANNELS::CHANNEL_CHUNK_LENGTH];
+    WriteOnlyStream virtual_channel_stream(virtual_channel_data,
+                                           sizeof(virtual_channel_data));
+
+    virtual_channel_stream.reset();
+
+    bool end_of_file_reached = false;
+
+    try
+    {
+        while (true) {
+            t.recv(reinterpret_cast<char**>(&virtual_channel_stream.end),
+                   16    // dest(4) + total_length(4) + flags(4) +
+                         //     chunk_length(4)
+                );
+
+            const uint32_t dest              =
+                virtual_channel_stream.in_uint32_le();
+            const uint32_t total_length      =
+                virtual_channel_stream.in_uint32_le();
+            const uint32_t flags             =
+                virtual_channel_stream.in_uint32_le();
+            const uint32_t chunk_data_length =
+                virtual_channel_stream.in_uint32_le();
+
+            //std::cout << "dest=" << dest <<
+            //    ", total_length=" << total_length <<
+            //    ", flags=" <<  flags <<
+            //    ", chunk_data_length=" << chunk_data_length <<
+            //    std::endl;
+
+            virtual_channel_stream.reset();
+
+            uint8_t * chunk_data = virtual_channel_stream.end;
+
+            t.recv(&virtual_channel_stream.end, chunk_data_length);
+
+            //hexdump_c(chunk_data, virtual_channel_stream.in_remain());
+
+            if (!dest)  // Client
+            {
+                clipboard_virtual_channel.process_client_message(
+                    total_length, flags, chunk_data, chunk_data_length);
+            }
+            else
+            {
+                std::unique_ptr<AsynchronousTask> out_asynchronous_task;
+
+                clipboard_virtual_channel.process_server_message(
+                    total_length, flags, chunk_data, chunk_data_length,
+                    out_asynchronous_task);
+
+                BOOST_CHECK(false == (bool)out_asynchronous_task);
+            }
+
+            virtual_channel_stream.reset();
+        }
+    }
+    catch (Error & e) {
+        if (e.id != ERR_TRANSPORT_NO_MORE_DATA) {
+            LOG(LOG_ERR, "Exception=%d", e.id);
+            throw;
+        }
+
+        end_of_file_reached = true;
+    }
+
+    BOOST_CHECK(end_of_file_reached || t.get_status());
+}
+
+BOOST_AUTO_TEST_CASE(TestCliprdrChannelXfreeRDPFullDenied)
+{
+    int verbose = MODRDP_LOGLEVEL_CLIPRDR | MODRDP_LOGLEVEL_CLIPRDR_DUMP;
+
+    ClipboardVirtualChannel::Params clipboard_virtual_channel_params;
+
+    clipboard_virtual_channel_params.authentifier              = nullptr;
+    clipboard_virtual_channel_params.exchanged_data_limit      = 0;
+    clipboard_virtual_channel_params.verbose                   = verbose;
+
+    clipboard_virtual_channel_params.clipboard_down_authorized = false;
+    clipboard_virtual_channel_params.clipboard_up_authorized   = false;
+    clipboard_virtual_channel_params.clipboard_file_authorized = true;
+
+    clipboard_virtual_channel_params.dont_log_data_into_syslog = false;
+
+
+    #include "fixtures/test_cliprdr_channel_xfreerdp_full_denied.hpp"
+    TestTransport t("cliprdr", indata, sizeof(indata), outdata, sizeof(outdata),
+        verbose);
+
+    TestToClientSender to_client_sender(t);
+    TestToServerSender to_server_sender(t);
+
+    ClipboardVirtualChannel clipboard_virtual_channel(
+        &to_client_sender, &to_server_sender, clipboard_virtual_channel_params);
+
+    uint8_t         virtual_channel_data[CHANNELS::CHANNEL_CHUNK_LENGTH];
+    WriteOnlyStream virtual_channel_stream(virtual_channel_data,
+                                           sizeof(virtual_channel_data));
+
+    virtual_channel_stream.reset();
+
+    bool end_of_file_reached = false;
+
+    try
+    {
+        while (true) {
+            t.recv(reinterpret_cast<char**>(&virtual_channel_stream.end),
+                   16    // dest(4) + total_length(4) + flags(4) +
+                         //     chunk_length(4)
+                );
+
+            const uint32_t dest              =
+                virtual_channel_stream.in_uint32_le();
+            const uint32_t total_length      =
+                virtual_channel_stream.in_uint32_le();
+            const uint32_t flags             =
+                virtual_channel_stream.in_uint32_le();
+            const uint32_t chunk_data_length =
+                virtual_channel_stream.in_uint32_le();
+
+            //std::cout << "dest=" << dest <<
+            //    ", total_length=" << total_length <<
+            //    ", flags=" <<  flags <<
+            //    ", chunk_data_length=" << chunk_data_length <<
+            //    std::endl;
+
+            virtual_channel_stream.reset();
+
+            uint8_t * chunk_data = virtual_channel_stream.end;
+
+            t.recv(&virtual_channel_stream.end, chunk_data_length);
+
+            //hexdump_c(chunk_data, virtual_channel_stream.in_remain());
+
+            if (!dest)  // Client
+            {
+                clipboard_virtual_channel.process_client_message(
+                    total_length, flags, chunk_data, chunk_data_length);
+            }
+            else
+            {
+                std::unique_ptr<AsynchronousTask> out_asynchronous_task;
+
+                clipboard_virtual_channel.process_server_message(
+                    total_length, flags, chunk_data, chunk_data_length,
+                    out_asynchronous_task);
+
+                BOOST_CHECK(false == (bool)out_asynchronous_task);
+            }
+
+            virtual_channel_stream.reset();
+        }
+    }
+    catch (Error & e) {
+        if (e.id != ERR_TRANSPORT_NO_MORE_DATA) {
+            LOG(LOG_ERR, "Exception=%d", e.id);
+            throw;
+        }
+
+        end_of_file_reached = true;
+    }
+
+    BOOST_CHECK(end_of_file_reached || t.get_status());
+}
