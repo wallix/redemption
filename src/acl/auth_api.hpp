7a3f5366a598236e1669eb12f1754b86dec6aeb5
Raphael Zhou <rzhou@wallix.com>
2015-11-02 16:28:24 +0100

    Code cleaning. (Session log)

--- src/acl/auth_api.hpp
+++ src/acl/auth_api.hpp
@@ -32,11 +32,6 @@ public:
 
     virtual void report(const char * reason, const char * message) = 0;
 
-/*
-    virtual void log(const char * type, const char * data) const = 0;
-    virtual void log2(const char * type, const char * data, const char * info)
-        const = 0;
-*/
     virtual void log3(const char * type, const char * extra = nullptr) const = 0;
 };
 
@@ -49,12 +44,7 @@ class NullAuthentifier : public auth_api {
 
     void report(const char * reason, const char * message) override {}
 
-/*
-    void log(const char * type, const char * data) const override {}
-    void log2(const char * type, const char * data, const char * info)
-        const override {}
-*/
-    virtual void log3(const char * type, const char * extra = nullptr) const
+    void log3(const char * type, const char * extra = nullptr) const
         override {}
 };
 
