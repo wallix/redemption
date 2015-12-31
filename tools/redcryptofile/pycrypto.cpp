1c1ec632f6db150a930276286e42c20dbe610242
Christophe Grosjean <pykoder@gmail.com>
2015-12-31 13:12:04 +0100

    Simplifying cryptographic layer

--- tools/redcryptofile/pycrypto.cpp
+++ tools/redcryptofile/pycrypto.cpp
@@ -160,7 +160,91 @@ int crypto_close(crypto_file *cf, unsigned char hash[MD_HASH_LENGTH << 1], unsig
     return nResult;
 }
 
-#include "crypto_impl.h"
+#ifdef __cplusplus
+extern "C" {
+# if defined(__GNUC__) && !defined(__clang__)
+#  pragma GCC diagnostic push
+#  pragma GCC diagnostic ignored "-Wzero-as-null-pointer-constant"
+# endif
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/shm.h>
+#include <unistd.h>
+#include <errno.h>
+
+/* File format V1:  ([...] represent an uint32_t)
+ *
+ * Header:
+ *  [WABCRYPTOFILE_MAGIC][FILE_VERSION][Crypto IV]
+ *
+ * Chunk:
+ *  [ciphered chunk size (size it takes on disk)][data]
+ *
+ * Footer:
+ *  [WABCRYPTOFILE_EOF_MAGIC][raw_file_size]
+ *
+ */
+
+/*****************************************************************************************************
+ *                             WAB shared memory accessor procedures                                 *
+ *****************************************************************************************************/
+/* Attach, read and detach wab shared memory. Returns 0 on success, -1 on error.
+ */
+static inline int get_crypto_key(char * crypto_key)
+{
+    char tmp_buf[512] = {0};
+
+    int shmid = shmget(2242, 512, 0600);
+    if (shmid == -1){
+        printf("[CRYPTO_ERROR][%d]: Could not initialize crypto, shmget! error=%s\n", getpid(), strerror(errno));
+        return -1;
+    }
+    char *shm = (char*)shmat(shmid, 0, 0);
+    if (shm == (char *)-1){
+        printf("[CRYPTO_ERROR][%d]: Could not initialize crypto, shmat! error=%s\n", getpid(), strerror(errno));
+        return -1;
+    }
+    unbase64(tmp_buf, 512, shm);
+    if (shmdt(shm) == -1){
+        printf("[CRYPTO_ERROR][%d]: Could not initialize crypto, shmdt! error=%s\n", getpid(), strerror(errno));
+        return -1;
+    }
+
+    /* Extract the effective master key component, and check its control signature.
+     * Returns 0 on success, -1 on error.
+     */
+    char sha256_computed[SHA256_DIGEST_LENGTH];
+
+    if (SHA256((unsigned char *)(tmp_buf + SHA256_DIGEST_LENGTH+1),
+        MKSALT_LEN+CRYPTO_KEY_LENGTH, (unsigned char *)sha256_computed) == 0)
+    {
+        printf("[CRYPTO_ERROR][%d]: Could not check crypto key, SHA256!\n", getpid());
+        return -1;
+    }
+
+    if (strncmp(tmp_buf + 1, sha256_computed, SHA256_DIGEST_LENGTH)){
+        printf("[CRYPTO_ERROR][%d]: Crypto key integrity check failed!\n", getpid());
+        return -1;
+    }
+
+    memcpy(crypto_key, tmp_buf + SHA256_DIGEST_LENGTH+MKSALT_LEN+1, CRYPTO_KEY_LENGTH);
+    return 0;
+}
+
+static inline int derivate_crypto_key(const char * crypto_key, char * hmac_key)
+{
+    const unsigned char HASH_DERIVATOR[] = { 0x95, 0x8b, 0xcb, 0xd4, 0xee, 0xa9, 0x89, 0x5b };
+    return compute_hmac((unsigned char *)hmac_key, (const unsigned char *)crypto_key, HASH_DERIVATOR);
+}
+
+#ifdef __cplusplus
+# if defined(__GNUC__) && !defined(__clang__)
+#  pragma GCC diagnostic pop
+# endif
+} //extern "C"
+#endif
 
 extern "C" {
     UdevRandom * get_rnd();
--- tools/redcryptofile/test_trace.py
+++ tools/redcryptofile/test_trace.py
@@ -6,6 +6,10 @@ from redcryptotrace import CryptoTrace, Trace
 
 import hashlib
 
+import os
+PATH = os.path.dirname(os.path.realpath(__file__))
+print PATH
+
 def hashfile(fname):
     blocksize = 65536
     hasher = hashlib.md5()
@@ -23,25 +27,29 @@ import os
 class TestCryptoTrace(unittest.TestCase):
 
     def setUp(self):
-        for f in ["./text.txt.clear.trc", "./text.txt.clear.roundtrip", "./long.bin.clear.trc", "./long.bin.clear.roundtrip",
-                  "./text.txt.crypted.trc", "./text.txt.roundtrip", "./long.bin.crypted.trc", "./long.bin.roundtrip"]:
+        for f in ["text.txt.clear.trc", "text.txt.clear.roundtrip",
+                  "long.bin.clear.trc", "long.bin.clear.roundtrip",
+                  "text.txt.crypted.trc", "text.txt.roundtrip",
+                  "long.bin.crypted.trc", "long.bin.roundtrip"]:
             try:
-                os.unlink(f)
+                os.unlink(os.path.join(PATH,f))
             except:
                 pass
 
     def tearDown(self):
         """cleanup test files"""
-        for f in ["./text.txt.clear.trc", "./text.txt.clear.roundtrip", "./long.bin.clear.trc", "./long.bin.clear.roundtrip",
-                  "./text.txt.crypted.trc", "./text.txt.roundtrip", "./long.bin.crypted.trc", "./long.bin.roundtrip"]:
+        for f in ["text.txt.clear.trc", "text.txt.clear.roundtrip",
+                  "long.bin.clear.trc", "long.bin.clear.roundtrip",
+                  "text.txt.crypted.trc", "text.txt.roundtrip",
+                  "long.bin.crypted.trc", "long.bin.roundtrip"]:
             try:
-                os.unlink(f)
+                os.unlink(os.path.join(PATH,f))
             except:
                 pass
 
     def test_decrypttextfile(self):
-        t2 = CryptoTrace("./tools/redcryptofile/fixtures/text.txt.trc", "r")
-        with open("text.txt.roundtrip", 'w') as f:
+        t2 = CryptoTrace(os.path.join(PATH,"fixtures/text.txt.trc"), "r")
+        with open(os.path.join(PATH,"text.txt.roundtrip"), 'w') as f:
             while True:
                 data = t2.read(1024)
                 if len(data) == 0:
@@ -51,12 +59,13 @@ class TestCryptoTrace(unittest.TestCase):
         t2.close()
 
         # check decrypted file on disk same as original
-        self.assertEquals('1F439C7547B1EF1C1D3085483A87AD2E', hashfile("./text.txt.roundtrip"))
+        self.assertEquals('1F439C7547B1EF1C1D3085483A87AD2E', 
+            hashfile(os.path.join(PATH,"text.txt.roundtrip")))
 
 
     def test_decryptlongfile(self):
-        t2 = CryptoTrace("./tools/redcryptofile/fixtures/long.bin.trc", "r")
-        with open("long.bin.roundtrip", 'w') as f:
+        t2 = CryptoTrace(os.path.join(PATH,"fixtures/long.bin.trc"), "r")
+        with open(os.path.join(PATH,"long.bin.roundtrip"), 'w') as f:
             while True:
                 data = t2.read(1024)
                 if len(data) == 0:
@@ -65,22 +74,25 @@ class TestCryptoTrace(unittest.TestCase):
             t2.close()
 
         # check decrypted file on disk same as original
-        self.assertEquals('36209641806DB628D04F239192CD8E8C', hashfile("./long.bin.roundtrip"))
+        self.assertEquals('36209641806DB628D04F239192CD8E8C', 
+            hashfile(os.path.join(PATH,"long.bin.roundtrip")))
 
 
     def test_crypttextfile(self):
-        t = CryptoTrace("./text.txt.crypted.trc", "w")
-        with open("./tools/redcryptofile/fixtures/text.txt", 'r') as f:
+        t = CryptoTrace(os.path.join(PATH,"text.txt.crypted.trc"), "w")
+        with open(os.path.join(PATH,"fixtures/text.txt"), 'r') as f:
             for l in f:
                 t.write(l)
         t.flush()
         t.close()
+        
         # check encrypted file on disk did not change
-        self.assertEquals('995EB32D1067C5681B386E58305D14B5', hashfile("./text.txt.crypted.trc"))
+        self.assertEquals('995EB32D1067C5681B386E58305D14B5', 
+            hashfile(os.path.join(PATH,"text.txt.crypted.trc")))
 
         # check round trip
-        t2 = CryptoTrace("./text.txt.crypted.trc", "r")
-        with open("text.txt.roundtrip", 'w') as f:
+        t2 = CryptoTrace(os.path.join(PATH,"text.txt.crypted.trc"), "r")
+        with open(os.path.join(PATH,"text.txt.roundtrip"), 'w') as f:
             while True:
                 data = t2.read(1024)
                 if len(data) == 0:
@@ -89,20 +101,22 @@ class TestCryptoTrace(unittest.TestCase):
         t2.close()
 
         # check decrypted file on disk same as original
-        self.assertEquals('1F439C7547B1EF1C1D3085483A87AD2E', hashfile("./text.txt.roundtrip"))
+        self.assertEquals('1F439C7547B1EF1C1D3085483A87AD2E',
+            hashfile(os.path.join(PATH,"text.txt.roundtrip")))
 
     def test_cryptlongfile(self):
-        t = CryptoTrace("./long.bin.crypted.trc", "w")
-        with open("./tools/redcryptofile/fixtures/long.bin", 'r') as f:
+        t = CryptoTrace(os.path.join(PATH,"long.bin.crypted.trc"), "w")
+        with open(os.path.join(PATH,"fixtures/long.bin"), 'r') as f:
             for l in f:
                 t.write(l)
             t.close()
         # check encrypted file on disk did not change
-        self.assertEquals('06345B901BF761E14EEA841D322CA2E1', hashfile("./long.bin.crypted.trc"))
+        self.assertEquals('06345B901BF761E14EEA841D322CA2E1', 
+            hashfile(os.path.join(PATH,"long.bin.crypted.trc")))
 
         # check round trip
-        t2 = CryptoTrace("./long.bin.crypted.trc", "r")
-        with open("long.bin.roundtrip", 'w') as f:
+        t2 = CryptoTrace(os.path.join(PATH,"long.bin.crypted.trc"), "r")
+        with open(os.path.join(PATH,"long.bin.roundtrip"), 'w') as f:
             while True:
                 data = t2.read(1024)
                 if len(data) == 0:
@@ -111,20 +125,22 @@ class TestCryptoTrace(unittest.TestCase):
             t2.close()
 
         # check decrypted file on disk same as original
-        self.assertEquals('36209641806DB628D04F239192CD8E8C', hashfile("./long.bin.roundtrip"))
+        self.assertEquals('36209641806DB628D04F239192CD8E8C', 
+            hashfile(os.path.join(PATH,"long.bin.roundtrip")))
 
     def test_flattextfile(self):
-        t = open("./text.txt.clear.trc", "w")
-        with open("./tools/redcryptofile/fixtures/text.txt", 'r') as f:
+        t = open(os.path.join(PATH,"text.txt.clear.trc"), "w")
+        with open(os.path.join(PATH,"fixtures/text.txt"), 'r') as f:
             for l in f:
                 t.write(l)
             t.close()
         # check encrypted file on disk did not change
-        self.assertEquals('1F439C7547B1EF1C1D3085483A87AD2E', hashfile("./text.txt.clear.trc"))
+        self.assertEquals('1F439C7547B1EF1C1D3085483A87AD2E',
+             hashfile(os.path.join(PATH,"text.txt.clear.trc")))
 
         # check round trip
-        t2 = Trace("./text.txt.clear.trc")
-        with open("text.txt.clear.roundtrip", 'w') as f:
+        t2 = Trace(os.path.join(PATH,"text.txt.clear.trc"))
+        with open(os.path.join(PATH,"text.txt.clear.roundtrip"), 'w') as f:
             while True:
                 data = t2.read(1024)
                 if len(data) == 0:
@@ -132,20 +148,22 @@ class TestCryptoTrace(unittest.TestCase):
                 f.write(data)
 
         # check decrypted file on disk same as original
-        self.assertEquals('1F439C7547B1EF1C1D3085483A87AD2E', hashfile("./text.txt.clear.roundtrip"))
+        self.assertEquals('1F439C7547B1EF1C1D3085483A87AD2E',
+            hashfile(os.path.join(PATH,"text.txt.clear.roundtrip")))
 
     def test_flatlongfile(self):
-        t = open("./long.bin.clear.trc", "w")
-        with open("./tools/redcryptofile/fixtures/long.bin", 'r') as f:
+        t = open(os.path.join(PATH,"long.bin.clear.trc"), "w")
+        with open(os.path.join(PATH,"fixtures/long.bin"), 'r') as f:
             for l in f:
                 t.write(l)
             t.close()
         # check encrypted file on disk did not change
-        self.assertEquals('36209641806DB628D04F239192CD8E8C', hashfile("./long.bin.clear.trc"))
+        self.assertEquals('36209641806DB628D04F239192CD8E8C',
+            hashfile(os.path.join(PATH,"long.bin.clear.trc")))
 
         # check round trip
-        t2 = Trace("./long.bin.clear.trc")
-        with open("long.bin.clear.roundtrip", 'w') as f:
+        t2 = Trace(os.path.join(PATH,"long.bin.clear.trc"))
+        with open(os.path.join(PATH,"long.bin.clear.roundtrip"), 'w') as f:
             while True:
                 data = t2.read(1024)
                 if len(data) == 0:
@@ -153,9 +171,9 @@ class TestCryptoTrace(unittest.TestCase):
                 f.write(data)
 
         # check decrypted file on disk same as original
-        self.assertEquals('36209641806DB628D04F239192CD8E8C', hashfile("./long.bin.clear.roundtrip"))
+        self.assertEquals('36209641806DB628D04F239192CD8E8C', 
+            hashfile(os.path.join(PATH,"long.bin.clear.roundtrip")))
 
 if __name__ == '__main__':
     unittest.main()
-
 
