8b3f31fdc44bfb32213b2eb81070fbc9a6b21530
Christophe Grosjean <pykoder@gmail.com>
2016-01-20 12:05:58 +0100

    Fixed buffer overflow in SHA256 based get_derivator

--- src/transport/cryptofile.hpp
+++ src/transport/cryptofile.hpp
@@ -121,7 +121,9 @@ class CryptoContext {
         char * file_basename = basename(file);
         SslSha256 sha256;
         sha256.update(byte_ptr_cast(file_basename), strlen(file_basename));
-        sha256.final(derivator, derivator_len);
+        uint8_t tmp[SHA256_DIGEST_LENGTH];
+        sha256.final(tmp, SHA256_DIGEST_LENGTH);
+        memcpy(derivator, tmp, SHA256_DIGEST_LENGTH);
     }
 
     void random(void * dest, size_t size) 
@@ -168,7 +170,6 @@ class CryptoContext {
 
     int get_crypto_key_from_shm()
     {
-        printf("get_crypto_key_from_ini\n");
         char tmp_buf[512] = {0};
         int shmid = shmget(2242, 512, 0600);
         if (shmid == -1){
@@ -205,7 +206,6 @@ class CryptoContext {
 
     int get_crypto_key_from_ini()
     {
-        printf("get_crypto_key_from_ini\n");
         memcpy(this->crypto_key, this->ini.get<cfg::crypto::key0>(), sizeof(this->crypto_key));
         memcpy(this->hmac_key, ini.get<cfg::crypto::key1>(), sizeof(this->hmac_key));
         return 0;
@@ -213,8 +213,6 @@ class CryptoContext {
 
     int get_crypto_key_from_ini_derivated_hmac()
     {
-        printf("get_crypto_key_from_ini_derivated_hmac\n");
-
         memcpy(this->crypto_key, this->ini.get<cfg::crypto::key0>(), sizeof(this->crypto_key));
         this->crypto_key_loaded = true;
         const unsigned char tmp_derivation[] = 
@@ -247,8 +245,6 @@ class CryptoContext {
 
     const unsigned char * get_crypto_key()
     {
-        printf("get_crypto_key()\n");
-
         if (not this->crypto_key_loaded)
         {
             switch (key_source){
@@ -281,21 +277,12 @@ class CryptoContext {
 
     int compute_hmac(unsigned char * hmac, const unsigned char * derivator)
     {
-        printf("compute hmac\n");
         unsigned char tmp_derivation[DERIVATOR_LENGTH + CRYPTO_KEY_LENGTH] = {}; // derivator + masterkey
         unsigned char derivated[SHA256_DIGEST_LENGTH  + CRYPTO_KEY_LENGTH] = {}; // really should be MAX, but + will do
-
         memcpy(tmp_derivation, derivator, DERIVATOR_LENGTH);
         memcpy(tmp_derivation + DERIVATOR_LENGTH, this->get_crypto_key(), CRYPTO_KEY_LENGTH);
-
-        printf("compute hmac SHA\n");
-
         SHA256(tmp_derivation, CRYPTO_KEY_LENGTH + DERIVATOR_LENGTH, derivated);
-
-        printf("compute hmac copy result\n");
-
         memcpy(hmac, derivated, HMAC_KEY_LENGTH);
-        printf("compute hmac done\n");
 
         return 0;
     }
