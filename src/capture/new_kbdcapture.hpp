93f5ea954fe68617a3a802b6b32affb22e9b99e9
jonathanpoelen <jonathan.poelen@gmail.com>
2016-02-09 11:39:48 +0100

    fixes NewKbdCapture::send_data when enable_mask is actived

--- src/capture/new_kbdcapture.hpp
+++ src/capture/new_kbdcapture.hpp
@@ -22,7 +22,10 @@
 #ifndef CAPTURE_NEW_KBDCAPTURE_HPP
 #define CAPTURE_NEW_KBDCAPTURE_HPP
 
+#include "capture/CaptureDevice.hpp"
+#include "transport/transport.hpp"
 #include "utils/match_finder.hpp"
+#include "utils/difftimeval.hpp"
 #include "stream.hpp"
 #include "cast.hpp"
 
@@ -295,7 +298,7 @@ public:
 
     virtual void snapshot(const timeval & now, int x, int y, bool ignore_frame_in_timeval,
                           bool const & requested_to_stop) override {
-        if ((difftimeval(now, this->last_snapshot) < 1000000) &&
+        if ((difftimeval(now, this->last_snapshot) < this->time_to_wait) &&
             (this->unlogged_data.get_offset() < 8 * sizeof(uint32_t))) {
             return;
         }
@@ -384,6 +387,10 @@ public:
 
     void send_data(Transport & trans) {
         REDASSERT(!this->unlogged_data.get_offset());
+
+        if (this->keyboard_input_mask_enabled) {
+            ::memset(this->data.get_data(), '*', this->data.get_offset());
+        }
 
         trans.send(this->data.get_data(), this->data.get_offset());
 
