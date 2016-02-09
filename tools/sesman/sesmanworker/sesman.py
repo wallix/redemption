4dd4b263480a70bb948f529bc3b65c38f2ca4ea9
Meng Tan <mtan@wallix.com>
2016-02-09 17:17:24 +0100

    refactor target login completion

--- tools/sesman/sesmanworker/sesman.py
+++ tools/sesman/sesmanworker/sesman.py
@@ -1238,13 +1238,14 @@ class Sesman():
                         kv[u'shell_working_directory'] = app_params.workingdir
 
                         kv[u'target_application'] = "%s@%s" % \
-                            (target_login_info.account_login,
+                            (target_login_info.account_name,
                              target_login_info.target_name)
                         if app_params.params is not None:
-                            if app_params.params.find(u'${USER}') != -1:
+                            if u'${USER}' in app_params.params:
                                 kv[u'target_application_account'] = \
-                                    target_login_info.account_login
-                            if app_params.params.find(u'${PASSWORD}') != -1:
+                                    target_login_info.account_login or \
+                                    self.target_context.login or ""
+                            if u'${PASSWORD}' in app_params.params:
                                 kv[u'target_application_password'] = \
                                     self.engine.get_target_password(selected_target)
 
@@ -1252,17 +1253,19 @@ class Sesman():
                         kv[u'disable_tsk_switch_shortcuts'] = u'yes'
                     self.cn = target_login_info.target_name
 
-                    if not self.passthrough_mode:
-                        kv[u'target_login'] = physical_info.account_login
-                        if not kv.get(u'target_login') and self.target_context.login:
-                            kv[u'target_login'] = self.target_context.login
-
                     if self.target_context.host:
                         kv[u'target_host'] = self.target_context.host
                         kv[u'target_device'] = self.target_context.showname()
                     else:
                         kv[u'target_host'] = physical_info.device_host
 
+                    kv[u'target_login'] = physical_info.account_login
+                    if (not kv.get(u'target_login') and
+                        self.target_context.login and
+                        not application):
+                        # on application,
+                        # login in target_context is the login of application
+                        kv[u'target_login'] = self.target_context.login
 
                     kv[u'target_port'] = physical_info.service_port
                     kv[u'device_id'] = physical_info.device_id
