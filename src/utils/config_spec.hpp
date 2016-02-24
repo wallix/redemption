5ca39a323dffb81706fb7730151dcde6f66c2d41
Christophe Grosjean <pykoder@gmail.com>
2016-02-24 15:22:45 +0100

    Renamed sesman key0->encryption_key key1->sign_key

--- src/utils/config_spec.hpp
+++ src/utils/config_spec.hpp
@@ -472,13 +472,19 @@ void config_spec_definition(Writer && W)
 
     W.section("crypto", [&]
     {
-        W.member(H, type_<StaticKeyString<32>>(), "encryption_key", real_name{"key0"}, set(
+        W.member(H, type_<StaticKeyString<32>>(), "encryption_key", 
+            real_name{"key0"}, 
+            str_authid{"encryption_key"}, 
+            set(
             "\x00\x01\x02\x03\x04\x05\x06\x07"
             "\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"
             "\x10\x11\x12\x13\x14\x15\x16\x17"
             "\x18\x19\x1A\x1B\x1C\x1D\x1E\x1F"
         ), r);
-        W.member(H, type_<StaticKeyString<32>>(), "sign_key", real_name{"key1"}, set(
+        W.member(H, type_<StaticKeyString<32>>(), "sign_key", 
+            real_name{"key1"}, 
+            str_authid{"sign_key"}, 
+            set(
             "\x00\x01\x02\x03\x04\x05\x06\x07"
             "\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F"
             "\x10\x11\x12\x13\x14\x15\x16\x17"
