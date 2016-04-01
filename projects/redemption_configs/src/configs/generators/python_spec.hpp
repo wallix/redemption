8e61ff723de3bfb06c9f8b79cd357171d55adef9
jonathanpoelen <jonathan.poelen@gmail.com>
2016-04-01 16:07:47 +0200

    config_spec.hpp moved to specs/

--- projects/redemption_configs/src/configs/generators/python_spec.hpp
+++ projects/redemption_configs/src/configs/generators/python_spec.hpp
@@ -21,7 +21,7 @@
 #ifndef REDEMPTION_SRC_UTILS_APPS_APP_WRITE_PYTHON_SPEC_HPP
 #define REDEMPTION_SRC_UTILS_APPS_APP_WRITE_PYTHON_SPEC_HPP
 
-#include "configs/config_spec.hpp"
+#include "configs/specs/config_spec.hpp"
 #include "configs/multi_filename_writer.hpp"
 
 #include <algorithm>
