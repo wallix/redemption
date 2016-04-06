50a081282422c7464daac6e6921bd18ae74a8482
Jennifer Inthavong <jinthavong@wallix.com>
2016-04-06 18:17:49 +0200

    JS/RDP Client : JS native client - connexion ok.

--- projects/browser_client/sys/js_client/js_client0.js
+++ projects/browser_client/sys/js_client/js_client0.js
@@ -1,21 +1,15 @@
-function square(x, y, w, h, color) {
-  var canvas = document.getElementById("canvas");
-  var context = canvas.getContext('2d');
-  var colors = { 0:"SeaGreen",
-                 1:"Blue", 2:"Crimson", 3:"Coral", 4:"Cyan",
-                 5:"Chartreuse", 6:"Fuchsia ", 7:"Indigo", 8:"LightPink",
-                 9:"Gray", 10:"Orange", 11:"Yellow", 12:"Black" };
-  
-  context.fillStyle = colors[color];
-  context.fillRect(
-    x * canvas.width/12, canvas.height - (y+1) * canvas.height/5,
-    canvas.width/12, canvas.height/5);
-};
 
 window.onload = function() {
-  _run_main();
-  document.getElementById("canvas").addEventListener('click', function() {_run_main();}); 
-};
+   try {
+      var socket = io.connect('http://localhost:8080');
 
-//square(0, 0, 12, 5, 'GoldenRod')
+
+      socket.on('connect', function(data) {
+        socket.send('Hello.');
+      });
+   }
+   catch(err) {
+        alert('ERROR: socket.io encountered a problem:\n\n' + err);
+   }
+};
 
