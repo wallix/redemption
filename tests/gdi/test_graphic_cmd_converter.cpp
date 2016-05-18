7279cac7f9315bd5a24b0d10706539f2430c1932
jonathanpoelen <jonathan.poelen@gmail.com>
2016-05-18 15:10:51 +0200

    [warn] fixed missing-field-initializers

--- tests/gdi/test_graphic_cmd_converter.cpp
+++ tests/gdi/test_graphic_cmd_converter.cpp
@@ -60,7 +60,7 @@ struct RngByBpp
 
 template<bool has_enc15, bool has_enc16, bool has_enc24, class Dec>
 Color4 get_colors(Dec dec, uint32_t color) {
-    Color4 colors {};
+    Color4 colors {{}};
     RngByBpp rng_by_bpp{colors};
     gdi::GraphicCmdColorDistributor<RngByBpp, Dec, false, has_enc15, has_enc16, has_enc24>{
         rng_by_bpp, dec
