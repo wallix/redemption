4e1eea46697d5dc225f38a65b0cd418311275a7f
Pianna <moroldo.clement@gmail.com>
2016-06-02 11:33:18 +0200

    tests/mod/rdp/test_show_data.cpp

--- projects/browser_client_JS/sys/js_client/data_test_transport_web_socket.js
+++ projects/browser_client_JS/sys/js_client/data_test_transport_web_socket.js
@@ -1,24 +1,26 @@
 
 var data = [42, 42, 42, 42, 42, 42, 42, 42,
-	    42, 42, 42, 42, 42, 42, 42, 42,
-	    42, 42, 42, 42, 42, 42, 42, 42,
-	    42, 42, 42, 42, 42, 42, 42, 42,
-	    42, 42, 42, 42, 42, 42, 42, 42,
-	    42, 42, 42, 42, 42, 42, 42, 42,
-	    42, 42, 42, 42, 42, 42, 42, 42,
-	    42, 42, 42, 42, 42, 42, 42, 42,]
+            42, 42, 42, 42, 42, 42, 42, 42,
+            42, 42, 42, 42, 42, 42, 42, 42,
+            42, 42, 42, 42, 42, 42, 42, 42,
+            42, 42, 42, 42, 42, 42, 42, 42,
+            42, 42, 42, 42, 42, 42, 42, 42,
+            42, 42, 42, 42, 42, 42, 42, 42,
+            42, 42, 42, 42, 42, 42, 42, 42,]
 
 var len = 64;
+
 var current = 0;
 
 function getDataOctet(i) {
 	if (current < len) {
-		current++;
-		//console.log('getOctet = ' + data[current-1]);
-		//return data[current-1];
-		_recv_value(data[current-1], i);
+        var start = current;
+        current += i;
+        var newData = data.slice(start, current);
+		_recv_value(newData);
+
 	} else {
-		//return 0;
-		_recv_value(0, i);
+        //var tab[i] = [0];
+		//_recv_value(tab);
 	}
 }
