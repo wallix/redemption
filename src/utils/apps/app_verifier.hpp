e231f5bc60f6fb9c35b9a467d6ae3bb5159d2801
Christophe Grosjean <pykoder@gmail.com>
2016-06-27 02:37:53 +0200

    Simplifying app_verifier

--- src/utils/apps/app_verifier.hpp
+++ src/utils/apps/app_verifier.hpp
@@ -799,23 +799,15 @@ static inline int check_encrypted_or_checksumed(
 
                 // v2
                 if (this->cur[0] == 'v') {
-                    if (this->next_line()){
-                        throw Error(ERR_TRANSPORT_READ_FAILED, errno);
-                    }
-                    if (this->next_line()){
-                        throw Error(ERR_TRANSPORT_READ_FAILED, errno);
-                    }
+                    this->next_line();
+                    this->next_line();
                     header.version = 2;
                     header.has_checksum = (this->cur[0] == 'c');
                 }
                 // else v1
-                    if (this->next_line()){
-                        throw Error(ERR_TRANSPORT_READ_FAILED, errno);
-                    }
-                    if (this->next_line()){
-                        throw Error(ERR_TRANSPORT_READ_FAILED, errno);
-                    }
-                    return header;
+                this->next_line();
+                this->next_line();
+                return header;
             }
 
             int read_meta_file_v1(MetaLine2 & meta_line)
@@ -949,55 +941,20 @@ static inline int check_encrypted_or_checksumed(
                 return 0;
             }
 
-            ssize_t read_line(char * dest, size_t len, int err)
-            {
-                ssize_t total_read = 0;
-                while (1) {
-                    char * pos = std::find(this->cur, this->eof, '\n');
-                    if (len < size_t(pos - this->cur)) {
-                        total_read += len;
-                        memcpy(dest, this->cur, len);
-                        this->cur += len;
-                        break;
-                    }
-                    total_read += pos - this->cur;
-                    memcpy(dest, this->cur, pos - this->cur);
-                    dest += pos - this->cur;
-                    this->cur = pos + 1;
-                    if (pos != this->eof) {
-                        break;
-                    }
-                    ssize_t ret = this->reader_buf.read(this->buf, sizeof(this->buf));
 
-                    if (ret < 0 && errno != EINTR) {
-                        return -ERR_TRANSPORT_READ_FAILED;
-                    }
-                    if (ret == 0) {
-                        return -err;
-                    }
-                    this->eof = this->buf + ret;
-                    this->cur = this->buf;
-                }
-                return total_read;
-            }
-
-            int next_line()
+            void next_line()
             {
                 char * pos;
                 while ((pos = std::find(this->cur, this->eof, '\n')) == this->eof) {
                     ssize_t ret = this->reader_buf.read(this->buf, sizeof(this->buf));
 
-                    if (ret < 0 && errno != EINTR) {
-                        return -ERR_TRANSPORT_READ_FAILED;
-                    }
                     if (ret == 0) {
-                        return -ERR_TRANSPORT_READ_FAILED;
+                        throw Error(ERR_TRANSPORT_READ_FAILED, errno);
                     }
                     this->eof = this->buf + ret;
                     this->cur = this->buf;
                 }
                 this->cur = pos+1;
-                return 0;
             }
         } reader(ifile);
 
