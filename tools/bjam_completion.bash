319172792f6540f3480e34fd3ae6ac4bca62d512
jonathanpoelen <jonathan.poelen@gmail.com>
2016-11-07 10:58:49 +0100

    updated bjam_completion

--- tools/bjam_completion.bash
+++ tools/bjam_completion.bash
@@ -1,10 +1,14 @@
 _bjam_completion () {
   local cur prev words cword
   _init_completion || return
-  COMPREPLY=($(compgen -W '$(sed -n -E '\''/^ *(unit-test|exe|install|alias|lib|explicit)/{
+  COMPREPLY=($(compgen -W '$(sed -n -E '\''
+  /^ *(test-run|unit-test|exe|install|alias|lib|explicit) /{
     s/^ *[^ ]+\s+([-_a-zA-Z0-9]+).*/\1/
     H
     /_src$/!p
+  }
+  /^ *test-canonical /{
+    s#^.+/([^.]+)\.h.*#test_\1#p
   }'\'' Jamroot)' -- "$cur" ))
 }
 
