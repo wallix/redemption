b0924ac67533135be5b027e6861618c9ceb450e3
Pianna <moroldo.clement@gmail.com>
2017-02-13 16:46:26 +0100

    client qt Qt4 dependance isolated

--- projects/QtClient/src/front_Qt.hpp
+++ projects/QtClient/src/front_Qt.hpp
@@ -82,44 +82,7 @@
 #include "keymaps/Qt_ScanCode_KeyMap.hpp"
 #include "capture/capture.hpp"
 
-
-#include <QtGui/QImage>
-#include <QtGui/QRgb>
-#include <QtGui/QBitmap>
-#include <QtGui/QColormap>
-#include <QtGui/QPainter>
-
-#include <QtGui/QWidget>
-#include <QtGui/QLabel>
-#include <QtGui/QPainter>
-#include <QtGui/QColor>
-#include <QtGui/QDesktopWidget>
-#include <QtGui/QApplication>
-#include <QtGui/QMouseEvent>
-#include <QtGui/QWheelEvent>
-#include <QtCore/QSocketNotifier>
-#include <QtGui/QLineEdit>
-#include <QtGui/QFormLayout>
-#include <QtGui/QDialog>
-#include <QtGui/QPushButton>
-#include <QtGui/QClipboard>
-#include <QtGui/QTabWidget>
-#include <QtGui/QGridLayout>
-#include <QtGui/QComboBox>
-#include <QtGui/QCheckBox>
-#include <QtGui/QScrollArea>
-#include <QtGui/QTableWidget>
-#include <QtCore/QList>
-#include <QtCore/QStringList>
-#include <QtCore/QTimer>
-#include <QtCore/QMimeData>
-#include <QtCore/QUrl>
-#include <QtGui/QCompleter>
-#include <QtGui/QFileDialog>
-#include <QtCore/QThread>
-#include <QtCore/QtGlobal>
-#include <QtCore/QDebug>
-
+#include "Qt4/Qt.hpp"
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wfloat-equal"
