57e3d6be7b0fa006f2f122743c4a25087b2e8513
Christophe Grosjean <pykoder@gmail.com>
2017-02-22 13:55:31 +0100

    Inlined fdbuf in wrm_capture

--- src/capture/wrm_capture.hpp
+++ src/capture/wrm_capture.hpp
@@ -171,6 +171,162 @@ struct wrmcapture_out_sequence_filename_buf_param
     {}
 };
 
+#include "utils/sugar/exchange.hpp"
+
+#include <cerrno>
+#include <cstddef>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+class iofdbuf
+{
+    int fd;
+
+public:
+    explicit iofdbuf(int fd = -1) noexcept
+    : fd(fd)
+    {}
+
+    iofdbuf(iofdbuf const &) = delete ;
+    iofdbuf&operator=(iofdbuf const &) = delete ;
+
+    iofdbuf(iofdbuf && other) noexcept
+    : fd(other.fd)
+    {
+        other.fd = -1;
+    }
+
+    iofdbuf& operator=(iofdbuf && other) noexcept
+    {
+        this->fd = exchange(other.fd, -1);
+        return *this;
+    }
+
+    ~iofdbuf()
+    {
+        this->close();
+    }
+
+    //int get_fd() const
+    //{
+    //   return this->fd;
+    //}
+
+    int open(const char *pathname, int flags)
+    {
+        this->close();
+        this->fd = ::open(pathname, flags);
+        return fd;
+    }
+
+    int open(const char *pathname, int flags, mode_t mode)
+    {
+        this->close();
+        this->fd = ::open(pathname, flags, mode);
+        return fd;
+    }
+
+    int open(int fd)
+    {
+        this->close();
+        this->fd = fd;
+        return fd;
+    }
+
+    int close()
+    {
+        if (this->is_open()) {
+            const int ret = ::close(this->fd);
+            this->fd = -1;
+            return ret;
+        }
+        return 0;
+    }
+
+    bool is_open() const noexcept
+    {
+        return -1 != this->fd;
+    }
+
+    explicit operator bool () const noexcept
+    {
+        return this->is_open();
+    }
+
+    ssize_t read(void * data, size_t len) const
+    {
+        return this->read_all(data, len);
+    }
+
+    ssize_t read_all(void * data, size_t len) const
+    {
+        size_t remaining_len = len;
+        while (remaining_len) {
+            ssize_t ret = ::read(this->fd, static_cast<char*>(data) + (len - remaining_len), remaining_len);
+            if (ret < 0){
+                if (errno == EINTR){
+                    continue;
+                }
+                // Error should still be there next time we try to read
+                if (remaining_len != len){
+                    return len - remaining_len;
+                }
+                return ret;
+            }
+            // We must exit loop or we will enter infinite loop
+            if (ret == 0){
+                break;
+            }
+            remaining_len -= ret;
+        }
+        return len - remaining_len;
+    }
+
+
+    ssize_t write(const void * data, size_t len) const
+    {
+        return this->write_all(data, len);
+    }
+
+    ssize_t write_all(const void * data, size_t len) const
+    {
+        size_t remaining_len = len;
+        size_t total_sent = 0;
+        while (remaining_len) {
+            ssize_t ret = ::write(this->fd, static_cast<const char*>(data) + total_sent, remaining_len);
+            if (ret <= 0){
+                if (errno == EINTR){
+                    continue;
+                }
+                return -1;
+            }
+            remaining_len -= ret;
+            total_sent += ret;
+        }
+        return total_sent;
+    }
+
+    off64_t seek(off64_t offset, int whence) const
+    { return lseek64(this->fd, offset, whence); }
+
+    void swap(iofdbuf & other) noexcept
+    {
+        int const fd = this->fd;
+        this->fd = other.fd;
+        other.fd = fd;
+    }
+
+    int release() noexcept {
+        int old_fd = this->fd;
+
+        this->fd = -1;
+
+        return old_fd;
+    }
+};
+
 struct wrmcapture_no_param {};
 
 
@@ -187,7 +343,7 @@ class wrmcapture_out_sequence_filename_buf_impl
 {
     char current_filename_[1024];
     wrmcapture_FilenameGenerator filegen_;
-    wrmcapture_empty_ctor<io::posix::fdbuf> buf_;
+    wrmcapture_empty_ctor<iofdbuf> buf_;
     unsigned num_file_;
     int groupid_;
 
@@ -242,7 +398,7 @@ public:
     const wrmcapture_FilenameGenerator & seqgen() const noexcept
     { return this->filegen_; }
 
-    wrmcapture_empty_ctor<io::posix::fdbuf> & buf() noexcept
+    wrmcapture_empty_ctor<iofdbuf> & buf() noexcept
     { return this->buf_; }
 
     const char * current_path() const
