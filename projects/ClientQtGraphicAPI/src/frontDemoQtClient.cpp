56bbc32309d740714bcc387145a671aa938808f3
Pianna <moroldo.clement@gmail.com>
2017-03-23 15:14:12 +0100

    client qt non nla connection on nla server bug fixed

--- projects/ClientQtGraphicAPI/src/frontDemoQtClient.cpp
+++ projects/ClientQtGraphicAPI/src/frontDemoQtClient.cpp
@@ -19,7 +19,9 @@
 
 */
 
-#define LOGPRINT
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wfloat-equal"
+
 #include "utils/log.hpp"
 
 #include "rdp_client_graphic_api/front_qt_rdp_graphic_api.hpp"
@@ -27,18 +29,27 @@
 // VNC
 #include "mod/vnc/vnc.hpp"
 
+#include "mod/rdp/rdp.hpp"
+
+#pragma GCC diagnostic pop
+
 
 
 class FrontDemoQtClient : public FrontQtRDPGraphicAPI
 {
 
+    // VNC
+    Inifile ini;
     Translator translator;
     Theme theme;
+    LCGRandom            gen;
+
 
 public:
     FrontDemoQtClient(RDPVerbose verbose)
       : FrontQtRDPGraphicAPI(verbose)
       , translator(Translation::language_t::FR)
+      , gen(0)
     {}
 
     ~FrontDemoQtClient() {}
@@ -48,30 +59,63 @@ public:
         try {
 
             // VNC
-            Inifile ini;
-            //Translator translator(Translation::language_t::FR);
-            //Theme theme;
+//             this->mod = new mod_vnc( *(this->socket)
+//                                    , this->user_name.c_str()
+//                                    , this->user_password.c_str()
+//                                    , *(this)
+//                                    , this->info.width
+//                                    , this->info.height
+//                                    , ini.get<cfg::font>()
+//                                    , this->translator
+//                                    , this->theme
+//                                    , this->info.keylayout
+//                                    , 0
+//                                    , true
+//                                    , true
+//                                    , "0,1,-239"
+//                                    , false
+//                                    , true
+//                                    , mod_vnc::ClipboardEncodingType::UTF8
+//                                    , VncBogusClipboardInfiniteLoop::delayed
+//                                    , this->authentifier
+//                                    , 0xffffffff);
 
-            this->mod = new mod_vnc( *(this->socket)
-                                   , this->user_name.c_str()
-                                   , this->user_password.c_str()
-                                   , *(this)
-                                   , this->info.width
-                                   , this->info.height
-                                   , ini.get<cfg::font>()
-                                   , this->translator
-                                   , this->theme
-                                   , this->info.keylayout
-                                   , 0
-                                   , true
-                                   , true
-                                   , "0,1,-239"
-                                   , false
-                                   , true
-                                   , mod_vnc::ClipboardEncodingType::UTF8
-                                   , VncBogusClipboardInfiniteLoop::delayed
-                                   , this->authentifier
-                                   , 0xffffffff);
+
+        Inifile ini;
+
+        ModRDPParams mod_rdp_params( this->user_name.c_str()
+                                , this->user_password.c_str()
+                                , this->target_IP.c_str()
+                                , this->local_IP.c_str()
+                                , 2
+                                , ini.get<cfg::font>()
+                                , ini.get<cfg::theme>()
+                                , to_verbose_flags(0)
+                                );
+
+        mod_rdp_params.device_id                       = "device_id";
+        mod_rdp_params.enable_tls                      = true;
+        mod_rdp_params.enable_nla                      = true;
+        mod_rdp_params.enable_fastpath                 = false;
+        mod_rdp_params.enable_mem3blt                  = true;
+        mod_rdp_params.enable_new_pointer              = true;
+        mod_rdp_params.server_redirection_support      = true;
+        mod_rdp_params.enable_new_pointer              = true;
+        mod_rdp_params.enable_glyph_cache              = true;
+        std::string allow_channels = "*";
+        mod_rdp_params.allow_channels                  = &allow_channels;
+        mod_rdp_params.verbose = to_verbose_flags(0);
+
+
+            this->mod = new mod_rdp( *(this->socket)
+                                , *(this)
+                                , this->info
+                                , ini.get_ref<cfg::mod_rdp::redir_info>()
+                                , this->gen
+                                , this->timeSystem
+                                , mod_rdp_params
+                                , this->authentifier
+                                );
 
         } catch (const Error &) {
             return nullptr;
