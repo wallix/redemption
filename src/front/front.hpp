deb887277c987a1388df4b35f364837b039d8d5b
jonathanpoelen <jonathan.poelen@gmail.com>
2017-04-11 11:52:29 +0200

    front: fixes color in draw(RDPGlyphIndex)

--- src/front/front.hpp
+++ src/front/front.hpp
@@ -4443,9 +4443,9 @@ public:
                     const uint16_t ypix = y * fc.width * 3;
 
                     if (fc_bit_mask & *fc_data) {
-                        this->raw_data[xpix + ypix    ] = color_back.red();
+                        this->raw_data[xpix + ypix    ] = color_back.blue();
                         this->raw_data[xpix + ypix + 1] = color_back.green();
-                        this->raw_data[xpix + ypix + 2] = color_back.blue();
+                        this->raw_data[xpix + ypix + 2] = color_back.red();
                     }
                     fc_bit_mask >>= 1;
                 }
