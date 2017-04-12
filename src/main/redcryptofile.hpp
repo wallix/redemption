6ed52999b3981f9105e487d24abb95eaeb5d9f69
Christophe Grosjean <cgrosjean@wallix.com>
2017-04-12 13:33:24 +0200

    Scafolding for encryption python library

---
+++ src/main/redcryptofile.hpp
@@ -0,0 +1,48 @@
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   Product name: redemption, a FLOSS RDP proxy
+   Copyright (C) Wallix 2017
+   Author(s): Christophe Grosjean, Jonathan Poelen
+
+*/
+
+extern "C" {
+//    REDEMPTION_LIB_EXPORT
+//    void * redcryptofile_open_writer(int encryption, int checksum, char * path,
+//            get_hmac_key_prototype * hmac_fn,
+//            get_trace_key_prototype * trace_fn);
+//            
+//    REDEMPTION_LIB_EXPORT
+//    int redcryptofile_open_reader(char * path,
+//            get_hmac_key_prototype * hmac_fn,
+//            get_trace_key_prototype * trace_fn);
+//            
+//    REDEMPTION_LIB_EXPORT
+//    int redcryptofile_write(void * handle, uint8_t * buffer, size_t len);
+
+//    REDEMPTION_LIB_EXPORT
+//    int redcryptofile_read(void * handle, uint8_t * buffer, size_t len);
+
+//    REDEMPTION_LIB_EXPORT
+//    int redcryptofile_read_qhash(void * handle, char * qhashhex);
+
+//    REDEMPTION_LIB_EXPORT
+//    int redcryptofile_read_fhash(void * handle, char * qhashhex);
+
+//    REDEMPTION_LIB_EXPORT
+//    redcryptofile_close_writer(void * handle, char * qhashhex, char * fhashhex);            
+}
+
---
+++ tests/main/test_redcryptofile.cpp
@@ -0,0 +1,72 @@
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   Product name: redemption, a FLOSS RDP proxy
+   Copyright (C) Wallix 2017
+   Author(s): Christophe Grosjean
+
+*/
+
+#define RED_TEST_MODULE TestRedcryptofile
+#include "system/redemption_unit_tests.hpp"
+
+// #define LOGPRINT
+#define LOGNULL
+#include "utils/log.hpp"
+
+#include "main/redcryptofile.hpp"
+
+extern "C" {
+    int hmac_fn(char * buffer)
+    {
+        // E38DA15E501E4F6A01EFDE6CD9B33A3F2B4172131E975B4C3954231443AE22AE
+        uint8_t hmac_key[SslSha256::DIGEST_LENGTH] = {
+            0xe3, 0x8d, 0xa1, 0x5e, 0x50, 0x1e, 0x4f, 0x6a,
+            0x01, 0xef, 0xde, 0x6c, 0xd9, 0xb3, 0x3a, 0x3f,
+            0x2b, 0x41, 0x72, 0x13, 0x1e, 0x97, 0x5b, 0x4c,
+            0x39, 0x54, 0x23, 0x14, 0x43, 0xae, 0x22, 0xae };
+        memcpy(buffer, hmac_key, SslSha256::DIGEST_LENGTH);
+        return 0;
+    }
+
+    int trace_fn(char * base, int len, char * buffer, unsigned oldscheme)
+    {
+        // in real uses actual trace_key is derived from base and some master key
+        (void)base;
+        (void)len;
+        (void)oldscheme;
+        // 563EB6E8158F0EED2E5FB6BC2893BC15270D7E7815FA804A723EF4FB315FF4B2
+        uint8_t trace_key[SslSha256::DIGEST_LENGTH] = {
+            0x56, 0x3e, 0xb6, 0xe8, 0x15, 0x8f, 0x0e, 0xed,
+            0x2e, 0x5f, 0xb6, 0xbc, 0x28, 0x93, 0xbc, 0x15,
+            0x27, 0x0d, 0x7e, 0x78, 0x15, 0xfa, 0x80, 0x4a,
+            0x72, 0x3e, 0xf4, 0xfb, 0x31, 0x5f, 0xf4, 0xb2
+         };
+        memcpy(buffer, trace_key, sizeof(trace_key));
+        return 0;
+    }
+}
+
+RED_AUTO_TEST_CASE(TestRedCryptofileWriter)
+{
+//    int encryption = 0; // int used as boolean 0 false, true otherwise
+//    int checksum = 0;   // int used as boolean 0 false, true otherwise
+//    
+//    void * handle = redcryptofile_open_writer(encryption, checksum, "./encrypted.txt", trace_fn, hmac_fn);
+//    BOOST_CHECK(handle != nullptr);
+//    int res1 = redcryptofile_write(handle, buffer, len);
+//    BOOST_CHECK_EQUAL(res, len);
+//    int res2 = redcryptofile_close_writer(handle, qhashhex, fhashhex);
+}
