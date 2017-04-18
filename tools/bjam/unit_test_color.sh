7932ce0893780c9e854e26d1c3ea2fdd73a77bfa
jonathanpoelen <jonathan.poelen@gmail.com>
2017-04-18 16:35:08 +0200

    colorized output of RED_CHECK

--- tools/bjam/unit_test_color.sh
+++ tools/bjam/unit_test_color.sh
@@ -2,13 +2,15 @@
 
 esc="$(echo -n "\x1b[")"
 reset=$esc'0m'
-red=$esc'1;31m'
+err=$esc'1;31m'
+cat=
 file=$esc'37m'
-code=$esc'34m'
+code=$esc'31m'
 value=$esc'36m'
 op=$esc'33m'
 name=$esc'35m'
 
 sed -E -e '/^tests\//{
-  s/^(tests\/[^.]+\.cpp)\([0-9]+\): error in "([^\"]+)": ([^ ]+) (.*)( !=|==|<|<=|>|>= )(.*) failed \[(.*)( !=|==|<|<=|>|>= )(.*)\]$/'$file'\1'$reset': '$red'error'$reset' in  "'$name'\2'$reset'": '$red'\3'$reset' '$code'\4'$op'\5'$code'\6'$reset' failed ['$value'\7'$op'\8'$value'\9'$reset']/
+  s/^(tests\/[^.]+\.cpp)\([0-9]+\): error in "([^\"]+)": ([^ ]+) (.*)( !=|==|<|<=|>|>= )(.*) failed \[(.*)( !=|==|<|<=|>|>= )(.*)\]$/'$file'\1'$reset': '$err'error'$reset' in  "'$name'\2'$reset'": '$cat'\3'$reset' '$code'\4'$op'\5'$code'\6'$reset' failed ['$value'\7'$op'\8'$value'\9'$reset']/
+  s/^(tests\/[^.]+\.cpp)\([0-9]+\): error in "([^\"]+)": ([^ ]+) (.*) failed$/'$file'\1'$reset': '$err'error'$reset' in  "'$name'\2'$reset'": '$cat'\3'$reset' '$code'\4'$reset' failed/
 }' "$@"
