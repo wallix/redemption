3aba146ddf21d110a975b193d95319d474193548
jonathanpoelen <jonathan.poelen@gmail.com>
2017-04-18 18:14:21 +0200

    print number lines on error

--- tools/bjam/unit_test_color.sh
+++ tools/bjam/unit_test_color.sh
@@ -11,6 +11,6 @@ op=$esc'33m'
 name=$esc'35m'
 
 sed -E -e '/^tests\//{
-  s/^(tests\/[^.]+\.cpp)\([0-9]+\): error in "([^\"]+)": ([^ ]+) (.*)( !=|==|<|<=|>|>= )(.*) failed \[(.*)( !=|==|<|<=|>|>= )(.*)\]$/'$file'\1'$reset': '$err'error'$reset' in  "'$name'\2'$reset'": '$cat'\3'$reset' '$code'\4'$op'\5'$code'\6'$reset' failed ['$value'\7'$op'\8'$value'\9'$reset']/
-  s/^(tests\/[^.]+\.cpp)\([0-9]+\): error in "([^\"]+)": ([^ ]+) (.*) failed$/'$file'\1'$reset': '$err'error'$reset' in  "'$name'\2'$reset'": '$cat'\3'$reset' '$code'\4'$reset' failed/
+  s/^(tests\/[^.]+\.cpp\([0-9]+\)): error in "([^\"]+)": ([^ ]+) (.*)( !=|==|<|<=|>|>= )(.*) failed \[(.*)( !=|==|<|<=|>|>= )(.*)\]$/'$file'\1'$reset': '$err'error'$reset' in  "'$name'\2'$reset'": '$cat'\3'$reset' '$code'\4'$op'\5'$code'\6'$reset' failed ['$value'\7'$op'\8'$value'\9'$reset']/
+  s/^(tests\/[^.]+\.cpp\([0-9]+\)): error in "([^\"]+)": ([^ ]+) (.*) failed$/'$file'\1'$reset': '$err'error'$reset' in  "'$name'\2'$reset'": '$cat'\3'$reset' '$code'\4'$reset' failed/
 }' "$@"
