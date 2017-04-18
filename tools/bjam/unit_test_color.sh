5eb0ad539058e0e08400150d1d749623df9f98f9
jonathanpoelen <jonathan.poelen@gmail.com>
2017-04-18 18:40:11 +0200

    colorized last checkpoint and fatal error

--- tools/bjam/unit_test_color.sh
+++ tools/bjam/unit_test_color.sh
@@ -3,7 +3,7 @@
 esc="$(echo -n "\x1b[")"
 reset=$esc'0m'
 err=$esc'1;31m'
-cat=
+cat=$esc'34m'
 file=$esc'37m'
 code=$esc'31m'
 value=$esc'36m'
@@ -13,4 +13,7 @@ name=$esc'35m'
 sed -E -e '/^tests\//{
   s/^(tests\/[^.]+\.cpp\([0-9]+\)): error in "([^\"]+)": ([^ ]+) (.*)( !=|==|<|<=|>|>= )(.*) failed \[(.*)( !=|==|<|<=|>|>= )(.*)\]$/'$file'\1'$reset': '$err'error'$reset' in  "'$name'\2'$reset'": '$cat'\3'$reset' '$code'\4'$op'\5'$code'\6'$reset' failed ['$value'\7'$op'\8'$value'\9'$reset']/
   s/^(tests\/[^.]+\.cpp\([0-9]+\)): error in "([^\"]+)": ([^ ]+) (.*) failed$/'$file'\1'$reset': '$err'error'$reset' in  "'$name'\2'$reset'": '$cat'\3'$reset' '$code'\4'$reset' failed/
-}' "$@"
+  s/^(tests\/[^.]+\.cpp\([0-9]+\)): last checkpoint$/'$file'\1'$reset': '$cat'last checkpoint'$reset'/
+}
+s/^unknown location\(0\): fatal error in "([^\"]+)": signal: (.*)$/'$cat'unknown location(0):'$reset' '$err'fatal error'$reset' in "'$name'\1'$reset'": signal: '$op'\2'$reset'/' \
+"$@"
