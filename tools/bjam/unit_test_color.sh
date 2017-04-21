479505afab7e4b67c54d85b4f8822567dc4499a3
jonathanpoelen <jonathan.poelen@gmail.com>
2017-04-21 12:35:10 +0200

    unit_test_color.sh: support of fatal error output

--- tools/bjam/unit_test_color.sh
+++ tools/bjam/unit_test_color.sh
@@ -11,10 +11,15 @@ op=$esc'33m'
 name=$esc'35m'
 
 sed -E -e '/^tests\//{
-  s/^(tests\/[^.]+\.cpp\([0-9]+\)): error in "([^\"]+)": ([^ ]+) (.*)( !=|==|<|<=|>|>= )(.*) failed \[(.*)( !=|==|<|<=|>|>= )(.*)\]$/'$file'\1'$reset': '$err'error'$reset' in  "'$name'\2'$reset'": '$cat'\3'$reset' '$code'\4'$op'\5'$code'\6'$reset' failed ['$value'\7'$op'\8'$value'\9'$reset']/
-  s/^(tests\/[^.]+\.cpp\([0-9]+\)): error in "([^\"]+)": ([^ ]+) (.*) failed$/'$file'\1'$reset': '$err'error'$reset' in  "'$name'\2'$reset'": '$cat'\3'$reset' '$code'\4'$reset' failed/
-  s/^(tests\/[^.]+\.cpp\([0-9]+\)): last checkpoint$/'$file'\1'$reset': '$cat'last checkpoint'$reset'/
-  s/^(tests\/[^.]+\.cpp\([0-9]+\)): fatal error in "([^\"]+)": (critical [^ ]+) (.*) failed$/'$file'\1'$reset': '$err'fatal error'$reset' in "'$name'\2'$reset'": '$cat'\3'$reset' '$code'\4'$reset' failed/
+  s/^(tests\/[^.]+\.cpp\([0-9]+\)): error in "([^\"]+)": check (.*)( !=|==|<=|>=|>|< )(.*) failed \[(.*)( !=|==|<=|>=|>|< )(.*)\]$/'$file'\1'$reset': '$err'error'$reset' in "'$name'\2'$reset'": '$cat'check'$reset' '$code'\3'$op'\4'$code'\5'$reset' failed ['$value'\6'$op'\7'$value'\8'$reset']/;t
+
+  s/^(tests\/[^.]+\.cpp\([0-9]+\)): fatal error in "([^\"]+)": critical check (.*)( !=|==|<=|>=|>|< )(.*) failed \[(.*)( !=|==|<=|>=|>|< )(.*)\]$/'$file'\1'$reset': '$err'fatal error'$reset' in "'$name'\2'$reset'": '$cat'critical check'$reset' '$code'\3'$op'\4'$code'\5'$reset' failed ['$value'\6'$op'\7'$value'\8'$reset']/;t
+
+  s/^(tests\/[^.]+\.cpp\([0-9]+\)): (fatal )?error in "([^\"]+)": (critical )?check (.*) failed$/'$file'\1'$reset': '$err'\2error'$reset' in "'$name'\3'$reset'": '$cat'\4check'$reset' '$code'\5'$reset' failed/;t
+
+  s/^(tests\/[^.]+\.cpp\([0-9]+\)): (fatal )?error in "([^\"]+)": (.*)$/'$file'\1'$reset': '$err'\2error'$reset' in "'$name'\3'$reset'": '$cat'\4'$reset'/;t
+
+  s/^(tests\/[^.]+\.cpp\([0-9]+\)): last checkpoint$/'$file'\1'$reset': '$cat'last checkpoint'$reset'/;t
 }
-s/^unknown location\(0\): fatal error in "([^\"]+)": signal: (.*)$/'$cat'unknown location(0):'$reset' '$err'fatal error'$reset' in "'$name'\1'$reset'": signal: '$op'\2'$reset'/' \
+s/^unknown location\(0\): fatal error in "([^\"]+)": signal: (.*)$/'$cat'unknown location(0):'$reset' '$err'fatal error'$reset' in "'$name'\1'$reset'": signal: '$op'\2'$reset'/;t' \
 "$@"
