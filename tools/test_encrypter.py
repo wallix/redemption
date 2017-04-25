4dbe41e2b86eb3935cfadab549ac94b6432cb0b7
Christophe Grosjean <cgrosjean@wallix.com>
2017-04-25 16:03:27 +0200

    Python tests for file encryption (WIP)

--- tools/test_encrypter.py
+++ tools/test_encrypter.py
@@ -68,6 +68,25 @@ except Exception as e:
     sys.exit(10)
     
     
+#class CryptoWriter(Object):
+#    def __init__(self, encryption, checksum, get_hmac_key_func, get_trace_key_func): 
+#        self.handle = lib.redcryptofile_writer_new(0, 0, get_hmac_key_func, get_trace_key_func)
+#    
+#    def __del__(self):
+#        lib.redcryptofile_writer_delete(self.handle)
+#    
+#    def __enter__(self):
+#        lib.redcryptofile_writer_open(self.handle)
+
+#    def __exit__(self, type, value, traceback):
+#        lib.redcryptofile_writer_close(self.handle)
+#        self.qhashhex = lib.redcryptofile_writer_qhashhex(self.handle).copy()
+#        self.fhashhex = lib.redcryptofile_writer_fhashhex(self.handle).copy()
+#        
+#    def write(self, data, length):
+#        return lib.redcryptofile_writer_write(self.handle, data, length)
+        
+    
 import unittest
     
 class TestEncrypter(unittest.TestCase):
