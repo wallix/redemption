4d37c8aeb98c16dfa0f6163ea5f3fa14bd45362a
Christophe Grosjean <cgrosjean@wallix.com>
2018-02-02 18:32:00 +0100

    Converting vnc encoders to object API (2) : added missing api header

---
+++ src/mod/vnc/encoder/encoder_api.hpp
@@ -0,0 +1,40 @@
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+h
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   Product name: redemption, a FLOSS RDP proxy
+   Copyright (C) Wallix 2018
+   Author(s): Christophe Grosjean
+
+   Vnc encoder module for RAW bitmaps
+*/
+
+#pragma once
+
+#include "utils/log.hpp"
+#include "utils/verbose_flags.hpp"
+#include "mod/vnc/vnc_verbose.hpp"
+#include "core/buf64k.hpp"
+#include "gdi/graphic_api.hpp"
+
+
+namespace VNC {
+    namespace Encoder {
+        class EncoderApi {
+        public:
+            virtual bool consume(Buf64k & buf, gdi::GraphicApi & drawable) = 0;
+            virtual ~EncoderApi() {}
+        };
+    } // namespace encoder
+} // namespace VNC
