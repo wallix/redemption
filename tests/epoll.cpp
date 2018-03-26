69decb043f2855b5aa5455a3517e1bfc9b9c55a1
jonathan poelen <jonathan.poelen@gmail.com>
2018-03-26 18:57:23 +0200

    executor v2: Context::create_sub_executor

--- tests/epoll.cpp
+++ tests/epoll.cpp
@@ -4,7 +4,7 @@
 
 #include <iostream>
 
-enum class R
+enum class [[nodiscard]] R : char
 {
     Next,
     CreateGroup,
@@ -12,14 +12,94 @@ enum class R
     ExitError,
     NeedMoreData,
     Terminate,
-    Substitude,
+    Substitute,
 };
 
 struct TopEvent;
 struct EventBase;
+struct GroupEvent;
+
+enum class ExitStatus : bool {
+    Error,
+    Success,
+};
+
+class [[nodiscard]] SubExecutorBuilder
+{
+    struct D
+    {
+        D(TopEvent& top);
+
+        TopEvent& top;
+        std::unique_ptr<GroupEvent> sub_executor;
+    };
+
+    struct [[nodiscard]] InitMore1
+    {
+        InitMore1(D d)
+        : d(std::move(d))
+        {}
+
+        operator R ();
+
+        template<class F>
+        InitMore1 then(F f);
+
+    private:
+        D d;
+    };
+
+    struct [[nodiscard]] Init1
+    {
+        Init1(D d)
+        : d(std::move(d))
+        {}
+
+        operator R ();
+
+        template<class F>
+        InitMore1 then(F f);
+
+    private:
+        D d;
+    };
+
+public:
+    explicit SubExecutorBuilder(TopEvent& top);
+
+    template<class F>
+    Init1 then(F f);
+
+private:
+    D d;
+};
 
 struct Context
 {
+    SubExecutorBuilder create_sub_executor();
+
+    R need_more_data() noexcept { return R::NeedMoreData; }
+    R terminate() noexcept { return R::Terminate; }
+    R next() noexcept { return R::Next; }
+    R exit_on_error() noexcept { return R::ExitError; }
+    R exit_on_success() noexcept { return R::ExitSuccess; }
+    R exit(ExitStatus status) noexcept {
+        return (status == ExitStatus::Success)
+            ? this->exit_on_success()
+            : this->exit_on_error();
+    }
+
+    template<class F>
+    R replace_action(F f);
+
+private:
+    friend GroupEvent;
+
+    Context(TopEvent& top, EventBase& current)
+    : top(top)
+    , current(current)
+    {}
+
     TopEvent& top;
     EventBase& current;
 };
@@ -31,7 +111,7 @@ struct EventBase
     EventBase* end_next = nullptr;
 
     template<class F>
-    void add(F f)
+    void then(F f)
     {
         if (!this->end_next) {
             this->end_next = this;
@@ -63,12 +143,11 @@ struct GroupEvent : EventBase
       : EventBase(std::move(e))
     {}
 
-    void sub(EventBase e)
+    void add_sub_executor(std::unique_ptr<GroupEvent> g)
     {
-        assert(bool(e.next));
-        auto p = std::make_unique<GroupEvent>(std::move(e));
-        p->group = std::move(this->group);
-        this->group = std::move(p);
+        assert(bool(g->next));
+        g->group = std::move(this->group);
+        this->group = std::move(g);
     }
 
     R exec_f(TopEvent& top, EventBase& e)
@@ -94,7 +173,7 @@ struct GroupEvent : EventBase
                 this->next = std::move(this->next->next);
                 r = R::NeedMoreData;
                 break;
-            case R::Substitude:
+            case R::Substitute:
                 r = R::NeedMoreData;
                 break;
         }
@@ -121,7 +200,7 @@ struct GroupEvent : EventBase
                 case R::NeedMoreData:
                     break;
                 case R::Next:
-                case R::Substitude:
+                case R::Substitute:
                 case R::CreateGroup:
                     assert(false);
             }
@@ -139,7 +218,7 @@ struct GroupEvent : EventBase
                 case R::ExitSuccess:
                 case R::NeedMoreData:
                     break;
-                case R::Substitude:
+                case R::Substitute:
                     r = R::NeedMoreData;
                     break;
                 case R::CreateGroup:
@@ -173,60 +252,103 @@ struct TopEvent : GroupEvent
     }
 };
 
+
+SubExecutorBuilder::D::D(TopEvent& top)
+: top(top)
+, sub_executor(std::make_unique<GroupEvent>())
+{}
+
+SubExecutorBuilder::SubExecutorBuilder(TopEvent& top)
+: d(top)
+{}
+
+template<class F>
+SubExecutorBuilder::Init1 SubExecutorBuilder::then(F f)
+{
+    this->d.sub_executor->then(f);
+    return {std::move(this->d)};
+}
+
+SubExecutorBuilder::Init1::operator R()
+{
+    // TODO static_assert prefered a no group context
+    this->d.top.add_sub_executor(std::move(this->d.sub_executor));
+    return R::CreateGroup;
+}
+
+template<class F>
+SubExecutorBuilder::InitMore1 SubExecutorBuilder::Init1::then(F f)
+{
+    this->d.sub_executor->then(f);
+    return {std::move(this->d)};
+}
+
+SubExecutorBuilder::InitMore1::operator R()
+{
+    this->d.top.add_sub_executor(std::move(this->d.sub_executor));
+    return R::CreateGroup;
+}
+
+template<class F>
+SubExecutorBuilder::InitMore1 SubExecutorBuilder::InitMore1::then(F f)
+{
+    this->d.sub_executor->then(f);
+    return {std::move(this->d)};
+}
+
+
+SubExecutorBuilder Context::create_sub_executor()
+{
+    return SubExecutorBuilder(this->top);
+}
+
+template<class F>
+R Context::replace_action(F f)
+{
+    this->current.f = f;
+    return R::Substitute;
+}
+
+
 #include <iostream>
 
 int main()
 {
     TopEvent e;
-    e.add([](auto /*ctx*/){std::cout << "1\n"; return R::Next; });
-    e.add([](auto ctx){
+    e.then([](Context ctx){std::cout << "1\n"; return ctx.next(); });
+    e.then([](Context ctx){
         std::cout << "2\n";
-        {
-            EventBase sub;
-            sub.add([](auto /*ctx*/){std::cout << "2.1\n"; return R::Next; });
-            sub.add([](auto /*ctx*/){std::cout << "2.2\n"; return R::Next; });
-            ctx.top.sub(std::move(sub));
-        }
-        return R::CreateGroup;
+        return ctx.create_sub_executor()
+        .then([](Context ctx){std::cout << "2.1\n"; return ctx.next(); })
+        .then([](Context ctx){std::cout << "2.2\n"; return ctx.next(); });
+
     });
-    e.add([](auto ctx){
+    e.then([](Context ctx){
         std::cout << "3\n";
-        {
-            EventBase sub;
-            sub.add([](auto ctx){
-                std::cout << "3.1\n";
-                {
-                    EventBase sub;
-                    sub.add([](auto /*ctx*/){std::cout << "3.1.1\n"; return R::Next; });
-                    sub.add([](auto /*ctx*/){std::cout << "3.1.2\n"; return R::Next; });
-                    ctx.top.sub(std::move(sub));
-                }
-                return R::CreateGroup;
-            });
-            ctx.top.sub(std::move(sub));
-        }
-        return R::CreateGroup;
-    });
-    e.add([&](auto ctx){
-        std::cout << "4\n";
-        {
-            EventBase sub;
-            sub.add([](auto /*ctx*/){std::cout << "4.1\n"; return R::Next; });
-            sub.add([](auto /*ctx*/){std::cout << "4.2\n"; return R::ExitSuccess; });
-            sub.add([](auto /*ctx*/){std::cout << "4.3\n"; return R::Next; });
-            sub.add([](auto /*ctx*/){std::cout << "4.4\n"; return R::Next; });
-            ctx.top.sub(std::move(sub));
-        }
-        return R::CreateGroup;
-    });
-    e.add([](auto ctx){
-        std::cout << "5\n";
-        ctx.current.replace([](auto /*ctx*/){
-            std::cout << "5bis\n";
-            return R::Next;
+        return ctx.create_sub_executor()
+        .then([](Context ctx){
+            std::cout << "3.1\n";
+            return ctx.create_sub_executor()
+            .then([](Context ctx){std::cout << "3.1.1\n"; return ctx.next(); })
+            .then([](Context ctx){std::cout << "3.1.2\n"; return ctx.next(); });
         });
-        return R::Substitude;
     });
+    e.then([&](Context ctx){
+        std::cout << "4\n";
+        return ctx.create_sub_executor()
+        .then([](Context ctx){std::cout << "4.1\n"; return ctx.next(); })
+        .then([](Context ctx){std::cout << "4.2\n"; return ctx.exit_on_success(); })
+        .then([](Context ctx){std::cout << "4.3\n"; return ctx.next(); })
+        .then([](Context ctx){std::cout << "4.4\n"; return ctx.next(); });
+    });
+    e.then([](Context ctx){
+        std::cout << "5\n";
+        return ctx.replace_action([](Context ctx){
+            std::cout << "5bis\n";
+            return ctx.next();
+        });
+    });
+
 
     for (int i = 0; ; ++i) {
         if (i >= 22) {
