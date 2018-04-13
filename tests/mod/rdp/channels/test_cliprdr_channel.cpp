b57e05c1a8009251697cb8cfe74a654f5025a8b9
Raphaël ZHOU <rzhou@wallix.com>
2018-04-13 14:47:14 +0200

    refs #16371: Fix exception ERR_RDP_DATA_TRUNCATED during file transfert via clipboard.

--- tests/mod/rdp/channels/test_cliprdr_channel.cpp
+++ tests/mod/rdp/channels/test_cliprdr_channel.cpp
@@ -531,3 +531,173 @@ RED_AUTO_TEST_CASE(TestCliprdrChannelMalformedFormatListPDU)
             out_s.get_data(),
             totalLength);
 }
+
+RED_AUTO_TEST_CASE(TestCliprdrChannelFailedFormatDataResponsePDU)
+{
+    ClientInfo info;
+    info.keylayout             = 0x04C;
+    info.console_session       = 0;
+    info.brush_cache_code      = 0;
+    info.bpp                   = 24;
+    info.width                 = 800;
+    info.height                = 600;
+    info.rdp5_performanceflags = PERF_DISABLE_WALLPAPER;
+    snprintf(info.hostname, sizeof(info.hostname), "test");
+    FakeFront front(info,
+                    511 // verbose
+                   );
+
+    int verbose = static_cast<int>(RDPVerbose::cliprdr | RDPVerbose::cliprdr_dump);
+
+    NullReportMessage report_message;
+    ClipboardVirtualChannel::Params clipboard_virtual_channel_params(report_message);
+
+    clipboard_virtual_channel_params.exchanged_data_limit      = 0;
+    clipboard_virtual_channel_params.verbose                   = to_verbose_flags(verbose);
+
+    clipboard_virtual_channel_params.clipboard_down_authorized = true;
+    clipboard_virtual_channel_params.clipboard_up_authorized   = true;
+    clipboard_virtual_channel_params.clipboard_file_authorized = true;
+
+    clipboard_virtual_channel_params.dont_log_data_into_syslog = false;
+    clipboard_virtual_channel_params.dont_log_data_into_wrm    = false;
+
+    NullSender to_client_sender;
+    NullSender to_server_sender;
+
+    ClipboardVirtualChannel clipboard_virtual_channel(
+        &to_client_sender, &to_server_sender, front,
+        clipboard_virtual_channel_params);
+
+// ClipboardVirtualChannel::process_server_message: total_length=28 flags=0x00000003 chunk_data_length=28
+// Recv done on channel (28) n bytes
+// /* 0000 */ "\x01\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x1c\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x03\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x1c\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x07\x00\x00\x00\x10\x00\x00\x00\x01\x00\x00\x00\x01\x00\x0c\x00" // ................
+// /* 0010 */ "\x02\x00\x00\x00\x1e\x00\x00\x00\x00\x00\x00\x00"                 // ............
+// Dump done on channel (28) n bytes
+// ClipboardVirtualChannel::process_server_message: Clipboard Capabilities PDU
+// ClipboardVirtualChannel::process_server_clipboard_capabilities_pdu: General Capability Set
+// RDPECLIP::GeneralCapabilitySet: capabilitySetType=CB_CAPSTYPE_GENERAL(1) lengthCapability=12 version=CB_CAPS_VERSION_2(0x00000002) generalFlags=0x0000001E
+
+    std::unique_ptr<AsynchronousTask> out_asynchronous_task;
+
+    clipboard_virtual_channel.process_server_message(
+            28,
+              CHANNELS::CHANNEL_FLAG_FIRST
+            | CHANNELS::CHANNEL_FLAG_LAST
+            | CHANNELS::CHANNEL_FLAG_SHOW_PROTOCOL,
+            byte_ptr_cast(
+        /* 0000 */ "\x07\x00\x00\x00\x10\x00\x00\x00\x01\x00\x00\x00\x01\x00\x0c\x00" // ................
+        /* 0010 */ "\x02\x00\x00\x00\x1e\x00\x00\x00\x00\x00\x00\x00"                 // ............
+                ),
+            28,
+            out_asynchronous_task);
+
+// ClipboardVirtualChannel::process_client_message: total_length=24 flags=0x00000013 chunk_data_length=24
+// Recv done on channel (24) n bytes
+// /* 0000 */ "\x00\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x18\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x13\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x18\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x07\x00\x00\x00\x10\x00\x00\x00\x01\x00\x00\x00\x01\x00\x0c\x00" // ................
+// /* 0010 */ "\x02\x00\x00\x00\x1e\x00\x00\x00"                                 // ........
+// Dump done on channel (24) n bytes
+// ClipboardVirtualChannel::process_client_message: Clipboard Capabilities PDU
+// ClipboardVirtualChannel::process_client_clipboard_capabilities_pdu: General Capability Set
+// RDPECLIP::GeneralCapabilitySet: capabilitySetType=CB_CAPSTYPE_GENERAL(1) lengthCapability=12 version=CB_CAPS_VERSION_2(0x00000002) generalFlags=0x0000001E
+
+    clipboard_virtual_channel.process_client_message(
+            24,
+              CHANNELS::CHANNEL_FLAG_FIRST
+            | CHANNELS::CHANNEL_FLAG_LAST
+            | CHANNELS::CHANNEL_FLAG_SHOW_PROTOCOL,
+            byte_ptr_cast(
+    /* 0000 */ "\x07\x00\x00\x00\x10\x00\x00\x00\x01\x00\x00\x00\x01\x00\x0c\x00" // ................
+    /* 0010 */ "\x02\x00\x00\x00\x1e\x00\x00\x00"                                 // ........
+                ),
+            24);
+
+// ClipboardVirtualChannel::process_client_message: total_length=130 flags=0x00000013 chunk_data_length=130
+// Recv done on channel (130) n bytes
+// /* 0000 */ "\x00\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x82\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x13\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x82\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x02\x00\x00\x00\x7a\x00\x00\x00\x6e\xc0\x00\x00\x46\x00\x69\x00" // ....z...n...F.i.
+// /* 0010 */ "\x6c\x00\x65\x00\x47\x00\x72\x00\x6f\x00\x75\x00\x70\x00\x44\x00" // l.e.G.r.o.u.p.D.
+// /* 0020 */ "\x65\x00\x73\x00\x63\x00\x72\x00\x69\x00\x70\x00\x74\x00\x6f\x00" // e.s.c.r.i.p.t.o.
+// /* 0030 */ "\x72\x00\x57\x00\x00\x00\x94\xc0\x00\x00\x46\x00\x69\x00\x6c\x00" // r.W.......F.i.l.
+// /* 0040 */ "\x65\x00\x43\x00\x6f\x00\x6e\x00\x74\x00\x65\x00\x6e\x00\x74\x00" // e.C.o.n.t.e.n.t.
+// /* 0050 */ "\x73\x00\x00\x00\x10\xc1\x00\x00\x50\x00\x72\x00\x65\x00\x66\x00" // s.......P.r.e.f.
+// /* 0060 */ "\x65\x00\x72\x00\x72\x00\x65\x00\x64\x00\x20\x00\x44\x00\x72\x00" // e.r.r.e.d. .D.r.
+// /* 0070 */ "\x6f\x00\x70\x00\x45\x00\x66\x00\x66\x00\x65\x00\x63\x00\x74\x00" // o.p.E.f.f.e.c.t.
+// /* 0080 */ "\x00\x00"                                                         // ..
+// Dump done on channel (130) n bytes
+// ClipboardVirtualChannel::process_client_message: Format List PDU
+// ClipboardVirtualChannel::process_client_format_list_pdu: Long Format Name variant of Format List PDU is used for exchanging updated format names.
+// ClipboardVirtualChannel::process_client_format_list_pdu: formatId=<unknown>(49262) wszFormatName="FileGroupDescriptorW"
+// ClipboardVirtualChannel::process_client_format_list_pdu: formatId=<unknown>(49300) wszFormatName="FileContents"
+// ClipboardVirtualChannel::process_client_format_list_pdu: formatId=<unknown>(49424) wszFormatName="Preferred DropEffect"
+
+    clipboard_virtual_channel.process_client_message(
+            130,
+              CHANNELS::CHANNEL_FLAG_FIRST
+            | CHANNELS::CHANNEL_FLAG_LAST
+            | CHANNELS::CHANNEL_FLAG_SHOW_PROTOCOL,
+            byte_ptr_cast(
+        /* 0000 */ "\x02\x00\x00\x00\x7a\x00\x00\x00\x6e\xc0\x00\x00\x46\x00\x69\x00" // ....z...n...F.i.
+        /* 0010 */ "\x6c\x00\x65\x00\x47\x00\x72\x00\x6f\x00\x75\x00\x70\x00\x44\x00" // l.e.G.r.o.u.p.D.
+        /* 0020 */ "\x65\x00\x73\x00\x63\x00\x72\x00\x69\x00\x70\x00\x74\x00\x6f\x00" // e.s.c.r.i.p.t.o.
+        /* 0030 */ "\x72\x00\x57\x00\x00\x00\x94\xc0\x00\x00\x46\x00\x69\x00\x6c\x00" // r.W.......F.i.l.
+        /* 0040 */ "\x65\x00\x43\x00\x6f\x00\x6e\x00\x74\x00\x65\x00\x6e\x00\x74\x00" // e.C.o.n.t.e.n.t.
+        /* 0050 */ "\x73\x00\x00\x00\x10\xc1\x00\x00\x50\x00\x72\x00\x65\x00\x66\x00" // s.......P.r.e.f.
+        /* 0060 */ "\x65\x00\x72\x00\x72\x00\x65\x00\x64\x00\x20\x00\x44\x00\x72\x00" // e.r.r.e.d. .D.r.
+        /* 0070 */ "\x6f\x00\x70\x00\x45\x00\x66\x00\x66\x00\x65\x00\x63\x00\x74\x00" // o.p.E.f.f.e.c.t.
+        /* 0080 */ "\x00\x00"                                                         // ..
+                ),
+            130);
+
+// ClipboardVirtualChannel::process_server_format_data_request_pdu: requestedFormatId=<unknown>(49262)
+// Sending on channel (16) n bytes
+// /* 0000 */ "\x00\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x10\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x03\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x10\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x04\x00\x00\x00\x04\x00\x00\x00\x6e\xc0\x00\x00\x00\x00\x00\x00" // ........n.......
+// Sent dumped on channel (16) n bytes
+// CLIPRDR CB_FORMAT_DATA_RESPONSE format data id = 49262 <unknown>
+
+    clipboard_virtual_channel.process_server_message(
+            16,
+              CHANNELS::CHANNEL_FLAG_FIRST
+            | CHANNELS::CHANNEL_FLAG_LAST
+            | CHANNELS::CHANNEL_FLAG_SHOW_PROTOCOL,
+            byte_ptr_cast(
+        /* 0000 */ "\x04\x00\x00\x00\x04\x00\x00\x00\x6e\xc0\x00\x00\x00\x00\x00\x00"
+                ),
+            16,
+            out_asynchronous_task);
+
+// ClipboardVirtualChannel::process_client_message: total_length=8 flags=0x00000013 chunk_data_length=8
+// Recv done on channel (8) n bytes
+// /* 0000 */ "\x00\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x08\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x13\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x08\x00\x00\x00"                                                 // ....
+// /* 0000 */ "\x05\x00\x02\x00\x00\x00\x00\x00"                                 // ........
+// Dump done on channel (8) n bytes
+// ClipboardVirtualChannel::process_client_message: Format Data Response PDU
+
+    clipboard_virtual_channel.process_client_message(
+            8,
+              CHANNELS::CHANNEL_FLAG_FIRST
+            | CHANNELS::CHANNEL_FLAG_LAST
+            | CHANNELS::CHANNEL_FLAG_SHOW_PROTOCOL,
+            byte_ptr_cast(
+        /* 0000 */ "\x05\x00\x02\x00\x00\x00\x00\x00"
+                ),
+            8);
+}
