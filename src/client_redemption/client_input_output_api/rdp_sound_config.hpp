9cdc6833004291c7c0f68cfe15e10ee401c9c144
Pianna <moroldo.clement@gmail.com>
2018-06-12 11:01:48 +0200

    update src/client_redemption/client_input_output_api/rdp_sound_config.hpp

--- src/client_redemption/client_input_output_api/rdp_sound_config.hpp
+++ src/client_redemption/client_input_output_api/rdp_sound_config.hpp
@@ -27,6 +27,13 @@
 
 struct RDPSoundConfig {
 
+    uint32_t dwFlags = rdpsnd::TSSNDCAPS_ALIVE | rdpsnd::TSSNDCAPS_VOLUME;
+    uint32_t dwVolume = 0x7fff7fff;
+    uint32_t dwPitch = 0;
+    uint16_t wDGramPort = 0;
+    uint16_t wNumberOfFormats = 1;
+    uint16_t wVersion = 0x06;
+		
 };
 
 	
