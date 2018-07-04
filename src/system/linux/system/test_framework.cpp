3b4efb08c82146c4ad8f16f3da797cc0c08230d4
Christophe Grosjean <cgrosjean@wallix.com>
2018-07-04 18:25:29 +0200

    Fixed unit tests (not compiling any more with recent versions of boost)

--- src/system/linux/system/test_framework.cpp
+++ src/system/linux/system/test_framework.cpp
@@ -1,9 +1,9 @@
 #define BOOST_AUTO_TEST_MAIN
 #define BOOST_TEST_DYN_LINK
 
-#include <boost/test/included/unit_test.hpp>
 
 #include <boost/test/unit_test_monitor.hpp>
+#include <boost/test/included/unit_test.hpp>
 
 #include <cstdio>
 #include "core/error.hpp"
