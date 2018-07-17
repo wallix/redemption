49a85deb546d70eae224118469864362cfbca6fe
Jonathan Poelen <jpoelen@wallix.com>
2018-07-17 17:35:47 +0200

    PngCaptureRT: log enable/disable rt_display

--- src/capture/capture.cpp
+++ src/capture/capture.cpp
@@ -734,6 +734,7 @@ class PngCaptureRT : public PngCapture
     unsigned png_limit;
 
     bool enable_rt_display;
+    bool enable_verbose;
 
     SmartVideoCropping smart_video_cropping;
 
@@ -745,6 +746,7 @@ public:
     , num_start(this->trans.get_seqno())
     , png_limit(png_params.png_limit)
     , enable_rt_display(png_params.rt_display)
+    , enable_verbose(capture_params.verbose)
     , smart_video_cropping(capture_params.smart_video_cropping)
     {
     }
@@ -755,6 +757,9 @@ public:
 
     void update_config(bool enable_rt_display) {
         if (enable_rt_display != this->enable_rt_display){
+            if (this->enable_verbose) {
+                LOG(LOG_INFO, "PngCaptureRT::enable_rt_display=%d", enable_rt_display);
+            }
             this->enable_rt_display = enable_rt_display;
             // clear files if we go from RT to non-RT
             if (!this->enable_rt_display) {
