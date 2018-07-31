ad7a514f95064a51c956718e7ede6e0df52a2199
Christophe Grosjean <cgrosjean@wallix.com>
2018-07-31 16:00:42 +0200

    Fixed some reinterpret_cast warnings

--- src/core/RDP/mppc/mppc_utils.hpp
+++ src/core/RDP/mppc/mppc_utils.hpp
@@ -125,7 +125,7 @@ struct rdp_mppc_enc_hash_table_manager
     {
         LOG(LOG_INFO, "Type=RDP X.X bulk compressor hash table manager");
         LOG(LOG_INFO, "hashTable");
-        hexdump_d(reinterpret_cast<uint8_t const*>(this->hash_table.get()), (mini_dump ? 16 : get_table_size()));
+        hexdump_d(byte_ptr_cast(this->hash_table.get()), (mini_dump ? 16 : get_table_size()));
     }
 
     inline T get_offset(hash_type hash) const
