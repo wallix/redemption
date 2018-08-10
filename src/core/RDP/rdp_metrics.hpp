e9af0003865c6a5b35c13116ff97f1e4887ec73d
Pianna <moroldo.clement@gmail.com>
2018-08-10 12:19:53 +0200

    add metrics_api

--- src/core/RDP/rdp_metrics.hpp
+++ src/core/RDP/rdp_metrics.hpp
@@ -20,37 +20,25 @@
 
 #pragma once
 
-#include <cstdio>
-#include <ctime>
-#include <cstring>
-#include <fcntl.h>
-#include <sys/uio.h>
+// #include <cstdio>
+// #include <ctime>
+// #include <cstring>
+// #include <fcntl.h>
+// #include <sys/uio.h>
+//
+// #include "utils/sugar/unique_fd.hpp"
+// #include "utils/log.hpp"
+// #include "utils/difftimeval.hpp"
+// #include "utils/texttime.hpp"
+//
+// #include "system/linux/system/ssl_sha256.hpp"
 
-#include "utils/sugar/unique_fd.hpp"
-#include "utils/log.hpp"
-#include "utils/difftimeval.hpp"
-#include "utils/texttime.hpp"
+#include "core/RDP/metrics_api.hpp"
+
 #include "core/client_info.hpp"
-#include "system/linux/system/ssl_sha256.hpp"
-
 #include "core/RDP/clipboard.hpp"
 #include "core/RDP/channels/rdpdr.hpp"
 
-std::string metrics_encrypt(const char * src, const size_t src_len, const unsigned char * key_crypt);
-
-
-inline std::string hmac_user(const std::string & user, const unsigned char * key) {
-    return metrics_encrypt(user.c_str(), user.length(), key);
-}
-
-inline std::string hmac_account(const std::string & account, const unsigned char * key) {
-    return metrics_encrypt(account.c_str(), account.length(), key);
-}
-
-inline std::string hmac_device_service(const std::string & device, const std::string & service, const unsigned char * key) {
-    std::string target_device_and_service(service+" "+device);
-    return metrics_encrypt(target_device_and_service.c_str(), target_device_and_service.length(), key);
-}
 
 inline std::string hmac_client_info(const char * client_host, const ClientInfo & info, const unsigned char * key) {
     char session_info[1024];
@@ -59,31 +47,10 @@ inline std::string hmac_client_info(const char * client_host, const ClientInfo &
 }
 
 
-inline std::string metrics_encrypt(const char * src, const size_t src_len, const unsigned char * key_crypt) {
-    char res[SslSha256::DIGEST_LENGTH*2];
-    char * dest = res;
-    SslHMAC_Sha256 sha256(key_crypt, 32);
-    sha256.update(byte_ptr_cast(src), src_len);
-    uint8_t sig[SslSha256::DIGEST_LENGTH];
-    sha256.final(sig);
-
-    unsigned char * pin = sig;
-    const char * hex = "0123456789ABCDEF";
-    for(; pin < &sig[32]; dest+=2, pin++){
-        dest[0] = hex[(*pin>>4) & 0xF];
-        dest[1] = hex[ *pin     & 0xF];
-    }
-
-    return std::string (res, 64);
-}
-
-
-class RDPMetrics
+class RDPMetrics : public MetricsApi
 {
 
 private:
-
-    const char * version = "v1.0";                  // fields version
 
     enum : int {
 
@@ -173,56 +140,18 @@ private:
         return " unknow_rdp_metrics_name";
     }
 
-    //  output file info
-    const std::chrono::seconds file_interval;
-    time_t current_file_date;
-    char complete_file_path[4096] = {'\0'};
-    const std::string path;
-    unique_fd fd = invalid_fd();
-
-    // LOG info
-    char header[1024];
-    const char * session_id;
-    const bool active_ = false;
-    uint64_t current_data[COUNT_FIELD] = { 0 };
-
-    const time_t connection_time;
-
-    const std::chrono::seconds log_delay;
-    timeval next_log_time;
-
     // RDP context Info
+    uint64_t current_data[COUNT_FIELD] = { 0 };
     int last_x = -1;
     int last_y = -1;
     uint32_t file_contents_format_ID = 0;
     uint32_t last_formatID = 0;
     bool cliprdr_init_format_list_done = false;
 
-   const bool debug;
 
-
-    void encrypt(char * dest, const char * src, const size_t src_len, const unsigned char * key_crypt) {
-        SslHMAC_Sha256 sha256(key_crypt, 32);
-        sha256.update(byte_ptr_cast(src), src_len);
-        uint8_t sig[SslSha256::DIGEST_LENGTH];
-        sha256.final(sig);
-
-        unsigned char * pin = sig;
-        const char * hex = "0123456789ABCDEF";
-        for(; pin < &sig[32]; dest+=2, pin++){
-            dest[0] = hex[(*pin>>4) & 0xF];
-            dest[1] = hex[ *pin     & 0xF];
-        }
-        dest[64] = 0;
-    }
 
 
 public:
-
-    bool active() {
-        return this->active_;
-    }
-
     RDPMetrics( const bool activate                        // do nothing if false
               , std::string path
               , const char * session_id
@@ -234,58 +163,8 @@ public:
               , const std::chrono::hours file_interval     // daily rotation of filename (hours)
               , const std::chrono::seconds log_delay       // delay between 2 logs
               , const bool debug = false
-      )
-      : file_interval{file_interval}
-      , current_file_date(now-now%(this->file_interval.count()))
-      , path(std::move(path))
-      , session_id(session_id)
-      , active_(activate)
-      , connection_time(now)
-      , log_delay(log_delay)
-      , next_log_time{ this->log_delay.count()+now, 0}
-      , debug(debug)
-    {
-        ::snprintf(header, sizeof(header), "%s user=%s account=%s target_service_device=%s client_info=%s\n", this->session_id, primary_user_sig.c_str(), account_sig.c_str(), target_service_sig.c_str(), session_info_sig.c_str());
+      ) : MetricsApi("v1.0", "rdp", activate, path, session_id, primary_user_sig, account_sig, target_service_sig, session_info_sig, now, file_interval, log_delay, debug) {}
 
-        if (this->path.c_str() && activate) {
-            std::string dir_path = this->path.substr(0, this->path.length()-1);
-            int mkdir_res = mkdir(dir_path.c_str(), ACCESSPERMS);
-            if (mkdir_res == -1) {
-                LOG(LOG_ERR, "Impossible to create directory at %s, error(%d)", dir_path, mkdir_res);
-            }
-
-            this->new_day(this->current_file_date);
-        }
-    }
-
-
-    ~RDPMetrics() {
-         this->disconnect();
-    }
-
-    void disconnect() {
-
-        this->rotate(this->next_log_time.tv_sec);
-
-        std::string text_date = ((this->current_file_date % (24*3600)) == 0)?text_gmdate(this->current_file_date):filename_gmdatetime(this->current_file_date);
-
-        char index_file_path[1024];
-        ::snprintf(index_file_path, sizeof(index_file_path), "%s/rdp_metrics-%s-%s.logindex", path.c_str(), this->version, text_date.c_str());
-
-        char header_disconnection[2048];
-        ::snprintf(header_disconnection, sizeof(header_disconnection), "%s disconnection %s", text_gmdatetime(this->next_log_time.tv_sec).c_str(), this->header);
-
-        unique_fd fd_header(index_file_path, O_WRONLY | O_APPEND | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO );
-
-        iovec iov[1] = { {header_disconnection, strlen(header_disconnection)} };
-
-        ssize_t nwritten = ::writev(fd_header.fd(), iov, 1);
-
-        if (nwritten == -1) {
-            // TODO bad filename
-            LOG(LOG_ERR, "Log Metrics error(%d): can't write \"%s\"", fd_header.fd(), this->complete_file_path);
-        }
-    }
 
     void add_to_current_data(int index, uint64_t value) {
         this->current_data[index] += value;
@@ -610,61 +489,7 @@ public:
         this->add_to_current_data(main_channel_data_from_client, len);
     }
 
-
-    void new_day(time_t now) {
-        std::string text_date = ((now % (24*3600)) == 0)?text_gmdate(now):filename_gmdatetime(now);
-
-        ::snprintf(this->complete_file_path, sizeof(this->complete_file_path), "%s/rdp_metrics-%s-%s.logmetrics", this->path.c_str(), this->version, text_date.c_str());
-
-        this->fd = unique_fd(this->complete_file_path, O_WRONLY | O_APPEND | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO);
-        if (!this->fd.is_open()) {
-            LOG(LOG_ERR, "Log Metrics error(%d): can't open \"%s\"", this->fd.fd(), this->complete_file_path);
-        }
-
-        char index_file_path[1024];
-        ::snprintf(index_file_path, sizeof(index_file_path), "%s/rdp_metrics-%s-%s.logindex", path.c_str(), this->version, text_date.c_str());
-
-        char connection_header[1036];
-        ::snprintf(connection_header, sizeof(connection_header), "%s connection %s", text_gmdatetime(this->connection_time).c_str(), this->header);
-
-        unique_fd fd_header(index_file_path, O_WRONLY | O_APPEND | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO );
-
-            if (fd_header.fd() != -1) {
-                iovec iov[1] = { {connection_header, strlen(connection_header)} };
-
-                ssize_t nwritten = ::writev(fd_header.fd(), iov, 1);
-
-            if (nwritten == -1) {
-                // TODO bad filename
-                LOG(LOG_ERR, "Log Metrics error(%d): can't write in\"%s\"",this->fd.fd(), index_file_path);
-            }
-        } else {
-            LOG(LOG_ERR, "Log Metrics error(%d): can't write in\"%s\"",this->fd.fd(), index_file_path);
-        }
-    }
-
-    void rotate(time_t now) {
-
-        time_t next_file_date = now - now%(this->file_interval.count());
-        if (this->current_file_date != next_file_date) {
-            this->current_file_date = next_file_date;
-
-            this->new_day(next_file_date);
-        }
-    }
-
-
-    void log(timeval & now) {
-
-        timeval wait_log_metrics = ::how_long_to_wait(this->next_log_time, now);
-        if (this->active_ && !wait_log_metrics.tv_sec && ! wait_log_metrics.tv_usec) {
-            this->next_log_time.tv_sec += this->log_delay.count();
-            this->next_log_time.tv_usec = now.tv_usec;
-
-            this->rotate(now.tv_sec);
-
-            std::string text_datetime(text_gmdatetime(now.tv_sec));
-
+    virtual void write_log(std::string & text_datetime, char * sentence, size_t sentence_max_size/*4096*/) override {
             if (debug) {
 
                 std::string debug_sentence;
@@ -683,17 +508,6 @@ public:
                 LOG(LOG_INFO, "%s", debug_sentence);
             }
 
-            char sentence[4096];
-            ::snprintf(sentence, sizeof(sentence), "%s %s %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu\n", text_datetime.c_str(), this->session_id, current_data[0], current_data[1], current_data[2], current_data[3], current_data[4], current_data[5], current_data[6], current_data[7], current_data[8], current_data[9], current_data[10], current_data[11], current_data[12], current_data[13], current_data[14], current_data[15], current_data[16], current_data[17], current_data[18], current_data[19], current_data[20], current_data[21], current_data[22], current_data[23], current_data[24], current_data[25], current_data[26], current_data[27], current_data[28], current_data[29], current_data[30], current_data[31], current_data[32], current_data[33]);
-
-            iovec iov[] = { {sentence, strlen(sentence)} };
-
-            ssize_t nwritten = ::writev(this->fd.fd(), iov, 1);
-
-            if (nwritten == -1) {
-                // TODO bad filename
-                LOG(LOG_ERR, "Log Metrics error(%d): can't write in\"%s\"", this->fd.fd(), this->complete_file_path);
-            }
-        }
+            ::snprintf(sentence, sentence_max_size, "%s %s %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu %lu\n", text_datetime.c_str(), this->session_id, current_data[0], current_data[1], current_data[2], current_data[3], current_data[4], current_data[5], current_data[6], current_data[7], current_data[8], current_data[9], current_data[10], current_data[11], current_data[12], current_data[13], current_data[14], current_data[15], current_data[16], current_data[17], current_data[18], current_data[19], current_data[20], current_data[21], current_data[22], current_data[23], current_data[24], current_data[25], current_data[26], current_data[27], current_data[28], current_data[29], current_data[30], current_data[31], current_data[32], current_data[33]);
     }
 };
