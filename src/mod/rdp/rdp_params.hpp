41546c333ad0b82057515cb87fba73fbc73fd228
Christophe Grosjean <cgrosjean@wallix.com>
2018-11-14 17:06:59 +0100

    Backporting AI Metrics (WIP, vnc metrics and periodic snapshot still missing)

--- src/mod/rdp/rdp_params.hpp
+++ src/mod/rdp/rdp_params.hpp
@@ -44,12 +44,7 @@ using ModRdpVariables = vcfg::variables<
     vcfg::var<cfg::context::session_id,                        vcfg::accessmode::get>,
     vcfg::var<cfg::globals::auth_user,                         vcfg::accessmode::get>,
     vcfg::var<cfg::globals::host,                              vcfg::accessmode::get>,
-    vcfg::var<cfg::globals::target_device,                     vcfg::accessmode::get>,
-    vcfg::var<cfg::rdp_metrics::log_dir_path,                  vcfg::accessmode::get>,
-    vcfg::var<cfg::rdp_metrics::log_file_turnover_interval,    vcfg::accessmode::get>,
-    vcfg::var<cfg::rdp_metrics::sign_key,                      vcfg::accessmode::get>,
-    vcfg::var<cfg::rdp_metrics::activate_log_metrics,          vcfg::accessmode::get>,
-    vcfg::var<cfg::rdp_metrics::log_interval,                  vcfg::accessmode::get>
+    vcfg::var<cfg::globals::target_device,                     vcfg::accessmode::get>
 >;
 
 struct ModRDPParams {
