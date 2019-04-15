c84a21af309f42466ec6fbee6d1ab56a7be5aa58
Christophe Grosjean <cgrosjean@wallix.com>
2019-04-15 12:58:26 +0200

    Added missing files in proxy_recorder

---
+++ tests/proxy_recorder/test_proxy_recorder.cpp
@@ -0,0 +1,32 @@
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   Product name: redemption, a FLOSS RDP proxy
+   Copyright (C) Wallix 2019
+   Author(s): Christophe Grosjean
+
+*/
+
+#include "test_only/test_framework/redemption_unit_tests.hpp"
+
+RED_AUTO_TEST_CASE(TestNLARedemptionToWindows2012DC)
+{
+    RED_CHECK(1);
+}
+
+RED_AUTO_TEST_CASE(TestNLAWindows7ClientToRedemption)
+{
+    RED_CHECK(1);
+}
