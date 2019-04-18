41c354423a63bac456b2e0261536f4c2cc4ab31e
Jonathan Poelen <jpoelen@wallix.com>
2019-04-18 17:03:45 +0200

    synchronise sesman with wab/sesman

--- tools/sesman/sesmanworker/__init__.py
+++ tools/sesman/sesmanworker/__init__.py
@@ -1,1 +1,14 @@
-from sesman import Sesman
+#!/usr/bin/python -O
+
+##
+# Copyright (c) 2017 WALLIX. All rights reserved.
+# Licensed computer software. Property of WALLIX. 
+# Product Name: WALLIX Bastion v6.0
+# Author(s): Olivier Hervieu
+# Id: $Id$ 
+# URL: $URL$ 
+# Module description: init script for RDPWorker Module
+##
+
+from __future__ import absolute_import
+from .sesman import * # pyflakes:ignore
