65442ac0366c28a3ea79e6eee854109cf1d3a4e2
Jonathan Poelen <jpoelen@wallix.com>
2019-05-17 12:10:06 +0200

    [jsclient] drawLineTo: preferred canvas.fillRect with vertical/horizontal line (strange behavior with canvas.stroke())

--- projects/browser_client_JS/src/application/rdp_graphics.js
+++ projects/browser_client_JS/src/application/rdp_graphics.js
@@ -51,42 +51,62 @@ class RDPGraphics
         }
     }
 
-    drawLineTo(backMode, nXStart, nYStart, nXEnd, nYEnd, backColor, penStyle, penWidth, penColor) {
+    drawLineTo(backMode, startX, startY, endX, endY, backColor, penStyle, penWidth, penColor) {
         // console.log('drawLineTo');
 		this.canvas.save();
-		this.canvas.beginPath();
-		this.canvas.moveTo(nXStart, nYStart);
-		this.canvas.lineTo(nXEnd, nYEnd);
-		this.canvas.lineWidth = penWidth;
 		this.canvas.fillStyle = rgbToCss(backColor);
 		this.canvas.strokeStyle = rgbToCss(penColor);
-		switch (penStyle) {
-            case 1: this.canvas.setLineDash([ 10, 6, 10, 6 ]); break;
-            case 2: this.canvas.setLineDash([ 3, 3, 3, 3 ]); break;
-            case 3: this.canvas.setLineDash([ 9, 6, 3, 6 ]); break;
-            case 4: this.canvas.setLineDash([ 9, 3, 3, 3 ]); break;
-            default: this.canvas.setLineDash([ 16, 0, 16, 0 ]);
-		}
-		this.canvas.setLineDash([ 16, 0, 16, 0 ]);
-		// BackMode does not imply the transparency level of what is about too be drawn
-        // canvas.globalAlpha = (backMode == 1 /* TRANSPARENT */? 0.0 : 1.0);
-		this.canvas.stroke();
+        // behavior of stroke is strange (transparency color with a odd penWidth)
+        if (!penStyle && startX == endX) {
+            if (endX < startX) {
+                startX = endX;
+            }
+            if (endY < startY) {
+                [startY, endY] = [endY, startY];
+            }
+            this.canvas.fillRect(startX, startY, penWidth||1, endY-startY+1);
+        }
+        else if (!penStyle && startY == endY) {
+            if (endY < startY) {
+                startY = endY;
+            }
+            if (endX < startX) {
+                [startX, endX] = [endX, startX];
+            }
+            this.canvas.fillRect(startX, startY, endX-startX+1, penWidth||1);
+        }
+        else {
+            this.canvas.beginPath();
+            this.canvas.moveTo(startX, startY);
+            this.canvas.lineTo(endX, endY);
+            this.canvas.lineWidth = penWidth;
+            switch (penStyle) {
+                case 1: this.canvas.setLineDash([ 10, 6, 10, 6 ]); break;
+                case 2: this.canvas.setLineDash([ 3, 3, 3, 3 ]); break;
+                case 3: this.canvas.setLineDash([ 9, 6, 3, 6 ]); break;
+                case 4: this.canvas.setLineDash([ 9, 3, 3, 3 ]); break;
+                case 5: this.canvas.setLineDash([ 16, 0, 16, 0 ]); break;
+            }
+            // BackMode does not imply the transparency level of what is about too be drawn
+            // canvas.globalAlpha = (backMode == 1 /* TRANSPARENT */? 0.0 : 1.0);
+            this.canvas.stroke();
+        }
 		this.canvas.restore();
     }
 
-    drawPolyline(xStart, yStart, deltas, penColor) {
+    drawPolyline(startX, startY, deltas, penColor) {
         // console.log('drawPolyline');
 		this.canvas.save();
 		this.canvas.strokeStyle = rgbToCss(penColor);
 		this.canvas.beginPath();
-		this.canvas.moveTo(xStart, yStart);
-        let endx = xStart;
-        let endy = yStart;
+		this.canvas.moveTo(startX, startY);
+        let endX = startX;
+        let endY = startY;
         const iend = deltas.length
 		for (let i = 0; i < iend; i += 2) {
-			endx += deltas[i];
-			endy += deltas[i+1];
-			this.canvas.lineTo(endx, endy);
+			endX += deltas[i];
+			endY += deltas[i+1];
+			this.canvas.lineTo(endX, endY);
 		}
 		this.canvas.stroke();
 		this.canvas.restore();
