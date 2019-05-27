67f6148e4b795a47bf038dd8a29399be6ecff74c
Jonathan Poelen <jpoelen@wallix.com>
2019-05-27 18:19:18 +0200

    [config] generator: fix " in a description. Generate a raw string for autogen/str_*

--- projects/redemption_configs/configs_specs/configs/specs/config_spec.hpp
+++ projects/redemption_configs/configs_specs/configs/specs/config_spec.hpp
@@ -186,7 +186,7 @@ void config_spec_definition(Writer && W)
         W.member(ini_and_gui, no_sesman, L, type_<bool>(), "enable_arcsight_log", set(false));
         W.member(hidden_in_gui, sesman_to_proxy, is_target_ctx, L, type_<std::string>(), "log_path", sesman::name{"session_log_path"});
         W.member(hidden_in_gui, rdp_connpolicy, L, type_<KeyboardInputMaskingLevel>(), "keyboard_input_masking_level", desc{
-            "(Please see also 'Disable keyboard log' in 'video' section of 'Confuguration Options'.)"
+            "(Please see also \"Disable keyboard log\" in \"video\" section of \"Configuration Options\".)"
         }, set(KeyboardInputMaskingLevel::password_and_unidentified));
         W.member(advanced_in_gui, no_sesman, L, type_<bool>(), "hide_non_printable_kbd_input", set(false));
     });
@@ -501,7 +501,7 @@ void config_spec_definition(Writer && W)
 
         W.member(ini_and_gui, allow_connpolicy_and_gui, rdp_connpolicy, L, type_<KeyboardLogFlags>{}, connpolicy::type_<KeyboardLogFlagsCP>{}, "disable_keyboard_log", desc{
             "Disable keyboard log:\n"
-            "(Please see also 'Keyboard input masking level' in 'session_log' section of 'Connection Policy'.)"
+            "(Please see also \"Keyboard input masking level\" in \"session_log\" section of \"Connection Policy\".)"
         }, disable_prefix_val, set(KeyboardLogFlags::syslog), connpolicy::set(KeyboardLogFlagsCP::syslog));
 
         W.member(ini_and_gui, no_sesman, L, type_<ClipboardLogFlags>(), "disable_clipboard_log", desc{"Disable clipboard log:"}, disable_prefix_val, set(ClipboardLogFlags::syslog));
