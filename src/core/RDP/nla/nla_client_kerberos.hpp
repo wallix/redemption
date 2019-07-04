71a85f782a2f84232f62c7467bbf75a5ae152452
Christophe Grosjean <cgrosjean@wallix.com>
2019-07-04 18:20:18 +0200

    Simplifying NLA Kerberos Client Layer

--- src/core/RDP/nla/nla_client_kerberos.hpp
+++ src/core/RDP/nla/nla_client_kerberos.hpp
@@ -49,11 +49,329 @@ private:
     std::vector<uint8_t> PublicKey;
     Array ClientServerHash;
     Array ServerClientHash;
-
     Array ServicePrincipalName;
     SEC_WINNT_AUTH_IDENTITY identity;
-    std::unique_ptr<SecurityFunctionTable> table
-      = std::make_unique<UnimplementedSecurityFunctionTable>();
+    
+    struct Kerberos_SecurityFunctionTable
+    {
+    private:
+        struct Krb5Creds_deleter
+        {
+            void operator()(Krb5Creds* credentials) const
+            {
+                credentials->destroy_credentials(nullptr);
+                delete credentials; /*NOLINT*/
+            }
+        };
+
+        using Krb5CredsPtr = std::unique_ptr<Krb5Creds, Krb5Creds_deleter>;
+        Krb5CredsPtr sspi_credentials = nullptr;
+        std::unique_ptr<KERBEROSContext> sspi_krb_ctx = nullptr;
+
+    public:
+        ~Kerberos_SecurityFunctionTable()
+        {
+            this->sspi_krb_ctx.reset();
+            this->sspi_credentials.reset();
+            unsetenv("KRB5CCNAME");
+        }
+
+        // GSS_Acquire_cred
+        // ACQUIRE_CREDENTIALS_HANDLE_FN AcquireCredentialsHandle;
+        SEC_STATUS sspi_AcquireCredentialsHandle(
+            const char * pszPrincipal, Array * pvLogonID, SEC_WINNT_AUTH_IDENTITY const* pAuthData
+        ) {
+            if (pszPrincipal && pvLogonID) {
+                size_t length = strlen(pszPrincipal);
+                pvLogonID->init(length + 1);
+                pvLogonID->copy({pszPrincipal, length});
+                pvLogonID->get_data()[length] = 0;
+            }
+            this->sspi_credentials = Krb5CredsPtr(new Krb5Creds);
+
+            // set KRB5CCNAME cache name to specific with PID,
+            // call kinit to get tgt with identity credentials
+
+            int pid = getpid();
+            char cache[256];
+            snprintf(cache, 255, "FILE:/tmp/krb_red_%d", pid);
+            cache[255] = 0;
+            setenv("KRB5CCNAME", cache, 1);
+            LOG(LOG_INFO, "set KRB5CCNAME to %s", cache);
+            if (pAuthData) {
+                int ret = this->sspi_credentials->get_credentials(pAuthData->princname,
+                                                             pAuthData->princpass, nullptr);
+                if (!ret) {
+                    return SEC_E_OK;
+                }
+            }
+            return SEC_E_NO_CREDENTIALS;
+        }
+
+        bool sspi_get_service_name(array_view_const_char server, gss_name_t * name) {
+            gss_buffer_desc output;
+            OM_uint32 major_status, minor_status;
+            const char service_name[] = "TERMSRV";
+            gss_OID type = GSS_C_NT_HOSTBASED_SERVICE;
+            int size = (strlen(service_name) + 1 + server.size() + 1);
+
+            auto output_value = std::make_unique<char[]>(size);
+            output.value = output_value.get();
+            snprintf(static_cast<char*>(output.value), size, "%s@%.*s",
+                service_name, int(server.size()), server.data());
+            output.length = strlen(static_cast<char*>(output.value)) + 1;
+            LOG(LOG_INFO, "GSS IMPORT NAME : %s", static_cast<char*>(output.value));
+            major_status = gss_import_name(&minor_status, &output, type, name);
+            if (GSS_ERROR(major_status)) {
+                LOG(LOG_ERR, "Failed to create service principal name");
+                return false;
+            }
+            return true;
+        }
+
+
+        // GSS_Init_sec_context
+        // INITIALIZE_SECURITY_CONTEXT_FN InitializeSecurityContext;
+        SEC_STATUS sspi_InitializeSecurityContext(
+            array_view_const_char pszTargetName, array_view_const_u8 input_buffer, Array& output_buffer
+        ) 
+        {
+            OM_uint32 major_status, minor_status;
+
+            gss_cred_id_t gss_no_cred = GSS_C_NO_CREDENTIAL;
+            if (!this->sspi_krb_ctx) {
+                // LOG(LOG_INFO, "Initialiaze Sec Ctx: NO CONTEXT");
+                this->sspi_krb_ctx = std::make_unique<KERBEROSContext>();
+
+                // Target name (server name, ip ...)
+                if (!this->sspi_get_service_name(pszTargetName, &this->sspi_krb_ctx->target_name)) {
+                    return SEC_E_WRONG_PRINCIPAL;
+                }
+            }
+            // else {
+            //     LOG(LOG_INFO, "Initialiaze Sec CTX: USE FORMER CONTEXT");
+            // }
+
+            // Token Buffer
+            gss_buffer_desc input_tok, output_tok;
+            output_tok.length = 0;
+            // LOG(LOG_INFO, "GOT INPUT BUFFER: length %d",
+            //     input_buffer->Buffer.size());
+            input_tok.length = input_buffer.size();
+            input_tok.value = const_cast<uint8_t*>(input_buffer.data()); /*NOLINT*/
+
+            gss_OID_desc desired_mech = _gss_spnego_krb5_mechanism_oid_desc();
+            if (!this->sspi_mech_available(&desired_mech)) {
+                LOG(LOG_ERR, "Desired Mech unavailable");
+                return SEC_E_CRYPTO_SYSTEM_INVALID;
+            }
+    // OM_uint32 gss_init_sec_context
+    //                  (OM_uint32 ,             /* minor_status */
+    //                   const gss_cred_id_t,    /* initiator_cred_handle */
+    //                   gss_ctx_id_t ,          /* context_handle */
+    //                   const gss_name_t,       /* target_name */
+    //                   const gss_OID,          /* mech_type */
+    //                   OM_uint32,              /* req_flags */
+    //                   OM_uint32,              /* time_req */
+    //                   const gss_channel_bindings_t,
+    //                                           /* input_chan_bindings */
+    //                   const gss_buffer_t,     /* input_token */
+    //                   gss_OID ,               /* actual_mech_type */
+    //                   gss_buffer_t,           /* output_token */
+    //                   OM_uint32 ,             /* ret_flags */
+    //                   OM_uint32 *             /* time_rec */
+    //                  );
+            major_status = gss_init_sec_context(&minor_status,
+                                                gss_no_cred,
+                                                &this->sspi_krb_ctx->gss_ctx,
+                                                this->sspi_krb_ctx->target_name,
+                                                &desired_mech,
+                                                GSS_C_MUTUAL_FLAG,
+                                                GSS_C_INDEFINITE,
+                                                GSS_C_NO_CHANNEL_BINDINGS,
+                                                &input_tok,
+                                                &this->sspi_krb_ctx->actual_mech,
+                                                &output_tok,
+                                                &this->sspi_krb_ctx->actual_services,
+                                                &this->sspi_krb_ctx->actual_time);
+
+            if (GSS_ERROR(major_status)) {
+                LOG(LOG_INFO, "MAJOR ERROR");
+                this->sspi_report_error(GSS_C_GSS_CODE, "CredSSP: SPNEGO negotiation failed.",
+                                   major_status, minor_status);
+                return SEC_E_OUT_OF_SEQUENCE;
+            }
+
+            // LOG(LOG_INFO, "output tok length : %d", output_tok.length);
+            output_buffer.init(output_tok.length);
+            output_buffer.copy({static_cast<uint8_t const*>(output_tok.value), output_tok.length});
+
+            (void) gss_release_buffer(&minor_status, &output_tok);
+
+            if (major_status & GSS_S_CONTINUE_NEEDED) {
+                // LOG(LOG_INFO, "MAJOR CONTINUE NEEDED");
+                (void) gss_release_buffer(&minor_status, &input_tok);
+                return SEC_I_CONTINUE_NEEDED;
+            }
+            // LOG(LOG_INFO, "MAJOR COMPLETE NEEDED");
+            return SEC_I_COMPLETE_NEEDED;
+        }
+
+        // GSS_Accept_sec_context
+        // ACCEPT_SECURITY_CONTEXT AcceptSecurityContext;
+        // Not used by client
+
+        // GSS_Wrap
+        // ENCRYPT_MESSAGE EncryptMessage;
+        SEC_STATUS sspi_EncryptMessage(array_view_const_u8 data_in, Array& data_out, unsigned long MessageSeqNo) {
+            (void)MessageSeqNo;
+            // OM_uint32 KRB5_CALLCONV
+            // gss_wrap(
+            //     OM_uint32 *,        /* minor_status */
+            //     gss_ctx_id_t,       /* context_handle */
+            //     int,                /* conf_req_flag */
+            //     gss_qop_t,          /* qop_req */
+            //     gss_buffer_t,       /* input_message_buffer */
+            //     int *,              /* conf_state */
+            //     gss_buffer_t);      /* output_message_buffer */
+
+            OM_uint32 major_status;
+            OM_uint32 minor_status;
+            int conf_state;
+            if (!this->sspi_krb_ctx) {
+                return SEC_E_NO_CONTEXT;
+            }
+            gss_buffer_desc inbuf, outbuf;
+
+            inbuf.value = const_cast<uint8_t*>(data_in.data()); /*NOLINT*/
+            inbuf.length = data_in.size();
+            // LOG(LOG_INFO, "GSS_WRAP inbuf length : %d", inbuf.length);
+            major_status = gss_wrap(&minor_status, this->sspi_krb_ctx->gss_ctx, true,
+				    GSS_C_QOP_DEFAULT, &inbuf, &conf_state, &outbuf);
+            if (GSS_ERROR(major_status)) {
+                LOG(LOG_INFO, "MAJOR ERROR");
+                this->sspi_report_error(GSS_C_GSS_CODE, "CredSSP: GSS WRAP failed.",
+                                   major_status, minor_status);
+                return SEC_E_ENCRYPT_FAILURE;
+            }
+            // LOG(LOG_INFO, "GSS_WRAP outbuf length : %d", outbuf.length);
+            data_out.init(outbuf.length);
+            data_out.copy({static_cast<uint8_t const*>(outbuf.value), outbuf.length});
+            gss_release_buffer(&minor_status, &outbuf);
+
+            return SEC_E_OK;
+        }
+
+        // GSS_Unwrap
+        // DECRYPT_MESSAGE DecryptMessage;
+        SEC_STATUS sspi_DecryptMessage(array_view_const_u8 data_in, Array& data_out, unsigned long MessageSeqNo) {
+            (void)MessageSeqNo;
+
+            // OM_uint32 gss_unwrap
+            //     (OM_uint32 ,             /* minor_status */
+            //      const gss_ctx_id_t,     /* context_handle */
+            //      const gss_buffer_t,     /* input_message_buffer */
+            //      gss_buffer_t,           /* output_message_buffer */
+            //      int ,                   /* conf_state */
+            //      gss_qop_t *             /* qop_state */
+            //      );
+
+            OM_uint32 major_status;
+            OM_uint32 minor_status;
+            int conf_state;
+            gss_qop_t qop_state;
+            if (!this->sspi_krb_ctx) {
+                return SEC_E_NO_CONTEXT;
+            }
+            gss_buffer_desc inbuf, outbuf;
+            inbuf.value = const_cast<uint8_t*>(data_in.data()); /*NOLINT*/
+            inbuf.length = data_in.size();
+            // LOG(LOG_INFO, "GSS_UNWRAP inbuf length : %d", inbuf.length);
+            major_status = gss_unwrap(&minor_status, this->sspi_krb_ctx->gss_ctx, &inbuf, &outbuf,
+                                      &conf_state, &qop_state);
+            if (GSS_ERROR(major_status)) {
+                LOG(LOG_INFO, "MAJOR ERROR");
+                this->sspi_report_error(GSS_C_GSS_CODE, "CredSSP: GSS UNWRAP failed.",
+                                   major_status, minor_status);
+                return SEC_E_DECRYPT_FAILURE;
+            }
+            // LOG(LOG_INFO, "GSS_UNWRAP outbuf length : %d", outbuf.length);
+            data_out.init(outbuf.length);
+            data_out.copy({static_cast<uint8_t const*>(outbuf.value), outbuf.length});
+            gss_release_buffer(&minor_status, &outbuf);
+            return SEC_E_OK;
+        }
+
+        void sspi_report_error(OM_uint32 code, const char *str,
+                          OM_uint32 major_status, OM_uint32 minor_status)
+        {
+            OM_uint32 msgctx = 0, ms;
+            gss_buffer_desc status_string;
+
+            LOG(LOG_ERR, "GSS error [%u:%u:%u]: %s",
+                (major_status & 0xff000000) >> 24,	// Calling error
+                (major_status & 0xff0000) >> 16,	// Routine error
+                major_status & 0xffff,	// Supplementary info bits
+                str);
+
+            LOG(LOG_ERR, "GSS Minor status error [%u:%u:%u]:%u %s",
+                (minor_status & 0xff000000) >> 24,	// Calling error
+                (minor_status & 0xff0000) >> 16,	// Routine error
+                minor_status & 0xffff,	// Supplementary info bits
+                minor_status,
+                str);
+
+            do {
+                ms = gss_display_status(
+                    &minor_status, major_status,
+                    code, GSS_C_NULL_OID, &msgctx, &status_string);
+            	if (ms != GSS_S_COMPLETE) {
+                    continue;
+                }
+
+                LOG(LOG_ERR," - %s", static_cast<char const*>(status_string.value));
+            }
+            while (ms == GSS_S_COMPLETE && msgctx);
+
+        }
+
+        bool sspi_mech_available(gss_OID mech)
+        {
+            int mech_found;
+            OM_uint32 major_status, minor_status;
+            gss_OID_set mech_set;
+
+            mech_found = 0;
+
+            if (mech == GSS_C_NO_OID) {
+                return true;
+            }
+
+            major_status = gss_indicate_mechs(&minor_status, &mech_set);
+            if (!mech_set) {
+                return false;
+            }
+            if (GSS_ERROR(major_status)) {
+                this->sspi_report_error(GSS_C_GSS_CODE, "Failed to get available mechs on system",
+                                   major_status, minor_status);
+                gss_release_oid_set(&minor_status, &mech_set);
+                return false;
+            }
+
+            gss_test_oid_set_member(&minor_status, mech, mech_set, &mech_found);
+
+            gss_release_oid_set(&minor_status, &mech_set);
+            if (GSS_ERROR(major_status)) {
+                this->sspi_report_error(GSS_C_GSS_CODE, "Failed to match mechanism in set",
+                                      major_status, minor_status);
+                return false;
+            }
+
+            return mech_found != 0;
+        }
+
+    } table;
+
     bool restricted_admin_mode;
 
     const char * target_host;
@@ -137,7 +455,7 @@ private:
             public_key = this->ClientServerHash.av();
         }
 
-        return this->table->EncryptMessage(
+        return this->table.sspi_EncryptMessage(
             public_key, this->ts_request.pubKeyAuth, this->send_seq_num++);
     }
 
@@ -146,7 +464,7 @@ private:
 
         Array Buffer;
 
-        SEC_STATUS const status = this->table->DecryptMessage(
+        SEC_STATUS const status = this->table.sspi_DecryptMessage(
             this->ts_request.pubKeyAuth.av(), Buffer, this->recv_seq_num++);
 
         if (status != SEC_E_OK) {
@@ -156,7 +474,7 @@ private:
                 this->extra_message.append(TR(trkeys::err_login_password, this->lang));
                 LOG(LOG_INFO, "Provided login/password is probably incorrect.");
             }
-            LOG(LOG_ERR, "DecryptMessage failure: 0x%08X", status);
+            LOG(LOG_ERR, "sspi_DecryptMessage failure: 0x%08X", status);
             return status;
         }
 
@@ -227,7 +545,7 @@ private:
         //unsigned long const fContextReq
         //  = ISC_REQ_MUTUAL_AUTH | ISC_REQ_CONFIDENTIALITY | ISC_REQ_USE_SESSION_KEY;
 
-        SEC_STATUS status = this->table->InitializeSecurityContext(
+        SEC_STATUS status = this->table.sspi_InitializeSecurityContext(
             bytes_view(this->ServicePrincipalName.av()).as_chars(),
             this->client_auth_data.input_buffer.av(),
             /*output*/this->ts_request.negoTokens);
@@ -354,7 +672,7 @@ private:
         StaticOutStream<65536> ts_credentials_send;
         this->ts_credentials.emit(ts_credentials_send);
 
-        return this->table->EncryptMessage(
+        return this->table.sspi_EncryptMessage(
             {ts_credentials_send.get_data(), ts_credentials_send.get_offset()},
             this->ts_request.authInfo, this->send_seq_num++);
     }
@@ -409,12 +727,9 @@ public:
         auto const key = this->trans.get_public_key();
         this->PublicKey.assign(key.data(), key.data()+key.size());
 
-        this->table.reset();
-
         LOG(LOG_INFO, "Credssp: KERBEROS Authentication");
-        this->table = std::make_unique<Kerberos_SecurityFunctionTable>();
 
-        SEC_STATUS status = this->table->AcquireCredentialsHandle(this->target_host, &this->ServicePrincipalName, &this->identity);
+        SEC_STATUS status = this->table.sspi_AcquireCredentialsHandle(this->target_host, &this->ServicePrincipalName, &this->identity);
 
         if (status != SEC_E_OK) {
             LOG(LOG_ERR, "Kerberos InitSecurityInterface status:%s0x%08X, fallback to NTLM",
