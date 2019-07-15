470c8515fb0b27ed107f18ad73487e42cb2218f3
Jonathan Poelen <jpoelen@wallix.com>
2019-07-15 14:30:47 +0200

    refs #21883: Character display problem at end of line
    Some characters are truncated
    
    "totoj" -> "toto̦ " (toto following of left pixel of j)

--- src/gdi/graphic_api.cpp
+++ src/gdi/graphic_api.cpp
@@ -216,7 +216,7 @@ void server_draw_text(
             total_width += font_item->offsetx + font_item->incby;
         }
 
-        const Rect bk(x, y, total_width+1, font.max_height());
+        const Rect bk(x, y, total_width + 2, font.max_height());
 
         RDPGlyphIndex glyphindex(
             cacheId,            // cache_id
