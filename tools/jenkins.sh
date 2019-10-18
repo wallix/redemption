7ada98615a4a41de59267111a15d8c488ab8aa26
Jonathan Poelen <jpoelen@wallix.com>
2019-10-18 18:32:47 +0200

    jenkins.sh: remove tests/client_redemption/client_channels

--- tools/jenkins.sh
+++ tools/jenkins.sh
@@ -85,7 +85,6 @@ big_mem='exe libs
   tests/lib
   tests/server
   tests/client_mods
-  tests/client_redemption/client_channels
   tests/mod/rdp.norec
   tests/mod/vnc.norec'
 build $toolset_gcc cxxflags=-g -j2 ocr_tools
