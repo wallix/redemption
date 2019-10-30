3358049a183bd96314b5954ebbcdcacb0a6c643a
Raphaël ZHOU <rzhou@wallix.com>
2019-10-30 11:39:04 +0100

    refs #22739: Test RDP licesing flow. (Fix string comparison)

--- tests/acl/test_license_api.cpp
+++ tests/acl/test_license_api.cpp
@@ -24,6 +24,7 @@
 #include "test_only/test_framework/redemption_unit_tests.hpp"
 
 #include "acl/auth_api.hpp"
+#include "acl/license_api.hpp"
 #include "configs/config.hpp"
 #include "core/client_info.hpp"
 #include "core/listen.hpp"
@@ -40,8 +41,6 @@
 #include "test_only/transport/test_transport.hpp"
 #include "test_only/core/font.hpp"
 #include "transport/socket_transport.hpp"
-
-#include "test_only/acl/test_license_store.hpp"
 
 //#define GENERATE_TESTING_DATA
 
@@ -153,10 +152,105 @@ RED_AUTO_TEST_CASE(TestWithoutExistingLicense)
             NullAuthentifier authentifier;
 
 #ifdef GENERATE_TESTING_DATA
-            CaptureLicenseStore license_store;
+            class CaptureLicenseStore : public LicenseApi
+            {
+            public:
+                // The functions shall return empty bytes_view to indicate the error.
+                bytes_view get_license(char const* client_name, uint32_t version, char const* scope, char const* company_name, char const* product_id, writable_bytes_view out, bool enable_log) override
+                {
+                    (void)client_name;
+                    (void)version;
+                    (void)scope;
+                    (void)company_name;
+                    (void)product_id;
+                    (void)out;
+                    (void)enable_log;
+
+                    return bytes_view { out.data(), 0 };
+                }
+
+                bool put_license(char const* client_name, uint32_t version, char const* scope, char const* company_name, char const* product_id, bytes_view in, bool enable_log) override
+                {
+                    (void)enable_log;
+
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ const char license_client_name[] =");
+                    hexdump_c(client_name, ::strlen(client_name));
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ ;");
+
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ uint32_t license_version = %u", version);
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ ;");
+
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ const char license_scope[] =");
+                    hexdump_c(scope, ::strlen(scope));
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ ;");
+
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ const char license_company_name[] =");
+                    hexdump_c(company_name, ::strlen(company_name));
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ ;");
+
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ const char license_product_id[] =");
+                    hexdump_c(product_id, ::strlen(product_id));
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ ;");
+
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ const uint8_t license_data[%zu] = {", in.size());
+                    hexdump_d(in.data(), in.size());
+                    LOG(LOG_INFO, "/*CaptureLicenseStore */ };");
+
+                    return true;
+                }
+            } license_store;
 #else
-            CompareLicenseStore license_store(license_client_name, license_version, license_scope, license_company_name,
-                license_product_id, bytes_view(license_data, sizeof(license_data)));
+            class CompareLicenseStore : public LicenseApi
+            {
+            public:
+                CompareLicenseStore(char const* client_name, uint32_t version, char const* scope, char const* company_name, char const* product_id, bytes_view license_data) :
+                    expected_client_name(client_name),
+                    expected_version(version),
+                    expected_scope(scope),
+                    expected_company_name(company_name),
+                    expected_product_id(product_id),
+                    expected_license_data(license_data) {}
+
+                // The functions shall return empty bytes_view to indicate the error.
+                bytes_view get_license(char const* client_name, uint32_t version, char const* scope, char const* company_name, char const* product_id, writable_bytes_view out, bool enable_log) override
+                {
+                    (void)client_name;
+                    (void)version;
+                    (void)scope;
+                    (void)company_name;
+                    (void)product_id;
+                    (void)out;
+                    (void)enable_log;
+
+                    return bytes_view { out.data(), 0 };
+                }
+
+                bool put_license(char const* client_name, uint32_t version, char const* scope, char const* company_name, char const* product_id, bytes_view in, bool enable_log) override
+                {
+                    (void)enable_log;
+
+                    RED_CHECK_EQ(client_name,  this->expected_client_name);
+                    RED_CHECK_EQ(version,      this->expected_version);
+                    RED_CHECK_EQ(scope,        this->expected_scope);
+                    RED_CHECK_EQ(company_name, this->expected_company_name);
+                    RED_CHECK_EQ(product_id,   this->expected_product_id);
+
+                    RED_REQUIRE_EQ(in.size(), this->expected_license_data.size());
+
+                    RED_CHECK_MEM(in, this->expected_license_data);
+
+                    return true;
+                }
+
+            private:
+                std::string_view expected_client_name;
+                uint32_t         expected_version;
+                std::string_view expected_scope;
+                std::string_view expected_company_name;
+                std::string_view expected_product_id;
+                bytes_view       expected_license_data;
+            } license_store(license_client_name, license_version, license_scope, license_company_name,
+                  license_product_id, bytes_view(license_data, sizeof(license_data)));
 #endif
 
             SessionReactor session_reactor;
@@ -320,8 +414,60 @@ RED_AUTO_TEST_CASE(TestWithExistingLicense)
             LCGTime timeobj;
             NullAuthentifier authentifier;
 
-            ReplayLicenseStore license_store(license_client_name, license_version, license_scope, license_company_name,
-                license_product_id, bytes_view(license_data, sizeof(license_data)));
+            class ReplayLicenseStore : public LicenseApi
+            {
+            public:
+                ReplayLicenseStore(char const* client_name, uint32_t version, char const* scope, char const* company_name, char const* product_id, bytes_view license_data) :
+                    expected_client_name(client_name),
+                    expected_version(version),
+                    expected_scope(scope),
+                    expected_company_name(company_name),
+                    expected_product_id(product_id),
+                    expected_license_data(license_data) {}
+
+                // The functions shall return empty bytes_view to indicate the error.
+                bytes_view get_license(char const* client_name, uint32_t version, char const* scope, char const* company_name, char const* product_id, writable_bytes_view out, bool enable_log) override
+                {
+                    (void)enable_log;
+
+                    RED_CHECK_EQ(client_name,  this->expected_client_name);
+                    RED_CHECK_EQ(version,      this->expected_version);
+                    RED_CHECK_EQ(scope,        this->expected_scope);
+                    RED_CHECK_EQ(company_name, this->expected_company_name);
+                    RED_CHECK_EQ(product_id,   this->expected_product_id);
+
+                    RED_REQUIRE_GE(out.size(), this->expected_license_data.size());
+
+                    size_t const effective_license_size = std::min(out.size(), this->expected_license_data.size());
+
+                    ::memcpy(out.data(), this->expected_license_data.data(), effective_license_size);
+
+                    return bytes_view { out.data(), effective_license_size };
+                }
+
+                bool put_license(char const* client_name, uint32_t version, char const* scope, char const* company_name, char const* product_id, bytes_view in, bool enable_log) override
+                {
+                    (void)enable_log;
+                    (void)in;
+
+                    RED_CHECK_EQ(client_name,  this->expected_client_name);
+                    RED_CHECK_EQ(version,      this->expected_version);
+                    RED_CHECK_EQ(scope,        this->expected_scope);
+                    RED_CHECK_EQ(company_name, this->expected_company_name);
+                    RED_CHECK_EQ(product_id,   this->expected_product_id);
+
+                    return true;
+                }
+
+            private:
+                std::string_view expected_client_name;
+                uint32_t         expected_version;
+                std::string_view expected_scope;
+                std::string_view expected_company_name;
+                std::string_view expected_product_id;
+                bytes_view       expected_license_data;
+            } license_store(license_client_name, license_version, license_scope, license_company_name,
+                  license_product_id, bytes_view(license_data, sizeof(license_data)));
 
             SessionReactor session_reactor;
 
