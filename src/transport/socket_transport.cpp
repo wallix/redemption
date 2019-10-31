4c6e4c6ab5bdd9d94679188125deeea91e26f962
Raphaël ZHOU <rzhou@wallix.com>
2019-10-31 12:18:30 +0100

    Fix reading VNC clipboard.

--- src/transport/socket_transport.cpp
+++ src/transport/socket_transport.cpp
@@ -213,7 +213,7 @@ size_t SocketTransport::do_partial_read(uint8_t * buffer, size_t len)
     ssize_t const res = this->tls ? this->tls->privpartial_recv_tls(buffer, len) : socket_recv_partial(this->sck, buffer, len);
 
     if (res < 0){
-        LOG(LOG_ERR, "SocketTransport::do_partial_read: Failed to read from socket!");
+        LOG(LOG_ERR, "SocketTransport::do_partial_read: Failed to read from socket! (%s)", this->name);
         throw Error(ERR_TRANSPORT_NO_MORE_DATA, 0);
     }
 
