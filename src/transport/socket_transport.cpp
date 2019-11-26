4635d0ce9b79acc8ad0ca18e947659c3fdb65e37
Christophe Grosjean <cgrosjean@wallix.com>
2019-11-26 14:15:10 +0100

    Backport of disabling logs on disconnection for watchdog

--- src/transport/socket_transport.cpp
+++ src/transport/socket_transport.cpp
@@ -213,7 +213,9 @@ size_t SocketTransport::do_partial_read(uint8_t * buffer, size_t len)
     ssize_t const res = this->tls ? this->tls->privpartial_recv_tls(buffer, len) : socket_recv_partial(this->sck, buffer, len);
 
     if (res < 0){
-        LOG(LOG_ERR, "SocketTransport::do_partial_read: Failed to read from socket!");
+        if (0 == strcmp("127.0.0.1", this->ip_address)){
+            LOG(LOG_ERR, "SocketTransport::do_partial_read: Failed to read from socket!");
+        }
         throw Error(ERR_TRANSPORT_NO_MORE_DATA, 0);
     }
 
