8a7e72a835d87d11a1d1f66a172ff5826da0d448
Christophe Grosjean <cgrosjean@wallix.com>
2019-12-11 17:25:06 +0100

    Upport of 7.3 cipher_string option message

--- projects/redemption_configs/autogen/include/configs/autogen/rdp_cp_spec.hpp
+++ projects/redemption_configs/autogen/include/configs/autogen/rdp_cp_spec.hpp
@@ -54,7 +54,7 @@ tls_min_level = integer(min=0, default=0)
 # Maximal incoming TLS level 0=no restriction, 1=TLSv1.1, 2=TLSv1.2, 3=TLSv1.3
 tls_max_level = integer(min=0, default=0)
 
-# TLSv1.2 additional ciphers supported by client
+# TLSv1.2 additional ciphers supported by client, default is empty to apply system-wide configuration (SSL security level 2), ALL for support of all ciphers to ensure highest compatibility with target servers.
 cipher_string = string(default='')
 
 # Show common cipher list supported by client and server
