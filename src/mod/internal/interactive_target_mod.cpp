23ae8e81661fc074a42230a3438272c227baef90
Christophe Grosjean <pykoder@gmail.com>
2020-02-04 15:08:04 +0100

    Removed dependency between interactive_target_mod and locally_integrable

--- src/mod/internal/interactive_target_mod.cpp
+++ src/mod/internal/interactive_target_mod.cpp
@@ -24,7 +24,169 @@
 #include "configs/config.hpp"
 #include "core/front_api.hpp"
 #include "utils/translation.hpp"
+#include "core/RDP/slowpath.hpp"
+#include "RAIL/client_execute.hpp"
 
+
+void InteractiveTargetMod::rdp_input_invalidate(Rect r)
+{
+    this->screen.rdp_input_invalidate(r);
+
+    if (this->rail_enabled) {
+        this->rail_client_execute.input_invalidate(r);
+    }
+}
+
+void InteractiveTargetMod::rdp_input_mouse(int device_flags, int x, int y, Keymap2 * keymap)
+{
+    if (device_flags & (MOUSE_FLAG_WHEEL | MOUSE_FLAG_HWHEEL)) {
+        x = this->old_mouse_x;
+        y = this->old_mouse_y;
+    }
+    else {
+        this->old_mouse_x = x;
+        this->old_mouse_y = y;
+    }
+
+    if (!this->rail_enabled) {
+        this->screen.rdp_input_mouse(device_flags, x, y, keymap);
+    }
+    else {
+        bool out_mouse_captured = false;
+        if (!this->rail_client_execute.input_mouse(device_flags, x, y, out_mouse_captured)) {
+            switch (this->dc_state) {
+                case DCState::Wait:
+                    if (device_flags == (SlowPath::PTRFLAGS_DOWN | SlowPath::PTRFLAGS_BUTTON1)) {
+                        this->dc_state = DCState::FirstClickDown;
+
+                        if (this->first_click_down_timer) {
+                            this->first_click_down_timer->set_delay(std::chrono::seconds(1));
+                        }
+                        else {
+                            this->first_click_down_timer = timer_events_
+                            .create_timer_executor(this->session_reactor)
+                            .set_delay(std::chrono::seconds(1))
+                            .on_action(jln::one_shot([this]{
+                                this->dc_state = DCState::Wait;
+                            }));
+                        }
+                    }
+                break;
+
+                case DCState::FirstClickDown:
+                    if (device_flags == SlowPath::PTRFLAGS_BUTTON1) {
+                        this->dc_state = DCState::FirstClickRelease;
+                    }
+                    else if (device_flags == (SlowPath::PTRFLAGS_DOWN | SlowPath::PTRFLAGS_BUTTON1)) {
+                    }
+                    else {
+                        this->cancel_double_click_detection();
+                    }
+                break;
+
+                case DCState::FirstClickRelease:
+                    if (device_flags == (SlowPath::PTRFLAGS_DOWN | SlowPath::PTRFLAGS_BUTTON1)) {
+                        this->dc_state = DCState::SecondClickDown;
+                    }
+                    else {
+                        this->cancel_double_click_detection();
+                    }
+                break;
+
+                case DCState::SecondClickDown:
+                    if (device_flags == SlowPath::PTRFLAGS_BUTTON1) {
+                        this->dc_state = DCState::Wait;
+
+                        bool out_mouse_captured_2 = false;
+
+                        this->rail_client_execute.input_mouse(PTRFLAGS_EX_DOUBLE_CLICK, x, y, out_mouse_captured_2);
+
+                        this->cancel_double_click_detection();
+                    }
+                    else if (device_flags == (SlowPath::PTRFLAGS_DOWN | SlowPath::PTRFLAGS_BUTTON1)) {
+                    }
+                    else {
+                        this->cancel_double_click_detection();
+                    }
+                break;
+
+                default:
+                    assert(false);
+
+                    this->cancel_double_click_detection();
+                break;
+            }
+
+            if (out_mouse_captured) {
+                this->allow_mouse_pointer_change(false);
+
+                this->current_mouse_owner = MouseOwner::ClientExecute;
+            }
+            else {
+                if (MouseOwner::WidgetModule != this->current_mouse_owner) {
+                    this->redo_mouse_pointer_change(x, y);
+                }
+
+                this->current_mouse_owner = MouseOwner::WidgetModule;
+            }
+        }
+
+        this->screen.rdp_input_mouse(device_flags, x, y, keymap);
+
+        if (out_mouse_captured) {
+            this->allow_mouse_pointer_change(true);
+        }
+    }
+}
+
+void InteractiveTargetMod::rdp_input_scancode(
+    long param1, long param2, long param3, long param4, Keymap2 * keymap)
+{
+    this->screen.rdp_input_scancode(param1, param2, param3, param4, keymap);
+
+    if (this->rail_enabled) {
+        if (!this->alt_key_pressed) {
+            if ((param1 == 56) && !(param3 & SlowPath::KBDFLAGS_RELEASE)) {
+                this->alt_key_pressed = true;
+            }
+        }
+        else {
+//            if ((param1 == 56) && (param3 == (SlowPath::KBDFLAGS_DOWN | SlowPath::KBDFLAGS_RELEASE))) {
+            if ((param1 == 56) && (param3 & SlowPath::KBDFLAGS_RELEASE)) {
+                this->alt_key_pressed = false;
+            }
+            else if ((param1 == 62) && !param3) {
+                LOG(LOG_INFO, "InteractiveTargetMod::rdp_input_scancode: Close by user (Alt+F4)");
+                throw Error(ERR_WIDGET);    // F4 key pressed
+            }
+        }
+    }
+}
+
+void InteractiveTargetMod::refresh(Rect r)
+{
+    this->screen.refresh(r);
+
+    if (this->rail_enabled) {
+        this->rail_client_execute.input_invalidate(r);
+    }
+}
+
+void InteractiveTargetMod::cancel_double_click_detection()
+{
+    assert(this->rail_enabled);
+
+    this->first_click_down_timer.reset();
+
+    this->dc_state = DCState::Wait;
+}
+
+bool InteractiveTargetMod::is_resizing_hosted_desktop_allowed() const
+{
+    assert(this->rail_enabled);
+
+    return false;
+}
 
 InteractiveTargetMod::InteractiveTargetMod(
     InteractiveTargetModVariables vars,
@@ -34,8 +196,18 @@ InteractiveTargetMod::InteractiveTargetMod(
     gdi::GraphicApi & drawable, FrontAPI & front,
     uint16_t width, uint16_t height, Rect const widget_rect,
     ClientExecute & rail_client_execute, Font const& font, Theme const& theme)
-    : LocallyIntegrableMod(session_reactor, timer_events_, graphic_events_, drawable, front, width, height, font,
-        rail_client_execute, theme)
+    : front_width(front_width)
+    , front_height(front_height)
+    , front(front)
+    , screen(drawable, font, nullptr, theme)
+    , rail_client_execute(rail_client_execute)
+    , dvc_manager(false)
+    , dc_state(DCState::Wait)
+    , rail_enabled(rail_client_execute.is_rail_enabled())
+    , current_mouse_owner(MouseOwner::WidgetModule)
+    , session_reactor(session_reactor)
+    , timer_events_(timer_events_)
+    , graphic_events_(graphic_events_)
     , ask_device(vars.is_asked<cfg::context::target_host>())
     , ask_login(vars.is_asked<cfg::globals::target_user>())
     , ask_password((this->ask_login || vars.is_asked<cfg::context::target_password>()))
@@ -54,6 +226,19 @@ InteractiveTargetMod::InteractiveTargetMod(
     , copy_paste(vars.get<cfg::debug::mod_internal>() != 0)
     , vars(vars)
 {
+    this->screen.set_wh(front_width, front_height);
+    if (this->rail_enabled) {
+        this->graphic_event = graphic_events_.create_action_executor(session_reactor)
+        .on_action(jln::one_shot([this](gdi::GraphicApi&){
+            if (!this->rail_client_execute) {
+                this->rail_client_execute.ready(
+                    *this, this->front_width, this->front_height, this->font(),
+                    this->is_resizing_hosted_desktop_allowed());
+
+                this->dvc_manager.ready(this->front);
+            }
+        }));
+    }
     this->screen.add_widget(&this->challenge);
     this->challenge.password_edit.set_text("");
     this->screen.set_widget_focus(
@@ -80,6 +265,7 @@ InteractiveTargetMod::InteractiveTargetMod(
 
 InteractiveTargetMod::~InteractiveTargetMod()
 {
+    this->rail_client_execute.reset(true);
     this->screen.clear();
 }
 
@@ -124,7 +310,14 @@ void InteractiveTargetMod::refused()
 
 void InteractiveTargetMod::send_to_mod_channel(CHANNELS::ChannelNameId front_channel_name, InStream& chunk, size_t length, uint32_t flags)
 {
-    LocallyIntegrableMod::send_to_mod_channel(front_channel_name, chunk, length, flags);
+    if (this->rail_enabled && this->rail_client_execute){
+        if (front_channel_name == CHANNELS::channel_names::rail) {
+            this->rail_client_execute.send_to_mod_rail_channel(length, chunk, flags);
+        }
+        else if (front_channel_name == CHANNELS::channel_names::drdynvc) {
+            this->dvc_manager.send_to_mod_drdynvc_channel(length, chunk, flags);
+        }
+    }
 
     if (this->copy_paste && front_channel_name == CHANNELS::channel_names::cliprdr) {
         this->copy_paste.send_to_mod_channel(chunk, flags);
--- src/mod/internal/interactive_target_mod.hpp
+++ src/mod/internal/interactive_target_mod.hpp
@@ -23,10 +23,13 @@
 
 #include "configs/config_access.hpp"
 #include "mod/internal/copy_paste.hpp"
-#include "mod/internal/locally_integrable_mod.hpp"
 #include "mod/internal/widget/notify_api.hpp"
 #include "mod/internal/widget/language_button.hpp"
 #include "mod/internal/widget/flat_interactive_target.hpp"
+#include "core/session_reactor.hpp"
+#include "mod/mod_api.hpp"
+#include "mod/internal/dvc_manager.hpp"
+#include "mod/internal/widget/screen.hpp"
 
 
 using InteractiveTargetModVariables = vcfg::variables<
@@ -40,8 +43,120 @@ using InteractiveTargetModVariables = vcfg::variables<
     vcfg::var<cfg::debug::mod_internal,                 vcfg::accessmode::get>
 >;
 
-class InteractiveTargetMod : public LocallyIntegrableMod, public NotifyApi
+class ClientExecute;
+
+class InteractiveTargetMod : public mod_api, public NotifyApi
 {
+public:
+    [[nodiscard]] Font const & font() const
+    {
+        return this->screen.font;
+    }
+
+    [[nodiscard]] Theme const & theme() const
+    {
+        return this->screen.theme;
+    }
+
+    [[nodiscard]] Rect get_screen_rect() const
+    {
+        return this->screen.get_rect();
+    }
+
+    void rdp_gdi_up_and_running(ScreenInfo & ) override {}
+
+    void rdp_gdi_down() override {}
+
+    void rdp_input_invalidate(Rect r) override;
+
+    void rdp_input_mouse(int device_flags, int x, int y, Keymap2 * keymap) override;
+
+    void rdp_input_scancode(long param1, long param2, long param3, long param4,
+            Keymap2 * keymap) override;
+
+    void rdp_input_unicode(uint16_t unicode, uint16_t flag) override
+    {
+        this->screen.rdp_input_unicode(unicode, flag);
+    }
+    
+    void rdp_input_synchronize(uint32_t time, uint16_t device_flags, int16_t param1, int16_t param2) override
+    {
+        (void)time;
+        (void)device_flags;
+        (void)param1;
+        (void)param2;
+    }
+
+    void refresh(Rect r) override;
+
+    void send_to_mod_channel(CHANNELS::ChannelNameId front_channel_name, InStream& chunk, size_t length, uint32_t flags) override;
+
+    [[nodiscard]] Dimension get_dim() const override
+    {
+        return Dimension(this->front_width, this->front_height);
+    }
+
+    void allow_mouse_pointer_change(bool allow)
+    {
+        this->screen.allow_mouse_pointer_change(allow);
+    }
+
+    void redo_mouse_pointer_change(int x, int y)
+    {
+        this->screen.redo_mouse_pointer_change(x, y);
+    }
+
+private:
+    void cancel_double_click_detection();
+
+    [[nodiscard]] virtual bool is_resizing_hosted_desktop_allowed() const;
+
+protected:
+    uint16_t front_width;
+    uint16_t front_height;
+
+    FrontAPI & front;
+
+    WidgetScreen screen;
+
+private:
+    ClientExecute & rail_client_execute;
+    DVCManager dvc_manager;
+
+    bool alt_key_pressed = false;
+
+    enum class DCState
+    {
+        Wait,
+        FirstClickDown,
+        FirstClickRelease,
+        SecondClickDown,
+    };
+
+    DCState dc_state;
+
+    TimerPtr first_click_down_timer;
+
+    const bool rail_enabled;
+
+    enum class MouseOwner
+    {
+        ClientExecute,
+        WidgetModule,
+    };
+
+    MouseOwner current_mouse_owner;
+
+    int old_mouse_x = 0;
+    int old_mouse_y = 0;
+
+protected:
+    SessionReactor& session_reactor;
+    TimerContainer& timer_events_;
+    GraphicEventContainer& graphic_events_;
+
+private:
+    GraphicEventPtr graphic_event;
 public:
     bool ask_device;
     bool ask_login;
@@ -73,8 +188,6 @@ public:
 
     [[nodiscard]] bool is_up_and_running() const override
     { return true; }
-
-    void send_to_mod_channel(CHANNELS::ChannelNameId front_channel_name, InStream& chunk, size_t length, uint32_t flags) override;
 
     void move_size_widget(int16_t left, int16_t top, uint16_t width, uint16_t height) override
     {
