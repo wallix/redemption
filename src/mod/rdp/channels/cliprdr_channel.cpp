f53242338fb6270609c7c010f990dabb5a2efdbe
Jonathan Poelen <jpoelen@wallix.com>
2020-03-03 12:07:48 +0100

    update header

--- src/mod/rdp/channels/cliprdr_channel.cpp
+++ src/mod/rdp/channels/cliprdr_channel.cpp
@@ -1,21 +1,21 @@
 /*
-    This program is free software; you can redistribute it and/or modify it
-     under the terms of the GNU General Public License as published by the
-     Free Software Foundation; either version 2 of the License, or (at your
-     option) any later version.
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful, but
-     WITHOUT ANY WARRANTY; without even the implied warranty of
-     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-     Public License for more details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License along
-     with this program; if not, write to the Free Software Foundation, Inc.,
-     675 Mass Ave, Cambridge, MA 02139, USA.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-    Product name: redemption, a FLOSS RDP proxy
-    Copyright (C) Wallix 2015
-    Author(s): Christophe Grosjean, Raphael Zhou, Clément Moroldo
+Product name: redemption, a FLOSS RDP proxy
+Copyright (C) Wallix 2010-2020
+Author(s): Jonathan Poelen, Christophe Grosjean, Raphael Zhou
 */
 
 #include "mod/rdp/channels/cliprdr_channel.hpp"
@@ -29,9 +29,6 @@
 #include "utils/sugar/numerics/safe_conversions.hpp"
 #include "capture/fdx_capture.hpp"
 
-
-// TODO add timer on file
-// TODO add limit on file transfered
 
 namespace
 {
@@ -903,8 +900,6 @@ struct ClipboardVirtualChannel::D
             self.verbose);
 
         clip.current_file_list_format_id = receiver.file_list_format_id;
-        // TODO
-        // clip.transfer_state = ClipCtx::TransferState::Empty;
 
         return true;
     }
@@ -921,7 +916,7 @@ struct ClipboardVirtualChannel::D
 
     void lock(ClipCtx& clip, bytes_view chunk_data)
     {
-        // TODO is validator or storage
+        // TODO if validator or storage
 
         RDPECLIP::LockClipboardDataPDU pdu;
         InStream in_stream(chunk_data);
@@ -940,7 +935,7 @@ struct ClipboardVirtualChannel::D
 
     void unlock(ClipCtx& clip, bytes_view chunk_data)
     {
-        // TODO is validator or storage
+        // TODO if validator or storage
 
         RDPECLIP::UnlockClipboardDataPDU pdu;
         InStream in_stream(chunk_data);
@@ -981,8 +976,6 @@ struct ClipboardVirtualChannel::D
         InStream in_stream(chunk_data);
         pdu.recv(in_stream);
         clip.requested_format_id = pdu.requestedFormatId;
-
-        // TODO check requested_format_id
 
         if (bool(this->self.verbose & RDPVerbose::cliprdr)) {
             pdu.log();
