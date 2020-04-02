a208ae981e0b4e9bd4b2984d932ee8a9553a1f6a
Raphaël ZHOU <rzhou@wallix.com>
2020-04-02 14:42:00 +0200

    refs #26174: Automatically enable Session Probe executable name customization.

--- src/acl/module_manager/create_module_rdp.cpp
+++ src/acl/module_manager/create_module_rdp.cpp
@@ -644,8 +644,13 @@ ModPack create_mod_rdp(ModWrapper & mod_wrapper,
     enum {
         SESSION_PROBE_ALTERNATE_DIRECTORY_ENVIRONMENT_VARIABLE_NAME_MAX_LENGTH = 3
     };
-    if (mod_rdp_params.session_probe_params.alternate_directory_environment_variable.length() > SESSION_PROBE_ALTERNATE_DIRECTORY_ENVIRONMENT_VARIABLE_NAME_MAX_LENGTH) {
-        mod_rdp_params.session_probe_params.alternate_directory_environment_variable.resize(SESSION_PROBE_ALTERNATE_DIRECTORY_ENVIRONMENT_VARIABLE_NAME_MAX_LENGTH);
+    size_t const alternate_directory_environment_variable_length = mod_rdp_params.session_probe_params.alternate_directory_environment_variable.length();
+    if (alternate_directory_environment_variable_length) {
+        if (alternate_directory_environment_variable_length > SESSION_PROBE_ALTERNATE_DIRECTORY_ENVIRONMENT_VARIABLE_NAME_MAX_LENGTH) {
+            mod_rdp_params.session_probe_params.alternate_directory_environment_variable.resize(SESSION_PROBE_ALTERNATE_DIRECTORY_ENVIRONMENT_VARIABLE_NAME_MAX_LENGTH);
+        }
+
+        mod_rdp_params.session_probe_params.customize_executable_name = true;
     }
 
     using LogCategoryFlags = DispatchReportMessage::LogCategoryFlags;
