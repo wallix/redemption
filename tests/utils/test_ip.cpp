8f14cd4a5f169a1e9e70bba62419bdcea900f6f2
Fabien <xytez@fabien>
2020-06-02 14:34:08 +0200

    refs #27339: Fix unit testing because of comparing pointers directly rather compare strings content

--- tests/utils/test_ip.cpp
+++ tests/utils/test_ip.cpp
@@ -3,16 +3,20 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string_view>
 
 #include "test_only/test_framework/redemption_unit_tests.hpp"
 #include "utils/ip.hpp"
 
+using namespace std::string_view_literals;
+
 template <std::size_t N>
 using char_array_t = std::array<char, N>;
 
-using ipv4_address_t = char_array_t<INET_ADDRSTRLEN>;
-using ipv6_address_t = char_array_t<INET6_ADDRSTRLEN>;
-using port_t = char_array_t<8>;
+// + 1 to be sure to have enough space for null terminated character
+using ipv4_address_t = char_array_t<INET_ADDRSTRLEN + 1>;
+using ipv6_address_t = char_array_t<INET6_ADDRSTRLEN + 1>;
+using port_t = char_array_t<8 + 1>;
 
 RED_AUTO_TEST_CASE(Test_is_ipv4_mapped_ipv6_with_null_ip_address)
 {
@@ -62,15 +66,16 @@ RED_AUTO_TEST_CASE(Test_get_ivp4_address)
     ipv4_address_t dest_ipv4 { };
 
     get_ipv4_address(ipv6_address, dest_ipv4.data(), dest_ipv4.size());
-    RED_CHECK_EQUAL(dest_ipv4.data(), "192.0.2.128");
+    RED_CHECK_EQUAL(std::string_view(dest_ipv4.data()), "192.0.2.128"sv);
 }
 
-RED_AUTO_TEST_CASE(Test_get_underlying_ip_port_with_sockaddr_no_initialized)
+RED_AUTO_TEST_CASE(Test_get_underlying_ip_port_with_empty_sockaddr)
 {
     sockaddr sa;
     ipv4_address_t dest_ip { };
     port_t dest_port { };
 
+    memset(&sa, 0, sizeof(sa));
     RED_CHECK_NE(get_underlying_ip_port(sa,
                                         sizeof(sa),
                                         dest_ip.data(),
@@ -103,8 +108,8 @@ RED_AUTO_TEST_CASE(Test_get_underlying_ip_port_with_ipv4_sockaddr)
                                            dest_port.data(),
                                            dest_port.size()),
                     nullptr);
-    RED_CHECK_EQUAL(dest_ip.data(), "192.0.2.128");
-    RED_CHECK_EQUAL(dest_port.data(), "4242");
+    RED_CHECK_EQUAL(std::string_view(dest_ip.data()), "192.0.2.128"sv);
+    RED_CHECK_EQUAL(std::string_view(dest_port.data()), "4242"sv);
 }
 
 RED_AUTO_TEST_CASE(Test_get_underlying_ip_port_with_ipv6_sockaddr)
@@ -130,8 +135,10 @@ RED_AUTO_TEST_CASE(Test_get_underlying_ip_port_with_ipv6_sockaddr)
                                            dest_port.data(),
                                            dest_port.size()),
                     nullptr);
-    RED_CHECK_EQUAL(dest_ip.data(), "2001:db8:85a3::8a2e:370:7334");
-    RED_CHECK_EQUAL(dest_port.data(), "4242");
+    RED_CHECK_EQUAL(std::string_view(dest_ip.data()),
+                    "2001:db8:85a3::8a2e:370:7334"sv);
+    RED_CHECK_EQUAL(std::string_view(dest_port.data()),
+                    "4242"sv);
 }
 
 RED_AUTO_TEST_CASE(Test_get_underlying_ip_port_with_ipv4_mapped_ipv6_sockaddr)
@@ -157,6 +164,7 @@ RED_AUTO_TEST_CASE(Test_get_underlying_ip_port_with_ipv4_mapped_ipv6_sockaddr)
                                            dest_port.data(),
                                            dest_port.size()),
                     nullptr);
-    RED_CHECK_EQUAL(dest_ip.data(), "192.0.2.128");
-    RED_CHECK_EQUAL(dest_port.data(), "4242");
+    RED_CHECK_EQUAL(std::string_view(dest_ip.data()), "192.0.2.128"sv);
+    RED_CHECK_EQUAL(std::string_view(dest_port.data()), "4242"sv);
 }
+    
