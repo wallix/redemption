5973fadca1bd3e4023fc9f3a8f5991231b385b5d
Christophe Grosjean <pykoder@gmail.com>
2020-07-23 13:37:00 +0200

    Removed Session Log specific tests from test_acl_serializer, Removed AclSerializer specific tests from test_session_log

--- tests/acl/test_acl_serializer.cpp
+++ tests/acl/test_acl_serializer.cpp
@@ -78,16 +78,6 @@ RED_AUTO_TEST_CASE(TestAclSerializeAskNextModule)
     AclSerializer acl(ini);
     Sesman sesman(ini, timebase);
 
-    auto notify_error = [&sesman](const Error & error)
-    {
-        if (error.errnum == ENOSPC) {
-            // error.id = ERR_TRANSPORT_WRITE_NO_ROOM;
-            sesman.report("FILESYSTEM_FULL", "100|unknown");
-        }
-    };
-
-
-    SessionLogFile log_file(ini, timebase, cctx, rnd, fstat, notify_error);
     acl.set_auth_trans(&trans);
 
     ini.set<cfg::context::forcemodule>(true);
@@ -103,7 +93,6 @@ RED_AUTO_TEST_CASE(TestAclSerializeAskNextModule)
     ThrowTransport transexcpt;
     AclSerializer aclexcpt(ini);
 
-    SessionLogFile log_file_excpt(ini, timebase, cctx, rnd, fstat, notify_error);
     aclexcpt.set_auth_trans(&transexcpt);
 
     ini.set_acl<cfg::globals::auth_user>("Newuser");
@@ -135,15 +124,6 @@ RED_AUTO_TEST_CASE(TestAclSerializeIncoming)
 
     Sesman sesman(ini, timebase);
 
-    auto notify_error = [&sesman](const Error & error)
-    {
-        if (error.errnum == ENOSPC) {
-            // error.id = ERR_TRANSPORT_WRITE_NO_ROOM;
-            sesman.report("FILESYSTEM_FULL", "100|unknown");
-        }
-    };
-
-    SessionLogFile log_file(ini, timebase, cctx, rnd, fstat, notify_error);
     acl.set_auth_trans(&trans);
     ini.set<cfg::context::session_id>("");
     ini.set_acl<cfg::globals::auth_user>("testuser");
@@ -182,7 +162,6 @@ RED_AUTO_TEST_CASE(TestAclSerializeIncoming)
     transexcpt.disable_remaining_error();
     AclSerializer aclexcpt(ini);
 
-    SessionLogFile log_file_excpt(ini, timebase, cctx, rnd, fstat, notify_error);
     aclexcpt.set_auth_trans(&transexcpt);
     RED_CHECK_EXCEPTION_ERROR_ID(aclexcpt.incoming(), ERR_ACL_MESSAGE_TOO_BIG);
 }
@@ -209,15 +188,6 @@ RED_AUTO_TEST_CASE(TestAclSerializerIncoming)
 
     Sesman sesman(ini, timebase);
 
-    auto notify_error = [&sesman](const Error & error)
-    {
-        if (error.errnum == ENOSPC) {
-            // error.id = ERR_TRANSPORT_WRITE_NO_ROOM;
-            sesman.report("FILESYSTEM_FULL", "100|unknown");
-        }
-    };
-
-    SessionLogFile log_file(ini, timebase, cctx, rnd, fstat, notify_error);
     acl.set_auth_trans(&trans);
 
     RED_CHECK(not ini.is_asked<cfg::context::opt_bpp>());
@@ -271,15 +241,6 @@ RED_AUTO_TEST_CASE(TestAclSerializeSendBigData)
 
     Sesman sesman(ini, timebase);
 
-    auto notify_error = [&sesman](const Error & error)
-    {
-        if (error.errnum == ENOSPC) {
-            // error.id = ERR_TRANSPORT_WRITE_NO_ROOM;
-            sesman.report("FILESYSTEM_FULL", "100|unknown");
-        }
-    };
-
-    SessionLogFile log_file(ini, timebase, cctx, rnd, fstat, notify_error);
     acl.set_auth_trans(&trans);
 
     ini.set_acl<cfg::context::rejected>(std::string(sz_string, 'a'));
@@ -328,15 +289,6 @@ RED_AUTO_TEST_CASE(TestAclSerializeReceiveBigData)
 
     Sesman sesman(ini, timebase);
 
-    auto notify_error = [&sesman](const Error & error)
-    {
-        if (error.errnum == ENOSPC) {
-            // error.id = ERR_TRANSPORT_WRITE_NO_ROOM;
-            sesman.report("FILESYSTEM_FULL", "100|unknown");
-        }
-    };
-
-    SessionLogFile log_file(ini, timebase, cctx, rnd, fstat, notify_error);
     acl.set_auth_trans(&trans);
 
     std::string result(sz_string, 'a');
@@ -382,15 +334,6 @@ RED_AUTO_TEST_CASE(TestAclSerializeReceiveKeyMultiPacket)
 
     Sesman sesman(ini, timebase);
 
-    auto notify_error = [&sesman](const Error & error)
-    {
-        if (error.errnum == ENOSPC) {
-            // error.id = ERR_TRANSPORT_WRITE_NO_ROOM;
-            sesman.report("FILESYSTEM_FULL", "100|unknown");
-        }
-    };
-
-    SessionLogFile log_file(ini, timebase, cctx, rnd, fstat, notify_error);
     acl.set_auth_trans(&trans);
 
     RED_CHECK_EXCEPTION_ERROR_ID(acl.incoming(), ERR_ACL_UNEXPECTED_IN_ITEM_OUT);
@@ -417,15 +360,6 @@ RED_AUTO_TEST_CASE(TestAclSerializeUnknownKey)
 
     Sesman sesman(ini, timebase);
 
-    auto notify_error = [&sesman](const Error & error)
-    {
-        if (error.errnum == ENOSPC) {
-            // error.id = ERR_TRANSPORT_WRITE_NO_ROOM;
-            sesman.report("FILESYSTEM_FULL", "100|unknown");
-        }
-    };
-
-    SessionLogFile log_file(ini, timebase, cctx, rnd, fstat, notify_error);
     acl.set_auth_trans(&trans);
 
     RED_CHECK(not ini.is_asked<cfg::context::opt_bpp>());
@@ -446,177 +380,3 @@ RED_AUTO_TEST_CASE(TestAclSerializeUnknownKey)
     RED_CHECK_EQUAL(ini.get<cfg::context::reporting>(), "didier");
 }
 
-
-RED_AUTO_TEST_CASE_WD(TestAclSerializeLog, wd)
-{
-    Inifile ini;
-    ini.clear_send_index();
-
-    LCGRandom rnd;
-    Fstat fstat;
-    CryptoContext cctx;
-    TimeBase timebase({0, 0});
-
-    ini.set_acl<cfg::globals::auth_user>("admin");
-    ini.set_acl<cfg::globals::target_user>("user1");
-    ini.set_acl<cfg::globals::host>("10.10.13.12");
-    ini.set<cfg::session_log::enable_arcsight_log>(true);
-    // ini.set_acl<cfg::globals::target_host>("13.12.10.10");
-    // ini.set_acl<cfg::globals::ip_target>("13.12.10.10");
-    // ini.set_acl<cfg::globals::session_id>("0x520");
-
-    auto logfile = wd.add_file("log5_6.log");
-    auto hashdir = wd.create_subdirectory("hash");
-    auto hashlog = hashdir.add_file("log5_6.log");
-
-    ini.set<cfg::capture::record_filebase>("log5_6");
-    ini.set<cfg::video::record_path>(wd.dirname().string());
-    ini.set<cfg::video::hash_path>(hashdir.dirname().string());
-
-    GeneratorTransport trans(""_av);
-    AclSerializer acl_serial(ini);
-
-    Sesman sesman(ini, timebase);
-
-    auto notify_error = [&sesman](const Error & error)
-    {
-        if (error.errnum == ENOSPC) {
-            // error.id = ERR_TRANSPORT_WRITE_NO_ROOM;
-            sesman.report("FILESYSTEM_FULL", "100|unknown");
-        }
-    };
-
-    SessionLogFile log_file(ini, timebase, cctx, rnd, fstat, notify_error);
-    acl_serial.set_auth_trans(&trans);
-
-    setenv("TZ", "CET-1CEST,M3.5.0,M10.5.0/3", 1);          // for localtime
-
-    log_file.open_session_log();
-
-    {
-        tu::log_buffered logbuf;
-
-        log_file.log6(LogId::INPUT_LANGUAGE, {
-            KVLog("identifier"_av,   "ident"_av),
-            KVLog("display_name"_av, "name"_av),
-        });
-
-        log_siem_syslog(LogId::INPUT_LANGUAGE, {
-            KVLog("identifier"_av,   "ident"_av),
-            KVLog("display_name"_av, "name"_av),
-        }, ini, acl_serial.session_type);
-
-        auto now = timebase.get_current_time();
-        log_siem_arcsight(now.tv_sec,
-            LogId::INPUT_LANGUAGE, {
-            KVLog("identifier"_av,   "ident"_av),
-            KVLog("display_name"_av, "name"_av),
-        }, ini, acl_serial.session_type);
-
-        auto expected6 = cstr_array_view("[Neutral Session] session_id=\"\" client_ip=\"10.10.13.12\" target_ip=\"\" user=\"admin\" device=\"\" service=\"\" account=\"user1\" type=\"INPUT_LANGUAGE\" identifier=\"ident\" display_name=\"name\"\nJan 01 1970 00:00:00 host message CEF:1|Wallix|Bastion|" VERSION "|24|INPUT_LANGUAGE|5|WallixBastionSessionType=Neutral WallixBastionSessionId= WallixBastionHost=10.10.13.12 WallixBastionTargetIP= WallixBastionUser=admin WallixBastionDevice= WallixBastionService= WallixBastionAccount=user1 identifier=ident display_name=name\n");
-        RED_CHECK(logbuf.buf() == expected6);
-    }
-
-    timebase.increment_sec(10);
-
-    {
-        tu::log_buffered logbuf;
-
-        log_file.log6(LogId::CONNECTION_FAILED, {
-            KVLog("msg"_av, "long long\nmessage=|x\\y\"z"_av),
-            KVLog("msg2"_av, "vnc"_av),
-        });
-
-        log_siem_syslog(LogId::CONNECTION_FAILED, {
-            KVLog("msg"_av, "long long\nmessage=|x\\y\"z"_av),
-            KVLog("msg2"_av, "vnc"_av),
-        }, ini, acl_serial.session_type);
-
-        auto now = timebase.get_current_time();
-        log_siem_arcsight(now.tv_sec,LogId::CONNECTION_FAILED, {
-            KVLog("msg"_av, "long long\nmessage=|x\\y\"z"_av),
-            KVLog("msg2"_av, "vnc"_av),
-        }, ini, acl_serial.session_type);
-
-        auto expected6 = cstr_array_view("[Neutral Session] session_id=\"\" client_ip=\"10.10.13.12\" target_ip=\"\" user=\"admin\" device=\"\" service=\"\" account=\"user1\" type=\"CONNECTION_FAILED\" msg=\"long long\\nmessage=|x\\\\y\\\"z\" msg2=\"vnc\"\n"
-                                         "Jan 01 1970 00:00:10 host message CEF:1|Wallix|Bastion|" VERSION "|11|CONNECTION_FAILED|5|WallixBastionSessionType=Neutral WallixBastionSessionId= WallixBastionHost=10.10.13.12 WallixBastionTargetIP= WallixBastionUser=admin WallixBastionDevice= WallixBastionService= WallixBastionAccount=user1 msg=long long\\nmessage\\=|x\\\\y\"z msg2=vnc\n");
-
-        RED_CHECK(logbuf.buf() == expected6);
-    }
-
-    timebase.increment_sec(3023);
-
-    {
-        tu::log_buffered logbuf;
-
-        log_siem_syslog(LogId::DRIVE_REDIRECTION_RENAME, {
-            KVLog("app"_av, "rdp"_av),
-            KVLog("oldFilePath"_av, "/dir/old_file.ext"_av),
-            KVLog("filePath"_av, "/dir/new_file.ext"_av),
-        }, ini, acl_serial.session_type);
-
-        auto now = timebase.get_current_time();
-        log_siem_arcsight(now.tv_sec,LogId::DRIVE_REDIRECTION_RENAME, {
-            KVLog("app"_av, "rdp"_av),
-            KVLog("oldFilePath"_av, "/dir/old_file.ext"_av),
-            KVLog("filePath"_av, "/dir/new_file.ext"_av),
-        }, ini, acl_serial.session_type);
-
-        log_file.log6(LogId::DRIVE_REDIRECTION_RENAME, {
-            KVLog("app"_av, "rdp"_av),
-            KVLog("oldFilePath"_av, "/dir/old_file.ext"_av),
-            KVLog("filePath"_av, "/dir/new_file.ext"_av),
-        });
-
-        auto expected6 = cstr_array_view("[Neutral Session] session_id=\"\" client_ip=\"10.10.13.12\" target_ip=\"\" user=\"admin\" device=\"\" service=\"\" account=\"user1\" type=\"DRIVE_REDIRECTION_RENAME\" app=\"rdp\" oldFilePath=\"/dir/old_file.ext\" filePath=\"/dir/new_file.ext\"\n"
-                                         "Jan 01 1970 00:50:33 host message CEF:1|Wallix|Bastion|" VERSION "|15|DRIVE_REDIRECTION_RENAME|5|WallixBastionSessionType=Neutral WallixBastionSessionId= WallixBastionHost=10.10.13.12 WallixBastionTargetIP= WallixBastionUser=admin WallixBastionDevice= WallixBastionService= WallixBastionAccount=user1 app=rdp oldFilePath=/dir/old_file.ext filePath=/dir/new_file.ext\n");
-
-        RED_CHECK(logbuf.buf() == expected6);
-    }
-
-    log_file.close_session_log();
-
-    RED_CHECK_FILE_CONTENTS(logfile,
-        "1970-01-01 01:00:00 type=\"INPUT_LANGUAGE\" identifier=\"ident\" display_name=\"name\"\n"
-        "1970-01-01 01:00:10 type=\"CONNECTION_FAILED\" msg=\"long long\\nmessage=|x\\\\y\\\"z\" msg2=\"vnc\"\n"
-        "1970-01-01 01:50:33 type=\"DRIVE_REDIRECTION_RENAME\" app=\"rdp\" oldFilePath=\"/dir/old_file.ext\" filePath=\"/dir/new_file.ext\"\n"_av);
-}
-
-
-RED_AUTO_TEST_CASE_WD(TestSessionLogFile, wd)
-{
-    std::string const logname = "acl_log.txt";
-    auto filename = wd.add_file(logname);
-    auto hashdir = wd.create_subdirectory("hash");
-    auto hashname = hashdir.add_file(logname);
-
-    LCGRandom rnd;
-    Fstat fstat;
-    CryptoContext cctx;
-    init_keys(cctx);
-    TimeBase timebase({0, 0});
-    Inifile ini;
-    SessionLogFile log_file(ini, timebase, cctx, rnd, fstat, [&](const Error & e){
-        RED_FAIL(e.errmsg());
-    });
-
-    setenv("TZ", "CET-1CEST,M3.5.0,M10.5.0/3", 1);          // for localtime
-
-    log_file.open(filename, hashname, 0, logname);
-    log_file.write_line(1512484183, cstr_array_view("test first"));
-    log_file.write_line(1512484185, cstr_array_view("test second"));
-    log_file.close_session_log();
-
-    RED_CHECK_FILE_CONTENTS(filename,
-        "2017-12-05 15:29:43 test first\n"
-        "2017-12-05 15:29:45 test second\n"_av);
-
-    InFileTransport t(unique_fd{open(hashname.c_str(), O_RDONLY)});
-    MwrmReader mwrm_reader(t);
-    MetaLine meta_line;
-    mwrm_reader.set_header({WrmVersion::v2, false});
-    mwrm_reader.read_meta_hash_line(meta_line);
-    RED_CHECK_EQ(meta_line.filename, logname);
-    RED_CHECK(not meta_line.with_hash);
-    RED_CHECK_EQ(meta_line.size, 63);
-}
