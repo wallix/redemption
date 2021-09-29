6d50d33b89dfa29fe87972ece1809af7cc367aa7
Jonathan Poelen <jonathan.poelen@gmail.com>
2021-09-29 19:28:47 +0200

    add LowerTag and converters

--- src/utils/ascii.hpp
+++ src/utils/ascii.hpp
@@ -57,6 +57,36 @@ namespace detail
     };
     static_assert(sizeof(ascii_to_upper_table) == 255);
 
+    inline constexpr char const ascii_to_lower_table[] = {
+        '\x00', '\x01', '\x02', '\x03', '\x04', '\x05', '\x06', '\x07', '\x08', '\x09',
+        '\x0a', '\x0b', '\x0c', '\x0d', '\x0e', '\x0f', '\x10', '\x11', '\x12', '\x13',
+        '\x14', '\x15', '\x16', '\x17', '\x18', '\x19', '\x1a', '\x1b', '\x1c', '\x1d',
+        '\x1e', '\x1f', '\x20', '\x21', '\x22', '\x23', '\x24', '\x25', '\x26', '\x27',
+        '\x28', '\x29', '\x2a', '\x2b', '\x2c', '\x2d', '\x2e', '\x2f', '\x30', '\x31',
+        '\x32', '\x33', '\x34', '\x35', '\x36', '\x37', '\x38', '\x39', '\x3a', '\x3b',
+        '\x3c', '\x3d', '\x3e', '\x3f', '\x40', 'a', 'b', 'c', 'd', 'e',
+        'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+        'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+        'z', '\x5b', '\x5c', '\x5d', '\x5e', '\x5f', '\x60', 'a', 'b', 'c',
+        'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w',
+        'x', 'y', 'z', '\x7b', '\x7c', '\x7d', '\x7e', '\x7f', '\x80', '\x81',
+        '\x82', '\x83', '\x84', '\x85', '\x86', '\x87', '\x88', '\x89', '\x8a', '\x8b',
+        '\x8c', '\x8d', '\x8e', '\x8f', '\x90', '\x91', '\x92', '\x93', '\x94', '\x95',
+        '\x96', '\x97', '\x98', '\x99', '\x9a', '\x9b', '\x9c', '\x9d', '\x9e', '\x9f',
+        '\xa0', '\xa1', '\xa2', '\xa3', '\xa4', '\xa5', '\xa6', '\xa7', '\xa8', '\xa9',
+        '\xaa', '\xab', '\xac', '\xad', '\xae', '\xaf', '\xb0', '\xb1', '\xb2', '\xb3',
+        '\xb4', '\xb5', '\xb6', '\xb7', '\xb8', '\xb9', '\xba', '\xbb', '\xbc', '\xbd',
+        '\xbe', '\xbf', '\xc0', '\xc1', '\xc2', '\xc3', '\xc4', '\xc5', '\xc6', '\xc7',
+        '\xc8', '\xc9', '\xca', '\xcb', '\xcc', '\xcd', '\xce', '\xcf', '\xd0', '\xd1',
+        '\xd2', '\xd3', '\xd4', '\xd5', '\xd6', '\xd7', '\xd8', '\xd9', '\xda', '\xdb',
+        '\xdc', '\xdd', '\xde', '\xdf', '\xe0', '\xe1', '\xe2', '\xe3', '\xe4', '\xe5',
+        '\xe6', '\xe7', '\xe8', '\xe9', '\xea', '\xeb', '\xec', '\xed', '\xee', '\xef',
+        '\xf0', '\xf1', '\xf2', '\xf3', '\xf4', '\xf5', '\xf6', '\xf7', '\xf8', '\xf9',
+        '\xfa', '\xfb', '\xfc', '\xfd', '\xfe',
+    };
+    static_assert(sizeof(ascii_to_lower_table) == 255);
+
     struct StringAsArrayAccess;
 
     template<class>
@@ -67,9 +97,6 @@ namespace detail
     using enable_if_same_tag_t = std::enable_if_t<
         std::is_same<Tag, typename extract_string_tag<T>::type
     >::value, R>;
-
-    constexpr std::size_t unsafe_ascii_to_upper(char* dest, chars_view src) noexcept;
-
 
     struct StringAsArrayAccess
     {
@@ -95,10 +122,44 @@ namespace detail
 } // namespace detail
 
 
-constexpr inline char ascii_to_upper(char c) noexcept
-{
-    return detail::ascii_to_upper_table[static_cast<unsigned char>(c)];
-}
+class UpperTag {};
+class LowerTag {};
+
+// fast with long string
+constexpr struct ascii_to_upper_t {
+    using tag_type = UpperTag;
+    constexpr inline char operator()(char c) const noexcept
+    {
+        return 'a' <= c && c <= 'z' ? (c - 'a' + 'A') : c;
+    }
+} ascii_to_upper;
+
+// fast with long string
+constexpr struct ascii_to_lower_t {
+    using tag_type = LowerTag;
+    constexpr inline char operator()(char c) const noexcept
+    {
+        return 'A' <= c && c <= 'Z' ? (c - 'A' + 'a') : c;
+    }
+} ascii_to_lower;
+
+// fast with short string (/!\ cache miss)
+constexpr struct ascii_to_upper_with_table_t {
+    using tag_type = UpperTag;
+    constexpr inline char operator()(char c) const noexcept
+    {
+        return detail::ascii_to_upper_table[static_cast<unsigned char>(c)];
+    }
+} ascii_to_upper_with_table;
+
+// fast with short string (/!\ cache miss)
+constexpr struct ascii_to_lower_with_table_t {
+    using tag_type = LowerTag;
+    constexpr inline char operator()(char c) const noexcept
+    {
+        return detail::ascii_to_lower_table[static_cast<unsigned char>(c)];
+    }
+} ascii_to_lower_with_table;
 
 template<class String>
 struct TaggedStringTraits
@@ -107,6 +168,12 @@ struct TaggedStringTraits
     {
         return std::string_view(str);
     }
+};
+
+template<class T>
+struct TagOfConverterTraits
+{
+    using type = typename T::tag_type;
 };
 
 template<std::size_t N>
@@ -216,43 +283,68 @@ namespace detail
         using type = Tag;
     };
 
-
-    constexpr inline std::size_t unsafe_ascii_to_upper(char* dest, chars_view src) noexcept
+    template<class Transform>
+    constexpr inline std::size_t unsafe_ascii_to(char* dest, chars_view src, Transform&& transform) noexcept
     {
         auto* start = dest;
         for (char c : src) {
-            *dest++ = ascii_to_upper(c);
+            *dest++ = transform(c);
         }
         return std::size_t(dest - start);
     }
 } // namespace detail
 
 
-class UpperTag {};
-// class LowerTag {};
-
-template<std::size_t N>
-constexpr TaggedStringArray<UpperTag, N> ascii_to_limited_upper(chars_view str) noexcept
+template<std::size_t N, class To,
+    class Tag = typename TagOfConverterTraits<std::decay_t<To>>::type>
+constexpr TaggedStringArray<Tag, N>
+chars_to_tagged_string_array(chars_view str, To&& to)
 {
-    TaggedStringArray<UpperTag, N> upper;
+    TaggedStringArray<Tag, N> upper;
     if (str.size() <= N) {
         auto& array = detail::StringAsArrayAccess::internal(upper.str);
         auto* p = array.buffer.data();
-        array.len = detail::unsafe_ascii_to_upper(p, str);
+        array.len = detail::unsafe_ascii_to(p, str, to);
+    }
+    return upper;
+}
+
+template<std::size_t N, class To,
+    class Tag = typename TagOfConverterTraits<std::decay_t<To>>::type>
+constexpr TaggedZStringArray<Tag, N>
+chars_to_tagged_zstring_array(chars_view str, To&& to)
+{
+    TaggedZStringArray<Tag, N> upper;
+    if (str.size() <= N) {
+        upper.str.delayed_build([&](auto& array){
+            return detail::unsafe_ascii_to(array.data(), str, to);
+        });
     }
     return upper;
 }
 
 template<std::size_t N>
+constexpr TaggedStringArray<UpperTag, N> ascii_to_limited_upper(chars_view str) noexcept
+{
+    return chars_to_tagged_string_array<N>(str, ascii_to_upper);
+}
+
+template<std::size_t N>
 constexpr TaggedZStringArray<UpperTag, N> ascii_to_limited_zupper(chars_view str) noexcept
 {
-    TaggedZStringArray<UpperTag, N> upper;
-    if (str.size() <= N) {
-        upper.str.delayed_build([str](auto& array){
-            return detail::unsafe_ascii_to_upper(array.data(), str);
-        });
-    }
-    return upper;
+    return chars_to_tagged_zstring_array<N>(str, ascii_to_upper);
+}
+
+template<std::size_t N>
+constexpr TaggedStringArray<LowerTag, N> ascii_to_limited_lower(chars_view str) noexcept
+{
+    return chars_to_tagged_string_array<N>(str, ascii_to_lower);
+}
+
+template<std::size_t N>
+constexpr TaggedZStringArray<LowerTag, N> ascii_to_limited_zlower(chars_view str) noexcept
+{
+    return chars_to_tagged_zstring_array<N>(str, ascii_to_lower);
 }
 
 REDEMPTION_DIAGNOSTIC_PUSH()
@@ -263,18 +355,43 @@ constexpr TaggedStringView<UpperTag> operator "" _ascii_upper() noexcept
 {
     return {jln::string_c<ascii_to_upper(cs)...>::sv()};
 }
+
+template<class C, C... cs>
+constexpr TaggedStringView<LowerTag> operator "" _ascii_lower() noexcept
+{
+    return {jln::string_c<ascii_to_lower(cs)...>::sv()};
+}
 REDEMPTION_DIAGNOSTIC_POP()
 
-inline bool insensitive_eq(chars_view sv, TaggedStringView<UpperTag> upper)
+namespace detail
 {
-    if (sv.size() != upper.sv().size()) {
-        return false;
-    }
+    template<class Tag, class To>
+    inline bool tagged_string_equal_with_transform(
+        chars_view sv, TaggedStringView<Tag> tagged_str, To const& to)
+    {
+        static_assert(std::is_same_v<typename TagOfConverterTraits<To>::type, Tag>);
 
-    for (std::size_t i = 0; i < upper.sv().size(); ++i) {
-        if (upper.sv()[i] != ascii_to_upper(sv[i])) {
+        if (sv.size() != tagged_str.sv().size()) {
             return false;
         }
+
+        for (std::size_t i = 0; i < tagged_str.sv().size(); ++i) {
+            if (tagged_str.sv()[i] != to(sv[i])) {
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
+} // namespace detail
+
+template<class To = ascii_to_upper_t>
+inline bool insensitive_eq(chars_view sv, TaggedStringView<UpperTag> upper, To&& to = To())
+{
+    return detail::tagged_string_equal_with_transform(sv, upper, to);
+}
+
+template<class To = ascii_to_lower_t>
+inline bool insensitive_eq(chars_view sv, TaggedStringView<LowerTag> lower, To&& to = To())
+{
+    return detail::tagged_string_equal_with_transform(sv, lower, to);
 }
--- tests/utils/test_ascii.cpp
+++ tests/utils/test_ascii.cpp
@@ -34,11 +34,33 @@ RED_AUTO_TEST_CASE(TestUpper)
     RED_CHECK(ascii_to_limited_zupper<50>("abcdefghijklmnopqrstuvwxyz"_av).zsv()[26] == '\0');
     RED_CHECK("abcdefghijklmnopqrstuvwxyz"_ascii_upper.sv() == "ABCDEFGHIJKLMNOPQRSTUVWXYZ"_av);
     RED_CHECK((ascii_to_limited_upper<50>("abcdefghijklmnopqrstuvwxyz"_av) == "abcdefghijklmnopqrstuvwxyz"_ascii_upper));
+    RED_CHECK(ascii_to_limited_upper<100>("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"_av).sv()
+        == "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"_av);
     RED_CHECK(insensitive_eq("abC"_av, "abc"_ascii_upper));
     RED_CHECK(insensitive_eq("ABC"_av, "abc"_ascii_upper));
     RED_CHECK(insensitive_eq("abc"_av, "abc"_ascii_upper));
     RED_CHECK(!insensitive_eq("axc"_av, "abc"_ascii_upper));
     RED_CHECK(!insensitive_eq("ab"_av, "abc"_ascii_upper));
+}
+
+RED_AUTO_TEST_CASE(TestLower)
+{
+    RED_CHECK(ascii_to_limited_lower<3>("1A."_av).sv() == "1a."_av);
+    RED_CHECK(ascii_to_limited_lower<3>("1A.."_av).sv() == ""_av);
+    RED_CHECK(ascii_to_limited_lower<50>("ABCDEFGHIJKLMNOPQRSTUVWXYZ"_av).sv() == "abcdefghijklmnopqrstuvwxyz"_av);
+    RED_CHECK(ascii_to_limited_lower<5>("ABCDEFGHIJKLMNOPQRSTUVWXYZ"_av).sv() == ""_av);
+    RED_CHECK(ascii_to_limited_zlower<50>("ABCDEFGHIJKLMNOPQRSTUVWXYZ"_av).sv() == "abcdefghijklmnopqrstuvwxyz"_av);
+    RED_CHECK(ascii_to_limited_zlower<5>("ABCDEFGHIJKLMNOPQRSTUVWXYZ"_av).sv() == ""_av);
+    RED_CHECK(ascii_to_limited_zlower<50>("ABCDEFGHIJKLMNOPQRSTUVWXYZ"_av).zsv()[26] == '\0');
+    RED_CHECK("ABCDEFGHIJKLMNOPQRSTUVWXYZ"_ascii_lower.sv() == "abcdefghijklmnopqrstuvwxyz"_av);
+    RED_CHECK((ascii_to_limited_lower<50>("ABCDEFGHIJKLMNOPQRSTUVWXYZ"_av) == "abcdefghijklmnopqrstuvwxyz"_ascii_lower));
+    RED_CHECK(ascii_to_limited_lower<100>("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"_av).sv()
+        == "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~0123456789abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"_av);
+    RED_CHECK(insensitive_eq("abC"_av, "abc"_ascii_lower));
+    RED_CHECK(insensitive_eq("ABC"_av, "abc"_ascii_lower));
+    RED_CHECK(insensitive_eq("abc"_av, "abc"_ascii_lower));
+    RED_CHECK(!insensitive_eq("axc"_av, "abc"_ascii_lower));
+    RED_CHECK(!insensitive_eq("ab"_av, "abc"_ascii_lower));
 }
 
 
