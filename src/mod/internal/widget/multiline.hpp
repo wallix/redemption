e7567576c0ae3cc3a259d74edc89359a93030b85
Jonathan Poelen <jonathan.poelen@gmail.com>
2021-10-15 15:57:54 +0200

    refs #32855: fix disclaimer Message truncated

--- src/mod/internal/widget/multiline.hpp
+++ src/mod/internal/widget/multiline.hpp
@@ -48,8 +48,8 @@ public:
     }
 
 private:
-    static const size_t buffer_size = 1024;
-    static const size_t max_line = 50;
+    static const size_t buffer_size = 1024 * 20;
+    static const size_t max_line = 1024;
 
     struct line_t
     {
