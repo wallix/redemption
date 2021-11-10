b9ed5ccee4237132858c19c054feca5de79b24aa
Meng Tan <mtan@wallix.com>
2021-11-10 16:49:10 +0100

    credssp: dft: add missing nla_server files

---
+++ src/core/RDP/nla/nla_server.cpp
@@ -0,0 +1,89 @@
+/*
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License as published by the
+  Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+  Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  675 Mass Ave, Cambridge, MA 02139, USA.
+
+  Product name: redemption, a FLOSS RDP proxy
+  Copyright (C) Wallix 2021
+  Author(s): David Fort
+*/
+#include "nla_server.hpp"
+#include "nla_server_backends.hpp"
+#include "utils/log.hpp"
+
+
+
+INlaAuthBackend::INlaAuthBackend(NlaServer & parent)
+: mParent(parent)
+{
+}
+
+
+NlaServer::NlaServer(Random & rand,
+        const TimeBase & time_base,
+        uint32_t credssp_version,
+        bool credssp_verbose, bool verbose)
+: credssp_verbose(credssp_verbose)
+, verbose(verbose)
+, credssp_version(credssp_version)
+, rand(rand)
+, time_base(time_base)
+, nla_state(NLA_STATE_INIT)
+{
+}
+
+
+std::vector<uint8_t> NlaServer::authenticate_next(bytes_view in_data) {
+    std::vector<uint8_t> ret;
+
+    TSRequest req = recvTSRequest(in_data, this->credssp_verbose);
+    std::vector<uint8_t> mic_data;
+
+    switch (this->nla_state) {
+    case NLA_STATE_INIT: {
+        bool u2u = false;
+        KnownOid r = guessAuthTokenType(req.negoTokens);
+        switch (r) {
+        case OID_NTLM:
+            backend = new NtlmNlaBackend(*this);
+            break;
+        case OID_SPNEGO:
+            backend = new SpNegoNlaBackend(*this);
+            break;
+        case OID_KRB5_U2U:
+            u2u = true;
+            REDEMPTION_CXX_FALLTHROUGH;
+        case OID_KRB5:
+        case OID_KRB5_KILE:
+            backend = new GssapiNlaAuthBackend(*this, false, u2u);
+            break;
+        default:
+            // TODO: error
+            break;
+        }
+
+        this->nla_state = NLA_STATE_AUTHENTICATING;
+        ret = backend->authenticate_next(req.negoTokens, bytes_view(), mic_data, this->state);
+        return emitTSRequest(req.version, /*emitMechTokensEnvelop(*/ret/*)*/, bytes_view(), bytes_view(), 0, bytes_view(), false, this->verbose);
+    }
+    case NLA_STATE_AUTHENTICATING:
+        ret = backend->authenticate_next(req.negoTokens, bytes_view(), mic_data, this->state);
+        return emitTSRequest(req.version, /*emitMechTokensEnvelop(*/ret/*)*/, bytes_view(), bytes_view(), 0, bytes_view(), false, this->verbose);
+
+    case NLA_STATE_AUTHENTICATED:
+    case NLA_STATE_ERR:
+        // TODO: assert we should not be called in this state
+        return ret;
+    }
+
+}
---
+++ src/core/RDP/nla/nla_server_backends.hpp
@@ -0,0 +1,76 @@
+/*
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+   Product name: redemption, a FLOSS RDP proxy
+   Copyright (C) Wallix 2021
+   Author(s): David Fort
+
+   NLA server authentication backends
+*/
+
+#pragma once
+
+#include "core/RDP/nla/nla_server.hpp"
+#include "core/RDP/nla/credssp.hpp"
+#include "core/RDP/nla/kerberos.hpp"
+
+/** @brief authentication handler using SPNego */
+class NtlmNlaBackend : public INlaAuthBackend {
+public:
+    NtlmNlaBackend(NlaServer & parent);
+    virtual ~NtlmNlaBackend() = default;
+
+    virtual std::vector<uint8_t> authenticate_next(bytes_view auth_data, bytes_view in_mic_data,
+            std::vector<uint8_t> & out_mic_data, credssp::State & next_state);
+
+protected:
+};
+
+
+/** @brief authentication handler using SPNego */
+class SpNegoNlaBackend : public INlaAuthBackend {
+public:
+    SpNegoNlaBackend(NlaServer & parent);
+    virtual ~SpNegoNlaBackend();
+
+    virtual std::vector<uint8_t> authenticate_next(bytes_view auth_data, bytes_view in_mic_data,
+            std::vector<uint8_t> & out_mic_data, credssp::State & next_state);
+protected:
+    KnownOid mBackendType;
+    KnownOid mAnswerOid;
+    INlaAuthBackend *mSubBackend;
+};
+
+/** @brief authentication handler using Kerberos */
+class GssapiNlaAuthBackend : public INlaAuthBackend {
+public:
+    GssapiNlaAuthBackend(NlaServer & parent, bool spNego, bool u2u);
+    virtual ~GssapiNlaAuthBackend() = default;
+
+    bool init(const char *serviceName);
+
+    virtual std::vector<uint8_t> authenticate_next(bytes_view auth_data, bytes_view in_mic_data,
+            std::vector<uint8_t> & out_mic_data, credssp::State & next_state);
+
+public:
+    static bool mech_available(gss_OID mech);
+    static void report_error(OM_uint32 code, const char *str, OM_uint32 major_status, OM_uint32 minor_status);
+    static bool sanityCheck();
+
+protected:
+    bool sp_nego;
+    bool u2u;
+    std::unique_ptr<KERBEROSContext> krb_ctx;
+};
