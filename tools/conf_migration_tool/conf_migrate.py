f46d4cd2aa6f1e3344da22572cc74160ec10f366
Raphaël ZHOU <rzhou@wallix.com>
2021-11-10 18:36:41 +0100

    refs #32878: Add class 'RedemptionVersion'.

--- tools/conf_migration_tool/conf_migrate.py
+++ tools/conf_migration_tool/conf_migrate.py
@@ -10,61 +10,106 @@
 # Module description:  Sesman Worker
 ##
 
-def get_first_revision_letter_pos(build_string):
-    for i in range(0, len(build_string)):
-        if build_string[i].isalpha():
-            return i
+import sys
+import traceback
 
-    return -1
+class RedemptionVersion:
+    def __init__(self, version_string=None):
+        self.__major = -1
+        self.__minor = 0
+        self.__build = 0
+        self.__revision = ""
 
-def get_redemption_version_from_string(version_string):
-    version = {
-        "major": -1,
-        "minor": 0,
-        "build": 0,
-        "revision": ""
-    }
+        if version_string:
+            try:
+                parts = version_string.split('.')
 
-    try:
-        parts = version_string.split('.')
+                if len(parts) != 3:
+                    print(
+                         "RedemptionVersion.__init__: "
+                         "Invalid version string format: "
+                        f"\"{version_string}\"")
 
-        if len(parts) != 3:
-            raise Exception()
+                    return
 
-        version["major"] = int(parts[0])
-        version["minor"] = int(parts[1])
+                self.__major = int(parts[0])
+                self.__minor = int(parts[1])
 
-        revision_position = get_first_revision_letter_pos(parts[2])
-        if revision_position == -1:
-            version["build"] = int(parts[2])
-            version["revision"] = ""
+                revision_position =                                         \
+                    self.__class__.__get_first_revision_letter_pos(parts[2])
+                if -1 == revision_position:
+                    self.__build = int(parts[2])
+                else:
+                    self.__build = int(parts[2][:revision_position])
+                    self.__revision = parts[2][revision_position:]
+            except Exception as e:
+                _, exc_value, exc_traceback = sys.exc_info();
+                print(
+                     "RedemptionVersion.__init__: "
+                    f"{type(exc_value).__name__}")
+                traceback.print_tb(exc_traceback,
+                    file=sys.stdout)
+
+    def __str__(self):
+        if -1 == self.__major:
+            return "(invalid)"
+
+        return f"{self.__major}.{self.__minor}.{self.__build}{self.__revision}"
+
+    def __gt__(self, other):
+        if -1 == self.__major:
+            if -1 == other.__major:
+                return False
+
+            return True
+        elif -1 == other.__major:
+            return False
+        elif self.__major > other.__major:
+            return True
+        elif self.__major < other.__major:
+            return False
         else:
-            version["build"] = int(parts[2][:revision_position])
-            version["revision"] = parts[2][revision_position:]
-    except Exception:
-        pass
+            if self.__minor > other.__minor:
+                return True
+            elif self.__minor < other.__minor:
+                return False
+            else:
+                if self.__build > other.__build:
+                    return True
+                elif self.__build < other.__build:
+                    return False
+                else:
+                    if self.__revision > other.__revision:
+                        return True
+                    else:
+                        return False
 
-    return version
+    @classmethod
+    def fromfile(cls, filename):
+        version_string = ""
 
-def get_redemption_version_from_file(file_path):
-    version_string = ""
+        try:
+            with open(filename) as f:
+                line = f.readline() # read first line
 
-    try:
-        with open(file_path) as f:
-            line = f.readline()   # read first line
+                items = line.split()
 
-            items = line.split()
+                version_string = items[1]
 
-            version_string = items[1]
+        except Exception as e:
+            _, exc_value, exc_traceback = sys.exc_info();
+            print(
+                 "RedemptionVersion.fromfile: "
+                f"{type(exc_value).__name__}")
+            traceback.print_tb(exc_traceback,
+                file=sys.stdout)
 
-    except Exception:
-        pass
+        return cls(version_string)
 
-    return get_redemption_version_from_string(version_string)
+    @classmethod
+    def __get_first_revision_letter_pos(cls, build_string):
+        for i in range(0, len(build_string)):
+            if build_string[i].isalpha():
+                return i
 
-
-
-# print(get_redemption_version_from_file("/tmp/REDEMPTION_VERSION"))
-
-# print(get_redemption_version_from_string("3.5.9"))
-
+        return -1
