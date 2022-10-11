11e484556fba8f6cc1c981ce4d5f9001e234247f
Jonathan Poelen <jpoelen@wallix.com>
2022-10-11 11:30:38 +0200

    [config] invert no_reset_back_to_selector <-> reset_back_to_selector

--- projects/redemption_configs/src/configs/generators/sesman_default_map.hpp
+++ projects/redemption_configs/src/configs/generators/sesman_default_map.hpp
@@ -166,8 +166,11 @@ public:
     template<class Pack>
     void evaluate_member(Names const& /*section_names*/, Pack const& infos, type_enumerations& enums)
     {
-        if constexpr (is_convertible_v<Pack, decltype(sesman::constants::no_reset_back_to_selector)>)
+        if constexpr (is_convertible_v<Pack, decltype(sesman::constants::reset_back_to_selector)>)
         {
+            static_assert(!is_convertible_v<Pack, decltype(sesman::constants::no_reset_back_to_selector)>,
+                "no_reset_back_to_selector and reset_back_to_selector are exclusives");
+
             Names const& names = infos;
             auto sesman_type = get_type<spec::type_>(infos);
 
