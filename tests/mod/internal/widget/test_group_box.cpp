e0aa8c78162d6b57261edd381bfd8b8ec8b64f68
Jonathan Poelen <jpoelen@wallix.com>
2023-01-04 14:40:04 +0100

    [test][widget] simplify test_group_box

--- tests/mod/internal/widget/test_group_box.cpp
+++ tests/mod/internal/widget/test_group_box.cpp
@@ -30,128 +30,68 @@
 
 #define IMG_TEST_PATH FIXTURES_PATH "/img_ref/mod/internal/widget/group_box/"
 
+struct TestWidgetGroupBoxCtx
+{
+    TestGraphic drawable{800, 600};
+    WidgetScreen parent{drawable, 800, 600, global_font_lato_light_16(), nullptr, Theme{}};
+    WidgetGroupBox wgroupbox;
+    WidgetButton wbutton;
+
+    TestWidgetGroupBoxCtx(const char * text)
+    : wgroupbox(drawable, parent, /*notifier=*/nullptr, text,
+                /*fg_color=*/RED, /*bg_color=*/YELLOW, global_font_lato_light_16())
+    , wbutton(drawable, wgroupbox, /*notifier=*/nullptr, "Button 1", /*group_id=*/0,
+              /*fg_color=*/RED, /*bg_color=*/YELLOW, /*focuscolor*/LIGHT_YELLOW, 2,
+              global_font_lato_light_16(), 4, 1)
+    {
+        int16_t x = 200;
+        int16_t y = 100;
+
+        wgroupbox.set_wh(150, 200);
+        wgroupbox.set_xy(x, y);
+
+        wbutton.set_wh(wbutton.get_optimal_dim());
+        wbutton.set_xy(x + 10, y + 20);
+
+        wgroupbox.add_widget(&wbutton);
+
+        wgroupbox.rdp_input_invalidate(wgroupbox.get_rect());
+    }
+
+    void click()
+    {
+        wbutton.rdp_input_mouse(MOUSE_FLAG_BUTTON1 | MOUSE_FLAG_DOWN,
+                                wbutton.x() + 1, wbutton.y() + 1);
+        wgroupbox.rdp_input_invalidate(wgroupbox.get_rect());
+    }
+};
+
 RED_AUTO_TEST_CASE(TraceWidgetGroupBox)
 {
-    TestGraphic drawable(800, 600);
+    TestWidgetGroupBoxCtx ctx("Group 1");
 
-    // WidgetGroupBox is a widget at position 0,0 in it's parent context
-    WidgetScreen parent(drawable, 800, 600, global_font_lato_light_16(), nullptr, Theme{});
+    RED_CHECK_IMG(ctx.drawable, IMG_TEST_PATH "group_box_1.png");
 
-    NotifyApi * notifier = nullptr;
-    BGRColor   fg_color = RED;
-    BGRColor   bg_color = YELLOW;
-    int         group_id = 0;
-    int16_t     x        = 200;
-    int16_t     y        = 100;
-    uint16_t    cx       = 150;
-    uint16_t    cy       = 200;
+    ctx.click();
 
-    /* TODO
-     * I believe users of this widget may wish to control text position and behavior inside rectangle
-     * ie: text may be centered, aligned left, aligned right, or even upside down, etc
-     * these possibilities (and others) are supported in RDPGlyphIndex */
-    WidgetGroupBox wgroupbox( drawable, parent, notifier, "Group 1"
-                            , fg_color, bg_color, global_font_lato_light_16());
-    wgroupbox.set_wh(cx, cy);
-    wgroupbox.set_xy(x, y);
-
-    BGRColor focuscolor  = LIGHT_YELLOW;
-    int  xtext       = 4;
-    int  ytext       = 1;
-    WidgetButton wbutton(drawable, wgroupbox, notifier, "Button 1",
-                             group_id, fg_color, bg_color, focuscolor, 2, global_font_lato_light_16(),
-                             xtext, ytext);
-    Dimension dim = wbutton.get_optimal_dim();
-    wbutton.set_wh(dim);
-    wbutton.set_xy(x + 10, y + 20);
-
-    wgroupbox.add_widget(&wbutton);
-
-    // ask to widget to redraw at it's current position
-    wgroupbox.rdp_input_invalidate(Rect( wgroupbox.x()
-                                       , wgroupbox.y()
-                                       , wgroupbox.cx()
-                                       , wgroupbox.cy()
-                                       ));
-
-    RED_CHECK_IMG(drawable, IMG_TEST_PATH "group_box_1.png");
-
-    wbutton.rdp_input_mouse(MOUSE_FLAG_BUTTON1 | MOUSE_FLAG_DOWN,
-                            wbutton.x() + 1, wbutton.y() + 1);
-
-    // ask to widget to redraw at it's current position
-    wgroupbox.rdp_input_invalidate(Rect( wgroupbox.x()
-                                       , wgroupbox.y()
-                                       , wgroupbox.cx()
-                                       , wgroupbox.cy()
-                                       ));
-
-    RED_CHECK_IMG(drawable, IMG_TEST_PATH "group_box_2.png");
+    RED_CHECK_IMG(ctx.drawable, IMG_TEST_PATH "group_box_2.png");
 }
 
 RED_AUTO_TEST_CASE(TraceWidgetGroupBoxMax)
 {
-    TestGraphic drawable(800, 600);
-
-    // WidgetGroupBox is a widget at position 0,0 in it's parent context
-    WidgetScreen parent(drawable, 800, 600, global_font_lato_light_16(), nullptr, Theme{});
-
-    NotifyApi * notifier = nullptr;
-    BGRColor   fg_color = RED;
-    BGRColor   bg_color = YELLOW;
-    int         group_id = 0;
-    int16_t     x        = 200;
-    int16_t     y        = 100;
-    uint16_t    cx       = 150;
-    uint16_t    cy       = 200;
-
     auto text =
         "éàéàéàéàéàéàéàéàéàéàéàéàéàéàéàéà"
         "éàéàéàéàéàéàéàéàéàéàéàéàéàéàéàéà"
         "éàéàéàéàéàéàéàéàéàéàéàéàéàéàéàéà"
         "éàéàéàéàéàéàéàéàéàéàéàéàéàéàéàéà"_av;
 
-    /* TODO
-     * I believe users of this widget may wish to control text position and behavior inside rectangle
-     * ie: text may be centered, aligned left, aligned right, or even upside down, etc
-     * these possibilities (and others) are supported in RDPGlyphIndex */
-    WidgetGroupBox wgroupbox( drawable, parent, notifier, text.data()
-                            , fg_color, bg_color, global_font_lato_light_16());
-    wgroupbox.set_wh(cx, cy);
-    wgroupbox.set_xy(x, y);
+    TestWidgetGroupBoxCtx ctx(text.data());
 
-    RED_CHECK(std::string_view(wgroupbox.get_text()) == text.first(text.size()-2));
+    RED_CHECK(std::string_view(ctx.wgroupbox.get_text()) == text.first(text.size()-2));
 
-    BGRColor focuscolor  = LIGHT_YELLOW;
-    int  xtext       = 4;
-    int  ytext       = 1;
-    WidgetButton wbutton(drawable, wgroupbox, notifier, "Button 1",
-                             group_id, fg_color, bg_color, focuscolor, 2, global_font_lato_light_16(),
-                             xtext, ytext);
-    Dimension dim = wbutton.get_optimal_dim();
-    wbutton.set_wh(dim);
-    wbutton.set_xy(x + 10, y + 20);
+    RED_CHECK_IMG(ctx.drawable, IMG_TEST_PATH "group_box_3.png");
 
-    wgroupbox.add_widget(&wbutton);
+    ctx.click();
 
-    // ask to widget to redraw at it's current position
-    wgroupbox.rdp_input_invalidate(Rect( wgroupbox.x()
-                                       , wgroupbox.y()
-                                       , wgroupbox.cx()
-                                       , wgroupbox.cy()
-                                       ));
-
-    RED_CHECK_IMG(drawable, IMG_TEST_PATH "group_box_3.png");
-
-    wbutton.rdp_input_mouse(MOUSE_FLAG_BUTTON1 | MOUSE_FLAG_DOWN,
-                            wbutton.x() + 1, wbutton.y() + 1);
-
-    // ask to widget to redraw at it's current position
-    wgroupbox.rdp_input_invalidate(Rect( wgroupbox.x()
-                                       , wgroupbox.y()
-                                       , wgroupbox.cx()
-                                       , wgroupbox.cy()
-                                       ));
-
-    RED_CHECK_IMG(drawable, IMG_TEST_PATH "group_box_4.png");
+    RED_CHECK_IMG(ctx.drawable, IMG_TEST_PATH "group_box_4.png");
 }
