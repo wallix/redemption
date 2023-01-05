643f1cbfe94ca970619427d7982c95fd64654954
Jonathan Poelen <jpoelen@wallix.com>
2023-01-05 14:07:03 +0100

    [test][widget] simplify test_login_mod

--- tests/mod/internal/test_login_mod.cpp
+++ tests/mod/internal/test_login_mod.cpp
@@ -32,57 +32,54 @@
 #include "test_only/core/font.hpp"
 #include "core/events.hpp"
 
-RED_AUTO_TEST_CASE(TestLoginMod)
+struct TestLoginModCtx
 {
     ScreenInfo screen_info{800, 600, BitsPerPixel{24}};
-    FakeFront front(screen_info);
+    FakeFront front{screen_info};
     WindowListCaps window_list_caps;
-    EventContainer events;
-    ClientExecute client_execute(events.get_time_base(), front.gd(), front, window_list_caps, false);
+    EventManager event_manager;
+    ClientExecute client_execute{event_manager.get_time_base(), front.gd(), front, window_list_caps, false};
 
     Inifile ini;
     Theme theme;
 
-    Keymap keymap(*find_layout_by_id(KeyLayout::KbdId(0x040C)));
+    Keymap keymap{*find_layout_by_id(KeyLayout::KbdId(0x040C))};
 
-    RED_CHECK_NE(ini.get<cfg::globals::auth_user>(), "user");
-    RED_CHECK_NE(ini.get<cfg::context::password>(), "pass");
+    LoginMod d;
 
-    LoginMod d(ini, events, "user", "pass", front.gd(), front, screen_info.width, screen_info.height,
-               Rect(0, 0, 799, 599), client_execute, global_font(), theme);
-    d.init();
+    TestLoginModCtx(std::chrono::seconds authentication_timeout = {})
+    : d(
+        [&]() -> Inifile& {
+            ini.set<cfg::globals::authentication_timeout>(authentication_timeout);
+            return ini;
+        }(), event_manager.get_events(), "user", "pass", front.gd(), front,
+        screen_info.width, screen_info.height,
+        Rect(0, 0, 799, 599), client_execute, global_font(), theme)
+    {
+        d.init();
+    }
+};
 
-    keymap.event(Keymap::KbdFlags(), Keymap::Scancode(0x1c)); // enter
-    d.rdp_input_scancode(Keymap::KbdFlags(), Keymap::Scancode(0x1c), 0, keymap);
+RED_AUTO_TEST_CASE(TestLoginMod)
+{
+    TestLoginModCtx ctx;
+    ctx.keymap.event(Keymap::KbdFlags(), Keymap::Scancode(0x1c)); // enter
+    ctx.d.rdp_input_scancode(Keymap::KbdFlags(), Keymap::Scancode(0x1c), 0, ctx.keymap);
 
-    RED_CHECK_EQUAL(ini.get<cfg::globals::auth_user>(), "user");
-    RED_CHECK_EQUAL(ini.get<cfg::context::password>(), "pass");
+    RED_CHECK_EQUAL(ctx.ini.get<cfg::globals::auth_user>(), "user");
+    RED_CHECK_EQUAL(ctx.ini.get<cfg::context::password>(), "pass");
 }
 
 RED_AUTO_TEST_CASE(TestLoginMod2)
 {
-    ScreenInfo screen_info{2048, 1536, BitsPerPixel{24}};
-    FakeFront front(screen_info);
-    WindowListCaps window_list_caps;
-    EventManager event_manager;
-    auto& events = event_manager.get_events();
-    ClientExecute client_execute(events.get_time_base(), front.gd(), front, window_list_caps, false);
-
-    Inifile ini;
-    Theme theme;
-
-    ini.set<cfg::globals::authentication_timeout>(std::chrono::seconds(1));
-
-    LoginMod d(ini, events, "user", "pass", front.gd(), front, screen_info.width, screen_info.height,
-               Rect(1024, 768, 1023, 767), client_execute, global_font(), theme);
-    d.init();
+    TestLoginModCtx ctx(std::chrono::seconds(1));
 
     using namespace std::literals::chrono_literals;
 
-    event_manager.execute_events([](int){return false;}, false);
-    RED_CHECK_EQUAL(BACK_EVENT_NONE, d.get_mod_signal());
+    ctx.event_manager.execute_events([](int){return false;}, false);
+    RED_CHECK_EQUAL(BACK_EVENT_NONE, ctx.d.get_mod_signal());
 
-    event_manager.get_writable_time_base().monotonic_time = MonotonicTimePoint{2s + 1us};
-    event_manager.execute_events([](int){return false;}, false);
-    RED_CHECK_EQUAL(BACK_EVENT_STOP, d.get_mod_signal());
+    ctx.event_manager.get_writable_time_base().monotonic_time = MonotonicTimePoint{2s + 1us};
+    ctx.event_manager.execute_events([](int){return false;}, false);
+    RED_CHECK_EQUAL(BACK_EVENT_STOP, ctx.d.get_mod_signal());
 }
