18308206741f6e9f6ff80557be95b0efda082391
Raphaël ZHOU <rzhou@wallix.com>
2023-03-29 18:13:13 +0200

    WAB-5515: Do not retry the connection without SP after a first launch failure when the session is using self-deploying AD.

--- src/acl/module_manager/create_module_rdp.cpp
+++ src/acl/module_manager/create_module_rdp.cpp
@@ -778,6 +778,9 @@ ModPack create_mod_rdp(
         mod_rdp_params.session_probe_params.enable_session_probe                               = true;
         mod_rdp_params.session_probe_params.vc_params.launch_application_driver                = true;
         mod_rdp_params.session_probe_params.vc_params.launch_application_driver_then_terminate = !(ini.get<cfg::mod_rdp::enable_session_probe>());
+        mod_rdp_params.session_probe_params.vc_params.on_launch_failure                        = SessionProbeOnLaunchFailure::disconnect_user;
+
+        ini.set<cfg::mod_rdp::session_probe_on_launch_failure>(SessionProbeOnLaunchFailure::disconnect_user);
     }
     // ================== End Application Driver ======================
 
