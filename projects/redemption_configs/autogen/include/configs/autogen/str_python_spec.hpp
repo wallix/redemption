9e71fff5ac5f083c44139d1565fd9731f7770871
Meng Tan <mtan@wallix.com>
2023-04-07 14:10:30 +0200

    WAB-5453: add warning description regarding Access Manager and port change

--- projects/redemption_configs/autogen/include/configs/autogen/str_python_spec.hpp
+++ projects/redemption_configs/autogen/include/configs/autogen/str_python_spec.hpp
@@ -10,6 +10,7 @@ glyph_cache = boolean(default=False)
 
 # Warning: Service will be automatically restarted and active sessions will be disconnected
 # The port set in this field must not be already used, otherwise the service will not run.
+# Changing the port number will prevent WALLIX Access Manager from working properly.
 #_iptables
 #_advanced
 port = integer(min=0, default=3389)
