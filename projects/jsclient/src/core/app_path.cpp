949632ada6179cf29734436695618319a053309a
Jonathan Poelen <jpoelen@wallix.com>
2023-04-20 19:43:11 +0200

    [jsclient] fix compilation with app_path_test

---
+++ projects/jsclient/src/core/app_path.cpp
@@ -0,0 +1,12 @@
+/*
+SPDX-FileCopyrightText: 2023 Wallix Proxies Team
+
+SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "core/app_path.hpp"
+
+zstring_view app_path(AppPath) noexcept
+{
+    return ""_zv;
+}
