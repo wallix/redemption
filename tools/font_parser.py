932b1a25f26a0ca9c31b106cdadfa71963df5094
Jonathan Poelen <jpoelen@wallix.com>
2023-06-20 15:09:51 +0200

    update tools/font_parser.py

--- tools/font_parser.py
+++ tools/font_parser.py
@@ -53,21 +53,29 @@
 from PIL.ImageFont import ImageFont, truetype as load_truetype
 from unicodedata import category
 from typing import Any, Iterable, NamedTuple
+from enum import IntEnum
+from collections.abc import Collection
 
 import os
 import sys
 import struct
+import PIL
 
-BBox = tuple[int, int, int, int]
-Mask = Any
+
+BBox = tuple[int, int, int, int]  # x1, y1, x2, y2
+Pixels = bytes
 
 
 global_fontsize = 14
-font_descriptions: Iterable[tuple[int, str, Iterable[str]]] = (
-    (0, "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", ('\u20e3', '\u4e2d',)),
-    # (0, "/usr/share/fonts/truetype/lato/Lato-Light.ttf", ('0x20e3', '0x4e2d',)),
-    (0, "/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc", ('\u0104', '\u0302', '\U0003134a')),
-    # (0, "/usr/share/fonts/truetype/noto/NotoSansThai-Regular.ttf", ('\u20e3', '\u4e2d',)),
+font_descriptions: Iterable[tuple[
+    int,  # fontsize or 0 for global fontsize
+    str,  # path of font
+    Iterable[str]  # glyph for invalid char rendering
+]] = (
+    (global_fontsize, "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", ('\u02ef', '\u20e3',)),
+    # (global_fontsize, "/usr/share/fonts/truetype/lato/Lato-Light.ttf", ('\u0104', '\u0302',)),
+    (global_fontsize, "/usr/share/fonts/opentype/noto/NotoSansCJK-Regular.ttc", ('\u0104', '\u0302')),
+    # (global_fontsize, "/usr/share/fonts/truetype/noto/NotoSansThai-Regular.ttf", ('\u0104', '\u0302',)),
 )
 output = f"{os.path.splitext(os.path.basename(font_descriptions[0][1]))[0]}_{global_fontsize}.rbf"
 
@@ -81,38 +89,12 @@ if len(sys.argv) > 1:
     parser = argparse.ArgumentParser(description='rfb2 font generator')
     parser.add_argument('-o', '--output', metavar='FILENAME', default=output)
     parser.add_argument('-r', '--range', nargs=2, type=int, default=(CHARSET_START, CHARSET_END))
-    parser.add_argument('fonts_and_sizes', nargs='*', help='[fontsize [font [size]]...]')
 
     args = parser.parse_args()
 
     CHARSET_START = int(args.range[0])
     CHARSET_END = int(args.range[1])
     output = args.output
-
-    if args.fonts_and_sizes:
-        i = 0
-        try:
-            global_fontsize = int(args.fonts_and_sizes[0])
-            i += 1
-        except ValueError:
-            pass
-
-        new_font_descriptions = []
-        end = len(args.fonts_and_sizes)
-        while i < end:
-            fontpath = args.fonts_and_sizes[i]
-            i += 1
-            fontsize = 0
-            if i < end:
-                try:
-                    fontsize = int(args.fonts_and_sizes[i])
-                    i += 1
-                except ValueError:
-                    pass
-            new_font_descriptions.append((fontsize, fontpath))
-
-        if new_font_descriptions:
-            font_descriptions = new_font_descriptions
 
 
 def nbbytes(x: int) -> int:
@@ -127,52 +109,80 @@ def count_bit_padding(cx: int) -> int:
 
 class FontInfo(NamedTuple):
     font: ImageFont
-    unknown_chars: Iterable[tuple[Mask, BBox]]
+    unknown_chars: Iterable[tuple[Pixels, BBox, BBox]]
     ascent: int
 
 
-class GlyphInfo(NamedTuple):
-    font_info: FontInfo
-    mask: Mask
-    bbox: BBox
+def mask_to_tuple(mask: PIL.Image.core, bbox: None | BBox = None) -> Pixels:
+    if bbox is None:
+        bbox = mask.getbbox()
+    yseq = range(bbox[1], bbox[3])
+    return bytes(bytearray(mask.getpixel((ix, iy))
+                           for iy in yseq
+                           for ix in range(bbox[0], bbox[2])))
 
 
 font_infos: list[FontInfo] = [
     FontInfo(font := load_truetype(fontpath, fontsize or global_fontsize),
-             (*((mask := font.getmask(uni, mode='1'), mask.getbbox())
+             (*((mask_to_tuple(mask := font.getmask(uni, mode='1')), mask.getbbox(), font.getbbox(uni))
                 for uni in unknown_unicode_for_glyphs),),
              font.getmetrics()[0],
              )
     for fontsize, fontpath, unknown_unicode_for_glyphs in font_descriptions
 ]
 
-unknown_chars_by_font = (*(
-    (*(GlyphInfo(font_info, mask, bbox)
-       for mask, bbox in font_info.unknown_chars),)
-    for font_info in font_infos
-),)
-replacement_char = GlyphInfo(font_infos[0], mask := font_infos[0].font.getmask('\uFFFD', mode='1'), mask.getbbox())
+max_ascent = max(font_info.ascent for font_info in font_infos)
+max_height = max_ascent + max(font_info.font.getmetrics()[1] for font_info in font_infos)
 
-def find_unknown_char(mask: Mask, bbox: BBox, font_info: FontInfo) -> None | GlyphInfo:
-    for i, (unknown_char, bbox2) in enumerate(font_info.unknown_chars):
-        if bbox == bbox2 and all(mask.getpixel((ix, iy)) == unknown_char.getpixel((ix, iy))
-                                 for iy in range(bbox[1], bbox[3])
-                                 for ix in range(bbox[0], bbox[2])):
-            return unknown_chars_by_font[0][i]
+class GlyphType(IntEnum):
+    Normal = 0
+    Replacement = 1
+    Unknown = 2
+
+GlyphInfo = tuple[GlyphType, Pixels, BBox, int, int]  # int, int = offsetx, offsety
+
+unknown_char = font_infos[0].unknown_chars[0]
+unknown_glyph = (GlyphType.Unknown,
+                 unknown_char[0],
+                 unknown_char[1],
+                 unknown_char[2][0],
+                 unknown_char[2][1] + max_ascent - font_infos[0].ascent
+                 )
+
+replacement_unicode_char = '\uFFFD'
+replacement_char: GlyphInfo = (GlyphType.Replacement,
+                               mask_to_tuple(mask := font_infos[0].font.getmask(replacement_unicode_char, mode='1')),
+                               mask.getbbox(),
+                               (bbox_font := font_infos[0].font.getbbox(replacement_unicode_char))[0],
+                               bbox_font[1] + max_ascent - font_infos[0].ascent,
+                               )
+
+def find_unknown_char(pixels: Pixels, bbox: BBox, unknown_chars: Iterable[tuple[Pixels, BBox, BBox]]) -> GlyphInfo:
+    for i, unknown_char in enumerate(unknown_chars):
+        if bbox == unknown_char[1] and pixels == unknown_char[0]:
+            return unknown_glyph
     return None
 
-def get_font_mask(char: str) -> GlyphInfo:
-    unknown_char = None
+def get_glyph_info(char: str) -> GlyphInfo:
+    unknown_char = replacement_char
     for font_info in font_infos:
         mask = font_info.font.getmask(char, mode='1')
+        bbox_font = font_info.font.getbbox(char)
+        x1, y1, x2, y2 = bbox_font
         bbox = mask.getbbox()
         # is None for spaces
         if not bbox:
-            bbox = font_info.font.getbbox(char)
-            bbox = (bbox[0], 0, bbox[2], bbox[3] - bbox[1])
-        elif unknown_char := find_unknown_char(mask, bbox, font_info):
-            continue
-        return GlyphInfo(font_info, mask, bbox)
+            bbox = (x1, 0, x2, y2 - y1)
+            pixels = mask_to_tuple(mask, bbox)
+        else:
+            pixels = mask_to_tuple(mask)
+            unknown_char = find_unknown_char(pixels, bbox, font_info.unknown_chars)
+            if unknown_char:
+                continue
+
+        offsetx = x1
+        offsety = y1 + max_ascent - font_info.ascent
+        return GlyphType.Normal, pixels, bbox, offsetx, offsety
     return unknown_char
 
 def valid_chr(char: str) -> bool:
@@ -182,6 +192,80 @@ def valid_chr(char: str) -> bool:
 
 
 ichar_gen = range(CHARSET_START, CHARSET_END)
+
+total_data_len = 0
+data_glyphs = []
+cache = {}
+
+for i in ichar_gen:
+    char = chr(i)
+    is_printable = valid_chr(char)
+
+    glyph_type, pixels, bbox, offsetx, offsety = get_glyph_info(char) if is_printable else replacement_char
+
+    x1, y1, x2, y2 = bbox
+    offsetx += x1
+    offsety += y1
+    cx = x2 - x1
+    cy = y2 - y1
+    # incby = max(0, offsetx) + cx
+    incby = x2
+
+    total_data_len += align4(nbbytes(cx) * cy)
+
+    if not is_printable:
+        char = 'NonPrintable'
+
+    print(f"{i:#x}  CHR: {char}  TYPE: {glyph_type}  CX/CY: {cx},{cy}  INCBY: {incby}  OFFSET: {offsetx},{offsety}  BBOX: {bbox}")
+
+    cache_id = glyph_type if glyph_type else (cx, cy, incby, offsetx, offsety, pixels)
+    cache_elem = cache.get(cache_id)
+
+    if not cache_elem:
+        data = b''
+        padding = count_bit_padding(cx)
+        empty_line = '+' * incby + '\n'
+        # padding_line = 'o' * padding
+        left_empty_line = '+' * offsetx
+        right_empty_line = '>' * (incby - cy) + '\n'
+        line = empty_line * y1
+        for iy in range(y1, y1 + cy):
+            line += left_empty_line
+            byte = 0
+            counter = 0
+
+            for ix in range(x1, x1 + cx):
+                pix = pixels[(iy - y1) * cx + (ix - x1)]
+                byte <<= 1
+                if pix == 255:
+                    line += '#'
+                    byte |= 1
+                else:
+                    line += '.'
+
+                counter += 1
+                if counter == 8:
+                    data += struct.pack('<B', byte)
+                    counter = 0
+                    byte = 0
+
+            if counter != 0:
+                # line += padding_line
+                data += struct.pack('<B', byte << padding)
+
+            line += right_empty_line
+
+        data += b'\0' * (align4(nbbytes(cx) * cy) - nbbytes(cx) * cy)
+        datainfo = struct.pack('<IhhhhhHH', i, offsetx, offsety, 0, 0, incby, cx, cy)
+        # TODO new format (save 1/3: 6.8M -> 4.3M)
+        # datainfo = struct.pack('<bbBBB', offsetx, offsety, incby, cx, cy)
+
+        cache_elem = (datainfo + data, line + '\n')
+        cache[cache_id] = cache_elem
+
+    print(cache_elem[1])
+    data_glyphs.append(cache_elem[0])
+
 
 print(f'Output file: {output}')
 
@@ -196,114 +280,11 @@ with open(output, 'wb') as f:
     f.write(name)
     f.write(b'\0' * (max(0, 32 - len(name))))
 
-    max_ascent = max(font_info.ascent for font_info in font_infos)
-    max_height = max_ascent + max(font_info.font.getmetrics()[1] for font_info in font_infos)
-
-    total_data_len = 0
-    glyph_infos: list[GlyphInfo] = []
-    for i in ichar_gen:
-        char = chr(i)
-        is_printable = valid_chr(char)
-        # if not is_unknown_char:
-        #     glyph_infos.append(replacement_char)
-        #     continue
-
-        font_info, mask, bbox = get_font_mask(char) if is_printable else replacement_char
-
-        x1, y1, x2, y2 = bbox
-        cx = x2 - x1
-        cy = y2 - y1
-        total_data_len += align4(nbbytes(cx) * cy)
-
-        glyph_infos.append((font_info, mask, bbox))
-
     f.write(struct.pack('<H', global_fontsize))
-    f.write(struct.pack('<h', 1))
+    f.write(struct.pack('<H', 1))
     f.write(struct.pack('<H', max_height))
     f.write(struct.pack('<I', len(ichar_gen)))
     f.write(struct.pack('<I', total_data_len))
 
-    for i, glyph in zip(ichar_gen, glyph_infos):
-        char = chr(i)
-        font_info, mask, bbox = glyph
-        font = font_info.font
-        bbox_char = font.getbbox(char)
-
-        x1, y1, x2, y2 = bbox_char
-        offsetx = x1
-        offsety = y1 + max_ascent - font_info.ascent
-
-        # for display only
-        w = x2 - x1
-        h = y2 - y1
-
-        x1, y1, x2, y2 = bbox
-        offsetx += x1
-        offsety += y1
-        cx = x2 - x1
-        cy = y2 - y1
-        # incby = max(0, offsetx) + cx
-        incby = x2
-
-        is_printable = valid_chr(char)
-        if not is_printable:
-            char = 'NonPrintable'
-
-        print(f"{i:#x}  CHR: {char}  SIZE: {w},{h}  CX/CY: {cx},{cy}  INCBY: {incby}  OFFSET: {offsetx},{offsety}  IMGBBOX: {bbox_char}  BBOX: {bbox}")
-
-        f.write(struct.pack('<I', i))
-        f.write(struct.pack('<h', offsetx))
-        f.write(struct.pack('<h', offsety))
-        f.write(struct.pack('<h', 0))
-        f.write(struct.pack('<h', 0))
-        f.write(struct.pack('<h', incby))
-        f.write(struct.pack('<H', cx))
-        f.write(struct.pack('<H', cy))
-
-        # TODO new format (save 1/3: 6.8 -> 4.3)
-        # f.write(struct.pack('<b', offsetx))
-        # f.write(struct.pack('<b', offsety))
-        # f.write(struct.pack('<B', incby))
-        # f.write(struct.pack('<B', cx))
-        # f.write(struct.pack('<B', cy))
-
-        # if not is_printable:
-        #     continue
-
-        padding = count_bit_padding(cx)
-        empty_line = '+' * (w + padding) + '\n'
-        padding_line = 'o' * padding
-        left_empty_line = '+' * x1
-        right_empty_line = '+' * (w - x1 - cx)
-        line = empty_line * y1
-        for iy in range(y1, y1 + cy):
-            line += left_empty_line
-            byte = 0
-            counter = 0
-
-            for ix in range(x1, x1+cx):
-                pix = mask.getpixel((ix, iy))
-                byte <<= 1
-                if pix == 255:
-                    line += '#'
-                    byte |= 1
-                else:
-                    line += '.'
-
-                counter += 1
-                if counter == 8:
-                    f.write(struct.pack('<B', byte))
-                    counter = 0
-                    byte = 0
-
-            if counter != 0:
-                line += padding_line
-                f.write(struct.pack('<B', byte << padding))
-
-            line += right_empty_line + '\n'
-
-        f.write(b'\0' * (align4(nbbytes(cx) * cy) - nbbytes(cx) * cy))
-
-        print(line + empty_line * (h - y2) + '\n')
-
-print(f'Output file: {output}')
+    for data in data_glyphs:
+        f.write(data)
