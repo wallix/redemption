e3ae5f73eddbf99d7cd911f3186936297be4bf5a
Meng Tan <mtan@wallix.com>
2023-09-06 16:56:43 +0200

    [tools] generate doc html in doc-metrics job

--- tools/compute_doc_percent.sh
+++ tools/compute_doc_percent.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
-set -o errexit -o pipefail -o noclobber -o nounset
+set -o errexit -o pipefail -o nounset
 
 cd $(dirname "$0")/../projects/redemption_configs/
 
-function show_progress()
+function generate_html()
 {
-    tools/spec2html.py "$1" | grep -oE '[0-9]+ / [0-9]+'
+    tools/spec2html.py "$1"
 }
 
-read OPT_NUM sep OPT_TOTAL < <(show_progress autogen/spec/rdp.spec)
-read CP_NUM sep CP_TOTAL   < <(show_progress <(
+function get_progress()
+{
+    grep -oE '[0-9]+ / [0-9]+' "$1"
+}
+
+generate_html autogen/spec/rdp.spec > rdp_cps.html
+generate_html <(
     sed -E 's/\)gen_config_ini"( << )?|( << )?R"gen_config_ini\(//g' \
         autogen/include/configs/autogen/str_python_spec.hpp
-))
+) > rdp_ops.html
+read OPT_NUM sep OPT_TOTAL < <(get_progress rdp_ops.html)
+read CP_NUM sep CP_TOTAL   < <(get_progress rdp_cps.html)
 
 echo "Documented parameters $(($OPT_NUM + $CP_NUM)) / $(($OPT_TOTAL + $CP_TOTAL))"
 result=$((($OPT_NUM + $CP_NUM) * 10000 / ($OPT_TOTAL + $CP_TOTAL)))
