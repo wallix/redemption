51cbd7e82bb436233c95546b0d749ac844affaa3
Jonathan Poelen <jpoelen@wallix.com>
2023-09-06 16:26:42 +0200

    [tools] update tools/compute_doc_percent.sh

--- tools/compute_doc_percent.sh
+++ tools/compute_doc_percent.sh
@@ -2,33 +2,19 @@
 
 set -o errexit -o pipefail -o noclobber -o nounset
 
-cd $(dirname "$0")/..
+cd $(dirname "$0")/../projects/redemption_configs/
 
-RDP_CP_HTML=rdp_connection_policy.html
-RDP_OPT_HTML=rdp_options.html
-CONFIG_PROJ_PATH=projects/redemption_configs/
-CONVERT_TOOL=spec2html.py
-
+function show_progress()
 {
-    cd projects/redemption_configs/
-    bjam -j4
+    tools/spec2html.py "$1" | grep -oE '[0-9]+ / [0-9]+'
+}
 
-    rm "$RDP_CP_HTML"
-    tools/"$CONVERT_TOOL" autogen/spec/rdp.spec > "$RDP_CP_HTML"
-
-    cd -
-
-    bjam -j4 rdpproxy
-
-    rm projects/redemption_configs/"$RDP_OPT_HTML"
-    bin/gcc-8.3.0/release/rdpproxy --print-spec | projects/redemption_configs/tools/"$CONVERT_TOOL" - > projects/redemption_configs/"$RDP_OPT_HTML"
-
-    read OPT_NUM sep OPT_TOTAL < <(grep -oE '[0-9]+ / [0-9]+' projects/redemption_configs/"$RDP_OPT_HTML")
-
-    read CP_NUM sep CP_TOTAL < <(grep -oE '[0-9]+ / [0-9]+' projects/redemption_configs/"$RDP_CP_HTML")
-
-} &> /dev/null
-
+read OPT_NUM sep OPT_TOTAL < <(show_progress autogen/spec/rdp.spec)
+read CP_NUM sep CP_TOTAL   < <(show_progress <(
+    sed -E 's/\)gen_config_ini"( << )?|( << )?R"gen_config_ini\(//g' \
+        autogen/include/configs/autogen/str_python_spec.hpp
+))
 
 echo "Documented parameters $(($OPT_NUM + $CP_NUM)) / $(($OPT_TOTAL + $CP_TOTAL))"
-echo "Percentage done $((($OPT_NUM + $CP_NUM) * 100 / ($OPT_TOTAL + $CP_TOTAL))) %"
+result=$((($OPT_NUM + $CP_NUM) * 10000 / ($OPT_TOTAL + $CP_TOTAL)))
+echo "Percentage done $((result / 100)).$((result % 100)) %"
