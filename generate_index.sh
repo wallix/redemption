05c6ee4d626961db8f1d30949dfdd6997aa193e7
Jonathan Poelen <jpoelen@wallix.com>
2023-11-21 12:12:48 +0100

    menu sorted by version

--- generate_index.sh
+++ generate_index.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env bash
-toc=''
+bastion_branches=''
+other_branches=''
 for d in */ ; do
-    toc+="<li><a href='$d'>$d</a></li>"$'\n'
+    name=${d/\/}
+    if [[ $name =~ ^'maintenance_bastion_'([0-9]+)'.0'$ ]]; then
+        name="Bastion <b>${BASH_REMATCH[1]}</b>"
+        bastion_branches+="<li><a href='$d'>$name</a></li>"$'\n'
+    else
+        other_branches+="<li><a href='$d'>$name</a></li>"$'\n'
+    fi
 done
+
+toc="$other_branches$(sort -Vr <<<"$bastion_branches")"
+
 echo -E '<!DOCTYPE html>
 <html>
 <head>
 <title>Index</title>
+<style>
+a {
+  font-size: 1.2em;
+  padding: .3rem;
+  display: block;
+}
+a:first-letter {
+  text-transform: uppercase;
+}
+a:hover {
+  background: #eee;
+}
+</style>
 <body>
 <ul>
 '"$toc"'
