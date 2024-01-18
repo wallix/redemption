79afd365dcd620671945d93ac5c7f2a85de3722a
Jonathan Poelen <jpoelen@wallix.com>
2024-01-18 18:22:32 +0100

    [tools] source_deps_cp: copy log_as_* and add configuration for destination

--- tools/source_deps_cp.sh
+++ tools/source_deps_cp.sh
@@ -6,41 +6,60 @@ if [[ "${DEBUG:-0}" = '1' ]]; then
     set -x
 fi
 
-dest=redemption_source
+dest=${DEST:-redemption_source}
 
 if (( $# == 0 )) || [[ "$1" = '-h' || "$1" = '--help' ]]; then
-    echo "Copy .cpp and .hpp in '$dest' from a bjam target
+    echo "Copy .cpp and .hpp in DEST from a bjam target
 
         $0 targets...
 
-    Ex: $0 libscytale" 2>&1
+    Ex: $0 libscytale
+
+Environment variables:
+    DEBUG=${DEBUG:-0}
+    DEST=$dest" >&2
     exit
 fi
+
+redemption_path="$(realpath "$(dirname "$0")"/..)"
+pushd "$redemption_path" >/dev/null
 
 # extract hpp/cpp
 typeset -A dirset sources
 while read l ; do
     if [[ $l =~ ^[a-zA-Z0-9_/]+'.o: '(.*) ]]; then
         for f in ${BASH_REMATCH[1]}; do
-            # check duplicate
+            # ignore duplications
             if [[ ! -v sources[$f] ]]; then
                 sources[$f]=1
-                dirset[${f%/*}]+="$f "
+                dirset[${f%/*}]+="$redemption_path/$f "
             fi
         done
     fi
 done < <(bjam "$@" cxxflags='-MM -MF -' -a ||:)
 
-rm -r $dest
+# insert all cpp for log.hpp (the choice depends on the compilation options)
+if [[ $f = 'src/utils/log.hpp' ]]; then
+    if [[ ! -v sources['src/utils/log_as_logprint.cpp'] ]]; then
+        dirset[src/utils]+="$redemption_path/src/utils/log_as_logprint.cpp "
+    fi
+    if [[ ! -v sources['src/utils/log_as_syslog.cpp'] ]]; then
+        dirset[src/utils]+="$redemption_path/src/utils/log_as_syslog.cpp "
+    fi
+fi
+
+popd >/dev/null
+
+# rm -r "$dest" ||:
 
 # create new arbo
-dirs=("${!dirset[@]}")
-for i in "${!dirs[@]}"; do
-    dirs[$i]=$dest/${dirs[$i]}
+dirs=()
+for dir in "${!dirset[@]}"; do
+    dirs+=("$dest/$dir")
 done
-mkdir -p ${dirs[@]}
+mkdir -p "${dirs[@]}"
 
 # copy files
 for dir in "${!dirset[@]}"; do
-    cp ${dirset[$dir]} $dest/$dir
+    cp ${dirset[$dir]} "$dest/$dir"
 done
