f5f213c6e8a305ae7c706d53a1ecfe001fd79a3a
Jonathan Poelen <jpoelen@wallix.com>
2024-03-01 10:47:42 +0100

    [jenkins] use clang-16 instead of clang-19. This last crash

--- tools/jenkins.sh
+++ tools/jenkins.sh
@@ -60,7 +60,7 @@ fi
 echo -e "
 using gcc : 10.0 : g++-10 -DREDEMPTION_DISABLE_NO_BOOST_PREPROCESSOR_WARNING ;
 using gcc : 8.0 : g++-8 -DREDEMPTION_DISABLE_NO_BOOST_PREPROCESSOR_WARNING ;
-using clang : : clang++ -DREDEMPTION_DISABLE_NO_BOOST_PREPROCESSOR_WARNING ;
+using clang : : clang++-16 -DREDEMPTION_DISABLE_NO_BOOST_PREPROCESSOR_WARNING ;
 " > project-config.jam
 valgrind_compiler=gcc-8
 toolset_gcc=toolset=gcc-10
